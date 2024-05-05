92205d059fcc052b36f258adb94ec45266b84923
ziedenit <guendil_zied@yahoo.com>
2024-05-05 17:48:57 +0200

    Update CompteControleur.java

--- BackGestionBanque/src/main/java/com/banque/fr/webServices/CompteControleur.java
+++ BackGestionBanque/src/main/java/com/banque/fr/webServices/CompteControleur.java
@@ -1,35 +1,48 @@
- public Bien createBienFromFinancementGarantieDto(FinancementGarantieDto financementGarantieDto, BienGarantieDto bienGarantieDto) {
-Refactor this method to reduce its Cognitive Complexity from 30 to the 15 allowed.
-        Bien bien = Bien.builder()
-                .idBien(IdGeneratorService.generateId("B"))
-                .adresseComplete(bienGarantieDto.isBienFinanceLCL() ? financementGarantieDto.getAdresseBien() : bienGarantieDto.getAdresseBien())
-                .codePostal(bienGarantieDto.isBienFinanceLCL() ? financementGarantieDto.getCodePostalBien() : bienGarantieDto.getCodePostalBien())
-                .codeNormeThermique(bienGarantieDto.isBienFinanceLCL() ? financementGarantieDto.getCodeNormeThermique() : bienGarantieDto.getCodeNormeThermique())
-                .nomCommune(bienGarantieDto.isBienFinanceLCL() ? financementGarantieDto.getVilleBien() : bienGarantieDto.getVilleBien())
-                .paysBien(bienGarantieDto.isBienFinanceLCL() ? financementGarantieDto.getPaysBien() : bienGarantieDto.getPaysBien())
-                .surfaceBien(bienGarantieDto.isBienFinanceLCL() ? financementGarantieDto.getSurfaceBien() : bienGarantieDto.getSurfaceBien() == null ? 0.00 : bienGarantieDto.getSurfaceBien())
-Extract this nested ternary operation into an independent statement.
-                .typeEnergie(bienGarantieDto.isBienFinanceLCL() ? financementGarantieDto.getTypeEnergie() : bienGarantieDto.getTypeEnergie())
-                .typeBatiment(bienGarantieDto.isBienFinanceLCL() ? financementGarantieDto.getTypeBatiment() : bienGarantieDto.getTypeBatiment())
-                .codeBatiment(bienGarantieDto.isBienFinanceLCL() ? financementGarantieDto.getCodeBatiment() : bienGarantieDto.getCodeBatiment())
-                .etatBien(bienGarantieDto.isBienFinanceLCL() ? financementGarantieDto.getEtatBien() : bienGarantieDto.getEtatBien())
-                .bienFinanceLCL(bienGarantieDto.isBienFinanceLCL() ? financementGarantieDto.isBienFinanceLCL() : bienGarantieDto.isBienFinanceLCL())
-                .prixBien(bienGarantieDto.isBienFinanceLCL() ? financementGarantieDto.getPrixBien() : bienGarantieDto.getPrixBien() == null ? 0.00 : bienGarantieDto.getPrixBien())
-Extract this nested ternary operation into an independent statement.
-                .montantFinanceLCL(bienGarantieDto.isBienFinanceLCL() ? financementGarantieDto.getMontantFinanceLCL() : bienGarantieDto.getMontantFinanceLCL() == null ? 0.00 : bienGarantieDto.getMontantFinanceLCL())
-Extract this nested ternary operation into an independent statement.
-                .partLCL(bienGarantieDto.isBienFinanceLCL() ? financementGarantieDto.getPartLCL() : bienGarantieDto.getPartLCL() == null ? 0.00 : bienGarantieDto.getPartLCL())
-Extract this nested ternary operation into an independent statement.
-                .dpeActuel(Dpe.builder()
-                        .numeroDpe(bienGarantieDto.isBienFinanceLCL() ? financementGarantieDto.getNumeroDpe() : bienGarantieDto.getNumeroDpe())
-                        .estimationCep(bienGarantieDto.isBienFinanceLCL() ? financementGarantieDto.getEstimationCep() : bienGarantieDto.getEstimationCep())
-                        .classeCep(bienGarantieDto.isBienFinanceLCL() ? financementGarantieDto.getClasseCep() : bienGarantieDto.getClasseCep())
-                        .estimationGes(bienGarantieDto.isBienFinanceLCL() ? financementGarantieDto.getEstimationGes() : bienGarantieDto.getEstimationGes())
-                        .classeGes(bienGarantieDto.isBienFinanceLCL() ? financementGarantieDto.getClasseGes() : bienGarantieDto.getClasseGes())
-                        .dateEtablissementDpe(bienGarantieDto.isBienFinanceLCL() ? financementGarantieDto.getDateEtablissementDpe() : bienGarantieDto.getDateEtablissementDpe())
-                        .dateFinValiditeDpe(bienGarantieDto.isBienFinanceLCL() ? financementGarantieDto.getDateFinValiditeDpe() : bienGarantieDto.getDateFinValiditeDpe())
-                        .sirenDiagnostiqueur(bienGarantieDto.isBienFinanceLCL() ? financementGarantieDto.getSirenDiagnostiqueur() : bienGarantieDto.getSirenDiagnostiqueur())
-                        .build())
-                .build();
-Immediately return this expression instead of assigning it to the temporary variable "bien".
-        return bien;
+public Bien createBienFromFinancementGarantieDto(FinancementGarantieDto financementGarantieDto, BienGarantieDto bienGarantieDto) {
+    Bien.BienBuilder bienBuilder = Bien.builder()
+            .idBien(IdGeneratorService.generateId("B"))
+            .adresseComplete(bienGarantieDto.isBienFinanceLCL() ? financementGarantieDto.getAdresseBien() : bienGarantieDto.getAdresseBien())
+            .codePostal(bienGarantieDto.isBienFinanceLCL() ? financementGarantieDto.getCodePostalBien() : bienGarantieDto.getCodePostalBien())
+            .codeNormeThermique(bienGarantieDto.isBienFinanceLCL() ? financementGarantieDto.getCodeNormeThermique() : bienGarantieDto.getCodeNormeThermique())
+            .nomCommune(bienGarantieDto.isBienFinanceLCL() ? financementGarantieDto.getVilleBien() : bienGarantieDto.getVilleBien())
+            .paysBien(bienGarantieDto.isBienFinanceLCL() ? financementGarantieDto.getPaysBien() : bienGarantieDto.getPaysBien())
+            .typeEnergie(bienGarantieDto.isBienFinanceLCL() ? financementGarantieDto.getTypeEnergie() : bienGarantieDto.getTypeEnergie())
+            .typeBatiment(bienGarantieDto.isBienFinanceLCL() ? financementGarantieDto.getTypeBatiment() : bienGarantieDto.getTypeBatiment())
+            .codeBatiment(bienGarantieDto.isBienFinanceLCL() ? financementGarantieDto.getCodeBatiment() : bienGarantieDto.getCodeBatiment())
+            .etatBien(bienGarantieDto.isBienFinanceLCL() ? financementGarantieDto.getEtatBien() : bienGarantieDto.getEtatBien())
+            .bienFinanceLCL(bienGarantieDto.isBienFinanceLCL() ? financementGarantieDto.isBienFinanceLCL() : bienGarantieDto.isBienFinanceLCL())
+            .prixBien(getPrixBien(financementGarantieDto, bienGarantieDto))
+            .montantFinanceLCL(getMontantFinanceLCL(financementGarantieDto, bienGarantieDto))
+            .partLCL(getPartLCL(financementGarantieDto, bienGarantieDto))
+            .dpeActuel(getDpeActuel(financementGarantieDto, bienGarantieDto));
+
+    return bienBuilder.build();
+}
+
+private double getPrixBien(FinancementGarantieDto financementGarantieDto, BienGarantieDto bienGarantieDto) {
+    Double prixBien = bienGarantieDto.isBienFinanceLCL() ? financementGarantieDto.getPrixBien() : bienGarantieDto.getPrixBien();
+    return prixBien == null ? 0.00 : prixBien;
+}
+
+private double getMontantFinanceLCL(FinancementGarantieDto financementGarantieDto, BienGarantieDto bienGarantieDto) {
+    Double montantFinanceLCL = bienGarantieDto.isBienFinanceLCL() ? financementGarantieDto.getMontantFinanceLCL() : bienGarantieDto.getMontantFinanceLCL();
+    return montantFinanceLCL == null ? 0.00 : montantFinanceLCL;
+}
+
+private double getPartLCL(FinancementGarantieDto financementGarantieDto, BienGarantieDto bienGarantieDto) {
+    Double partLCL = bienGarantieDto.isBienFinanceLCL() ? financementGarantieDto.getPartLCL() : bienGarantieDto.getPartLCL();
+    return partLCL == null ? 0.00 : partLCL;
+}
+
+private Dpe getDpeActuel(FinancementGarantieDto financementGarantieDto, BienGarantieDto bienGarantieDto) {
+    return Dpe.builder()
+            .numeroDpe(bienGarantieDto.isBienFinanceLCL() ? financementGarantieDto.getNumeroDpe() : bienGarantieDto.getNumeroDpe())
+            .estimationCep(bienGarantieDto.isBienFinanceLCL() ? financementGarantieDto.getEstimationCep() : bienGarantieDto.getEstimationCep())
+            .classeCep(bienGarantieDto.isBienFinanceLCL() ? financementGarantieDto.getClasseCep() : bienGarantieDto.getClasseCep())
+            .estimationGes(bienGarantieDto.isBienFinanceLCL() ? financementGarantieDto.getEstimationGes() : bienGarantieDto.getEstimationGes())
+            .classeGes(bienGarantieDto.isBienFinanceLCL() ? financementGarantieDto.getClasseGes() : bienGarantieDto.getClasseGes())
+            .dateEtablissementDpe(bienGarantieDto.isBienFinanceLCL() ? financementGarantieDto.getDateEtablissementDpe() : bienGarantieDto.getDateEtablissementDpe())
+            .dateFinValiditeDpe(bienGarantieDto.isBienFinanceLCL() ? financementGarantieDto.getDateFinValiditeDpe() : bienGarantieDto.getDateFinValiditeDpe())
+            .sirenDiagnostiqueur(bienGarantieDto.isBienFinanceLCL() ? financementGarantieDto.getSirenDiagnostiqueur() : bienGarantieDto.getSirenDiagnostiqueur())
+            .build();
+}
