7751863dd4584b3edae80aa117f6a0f41ed0cfa0
ziedenit <guendil_zied@yahoo.com>
2024-05-05 22:35:46 +0200

    Update CompteControleur.java

--- BackGestionBanque/src/main/java/com/banque/fr/webServices/CompteControleur.java
+++ BackGestionBanque/src/main/java/com/banque/fr/webServices/CompteControleur.java
@@ -1,13 +1,26 @@
-   @Test
-    void should_call_and_return_dpe_from_ademe_public_service() throws Exception {
-        Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("127.0.0.1", 3128));
-        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
-        requestFactory.setProxy(proxy);
-        RestTemplate restTemplate = new RestTemplate(requestFactory);
-        String url = "http://localhost:8081/api/v1/ademe/dpe/2095V1001813O";
-        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+@Test
+void should_call_and_return_dpe_from_ademe_public_service() throws Exception {
+    Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("127.0.0.1", 3128));
+    SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+    requestFactory.setProxy(proxy);
+    
+    RestTemplate restTemplate = new RestTemplate(requestFactory);
 
-        // Vérifiez le statut de la réponse et le contenu si nécessaire
-        assertEquals(200, response.getStatusCodeValue());
-        // ...
-    }
+    // Ajoutez les informations d'authentification Basic
+    String username = "user1";
+    String password = "user1Pass";
+    String auth = username + ":" + password;
+    byte[] encodedAuth = Base64.getEncoder().encode(auth.getBytes());
+    String authHeader = "Basic " + new String(encodedAuth);
+    HttpHeaders headers = new HttpHeaders();
+    headers.set("Authorization", authHeader);
+    HttpEntity<String> entity = new HttpEntity<>(headers);
+
+    // Utilisez l'entité HTTP avec les en-têtes d'authentification
+    String url = "http://localhost:8081/api/v1/ademe/dpe/2095V1001813O";
+    ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+
+    // Vérifiez le statut de la réponse et le contenu si nécessaire
+    assertEquals(HttpStatus.OK, response.getStatusCode());
+    // ...
+}
