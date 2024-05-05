048ebb01d2730241d22d8856d8c80d5a5cdb307b
ziedenit <guendil_zied@yahoo.com>
2024-05-05 22:25:39 +0200

    Update CompteControleur.java

--- BackGestionBanque/src/main/java/com/banque/fr/webServices/CompteControleur.java
+++ BackGestionBanque/src/main/java/com/banque/fr/webServices/CompteControleur.java
@@ -1,28 +1,13 @@
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-class MyApiTest {
-
-    private final RestTemplate restTemplate;
-
-    public MyApiTest() {
-        // Configuration du proxy dans RestTemplate
-        this.restTemplate = new RestTemplateBuilder()
-                .proxy("127.0.0.1", 3128)
-                .build();
-    }
-
-    @Test
-    void testApiCallWithProxy() {
-        String url = "http://example.com/api/v1/ademe/dpe/2095V1001813O";
+   @Test
+    void should_call_and_return_dpe_from_ademe_public_service() throws Exception {
+        Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("127.0.0.1", 3128));
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setProxy(proxy);
+        RestTemplate restTemplate = new RestTemplate(requestFactory);
+        String url = "http://localhost:8081/api/v1/ademe/dpe/2095V1001813O";
         ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
 
         // Vérifiez le statut de la réponse et le contenu si nécessaire
         assertEquals(200, response.getStatusCodeValue());
         // ...
     }
-}
