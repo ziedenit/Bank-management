8fbad3412a4b24c26b1702806a19be4dc4f79c85
ziedenit <guendil_zied@yahoo.com>
2024-05-07 11:31:38 +0200

    Update CompteControleur.java

--- BackGestionBanque/src/main/java/com/banque/fr/webServices/CompteControleur.java
+++ BackGestionBanque/src/main/java/com/banque/fr/webServices/CompteControleur.java
@@ -1,57 +1,75 @@
-public String alignement(ContextXTRA ligneContext) {
+private String alignForAcquisitionInNewBuild(ContextXTRA ligneContext) {
+    if (ligneContext.isPresenceDateDepotPc() && ligneContext.isPresenceDateDepotPcJustificatif()) {
+        if (ligneContext.isPresenceDpe() && ligneContext.isPresenceDpeJustificatif()) {
+            return alignForDateDepotAndDpe(ligneContext);
+        } else {
+            return alignForDateDepotWithoutDpe(ligneContext);
+        }
+    } else {
+        return alignForOldBuild(ligneContext);
+    }
+}
+
+private String alignForDateDepotAndDpe(ContextXTRA ligneContext) {
     try {
-        initializeDates();
-        // Extract other initializations
-        
-        String typeObjetFinancement = ligneContext.getTypeObjetFinancement();
-        
-        if (typeObjetFinancement == null || !isAcquisition(typeObjetFinancement)) {
-            return "07";
+        Date dateDepotPc = ligneContext.getDateDepotPc();
+        if (dateDepotPc.compareTo(endDate) <= 0 && dateDepotPc.compareTo(startDate) > 0) {
+            CalculAlignementStrategy calculAlignementFirstStrategy = new CalculAlignementStrategy();
+            return calculAlignementFirstStrategy.aligneDpeCep(
+                ligneContext.getEtiquetteDpe(),
+                ligneContext.getValeurCep(),
+                obtenirValeurCepTop(ligneContext.getCodeBatiment())
+            );
+        } else if (dateDepotPc.compareTo(formatDate.parse("31/12/2020")) <= 0) {
+            CalculAlignementStrategy calculAlignementSecondStrategy = new CalculAlignementStrategy();
+            return calculAlignementSecondStrategy.alignCepDpeEtNormeTh(
+                ligneContext.getEtiquetteDpe(),
+                ligneContext.getValeurCep(),
+                obtenirValeurCepTop(ligneContext.getCodeBatiment()),
+                ligneContext.getNormeThermique(),
+                endDate
+            );
+        } else if (dateDepotPc.compareTo(formatDate.parse("31/12/2021")) <= 0) {
+            CalculAlignementStrategy calculAlignementThirdStrategy = new CalculAlignementStrategy();
+            return calculAlignementThirdStrategy.aligneCepCepmax(
+                ligneContext.getValeurCep(),
+                obtenirValeurCepMax(ligneContext.getCodeBatiment())
+            );
+        } else {
+            return "01";
         }
-        
-        if (isAcquisitionInNewBuild(ligneContext)) {
-            return alignForAcquisitionInNewBuild(ligneContext);
-        }
-        
-        if (isAcquisitionInOldBuild(ligneContext)) {
-            return alignForAcquisitionInOldBuild(ligneContext);
-        }
-        
-        return "07"; // Default case
-    } catch (Exception e) {
+    } catch (ParseException e) {
         commonLogger.eventTyp(EventTyp.APPLICATIVE).secEventTyp(SecEventTyp.METIER).logger().info("Calcul : update error");
         return "07";
     }
 }
 
-private void initializeDates() throws ParseException {
-    startDate = formatDate.parse("01/01/1700");
-    endDate = formatDate.parse("31/12/2012");
-    anneeDebutConstruction = 1700;
-    anneeFinConstruction = 2012;
-    // Add other date initializations if necessary
+private String alignForDateDepotWithoutDpe(ContextXTRA ligneContext) {
+    try {
+        Date dateDepotPc = ligneContext.getDateDepotPc();
+        if (dateDepotPc.compareTo(startDate) >= 0) {
+            return "01";
+        } else {
+            return "07";
+        }
+    } catch (ParseException e) {
+        commonLogger.eventTyp(EventTyp.APPLICATIVE).secEventTyp(SecEventTyp.METIER).logger().info("Calcul : update error");
+        return "07";
+    }
 }
 
-private boolean isAcquisition(String typeObjetFinancement) {
-    return typeObjetFinancement.equals("02");
-}
-
-private boolean isAcquisitionInNewBuild(ContextXTRA ligneContext) {
-    // Check conditions for acquisition in new build
-    // Return true or false
-}
-
-private String alignForAcquisitionInNewBuild(ContextXTRA ligneContext) {
-    // Perform alignment calculation for acquisition in new build
-    // Return the alignment code
-}
-
-private boolean isAcquisitionInOldBuild(ContextXTRA ligneContext) {
-    // Check conditions for acquisition in old build
-    // Return true or false
-}
-
-private String alignForAcquisitionInOldBuild(ContextXTRA ligneContext) {
-    // Perform alignment calculation for acquisition in old build
-    // Return the alignment code
+private String alignForOldBuild(ContextXTRA ligneContext) {
+    try {
+        Date dateDepotPc = ligneContext.getDateDepotPc();
+        if (!ligneContext.isPresenceDateDepotPc() || !ligneContext.isPresenceDateDepotPcJustificatif()) {
+            if (dateDepotPc != null && dateDepotPc.compareTo(endDate) > 0) {
+                return "07";
+            }
+        }
+        // Add logic for old build alignment
+        return "07";
+    } catch (ParseException e) {
+        commonLogger.eventTyp(EventTyp.APPLICATIVE).secEventTyp(SecEventTyp.METIER).logger().info("Calcul : update error");
+        return "07";
+    }
 }
