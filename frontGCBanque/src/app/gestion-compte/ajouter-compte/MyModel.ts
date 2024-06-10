1756550c6bdaceb9827cc9239608f8c65497716f
ziedenit <guendil_zied@yahoo.com>
2024-06-10 23:26:26 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,37 +1,185 @@
-private void mergeList(Field field, Object target, Object source) throws Exception {
-        List<?> sourceList = (List<?>) field.get(source);
-        List<?> targetList = (List<?>) field.get(target);
+<ErrorResponse>
+    <code>TECHNICAL_EXCEPTION_OCCURED </code>
+    <message>
+        <message>Unable to make field private static final long java.util.ArrayList.serialVersionUID accessible: module java.base does not "opens java.util" to unnamed module @43a0cee9</message>
+    </message>
+</ErrorResponse>
 
-        if (sourceList == null || sourceList.isEmpty()) {
-            return;
+Lorsque je patch cet objet
+{
+    "idFinancement": "FFVRILZ5L",
+    "objetFinancement": [
+        {
+            "idObjetFinancement": "OBAQ0O5CU",
+            "codeObjetFinancement": "0203",
+            "quotePartObjet": 100.0,
+            "codeFamilleObjet": null,
+            "gainCEP": 25.0,
+            "dateFinTravaux": null,
+            "bien": {
+                "idBien": null,
+                "codeBatiment": null,
+                "typeBatiment": "maison",
+                "numeroVoie": null,
+                "typeVoie": null,
+                "nomRue": null,
+                "batiment": null,
+                "escalier": null,
+                "etage": null,
+                "porte": null,
+                "codePostal": "91300",
+                "nomCommune": null,
+                "paysBien": null,
+                "adresseComplete": "73 rue gutenberg, 91300 Massy",
+                "codeNormeThermique": null,
+                "etatBien": null,
+                "typeEnergie": null,
+                "codeTypeEnergie": null,
+                "codeDepartement": null,
+                "codeInseeCommune": null,
+                "numeroLot": null,
+                "numeroNomRue": null,
+                "typeUsage": null,
+                "anneeConstruction": null,
+                "periodeConstruction": null,
+                "dateDepotPc": null,
+                "dateDebutConstruction": null,
+                "surfaceBien": null,
+                "bienFinanceLCL": false,
+                "numeroFiscalLocal": null,
+                "eligibleDpe": null,
+                "labelBien": null,
+                "coordonneeCartographiqueX": null,
+                "coordonneeCartographiqueY": null,
+                "prixBien": null,
+                "montantFinanceLCL": 10000.0,
+                "partLCL": null,
+                "dpeActuel": {
+                    "idDpe": null,
+                    "numeroDpe": "2156E1018488U2",
+                    "codeModeleDpeType": null,
+                    "estimationCep": 100.0,
+                    "classeCep": "A",
+                    "estimationGes": 42.0,
+                    "classeGes": "C",
+                    "dateEtablissementDpe": "2017-02-06T00:00:00.000+00:00",
+                    "dateReceptionDpe": null,
+                    "dateFinValiditeDpe": null,
+                    "sirenDiagnostiqueur": null,
+                    "modelDpe": null,
+                    "numeroDpeRemplace": null,
+                    "versionDpe": null,
+                    "methodeDpeApplique": null
+                }
+            },
+            "dpeAvantTravaux": null,
+            "dpeApresTravaux": null,
+            "alignement": null,
+            "eligibilite": null,
+            "garantie": null,
+            "piecesJustificatives": null,
+            "statut": 0
+        },
+        {
+            "idObjetFinancement": "OY8IUIQS8",
+            "codeObjetFinancement": "0203",
+            "quotePartObjet": 100.0,
+            "codeFamilleObjet": null,
+            "gainCEP": 25.0,
+            "dateFinTravaux": null,
+            "bien": {
+                "idBien": null,
+                "codeBatiment": null,
+                "typeBatiment": "maison",
+                "numeroVoie": null,
+                "typeVoie": null,
+                "nomRue": null,
+                "batiment": null,
+                "escalier": null,
+                "etage": null,
+                "porte": null,
+                "codePostal": "91300",
+                "nomCommune": null,
+                "paysBien": null,
+                "adresseComplete": "73 rue gutenberg, 91300 Massy",
+                "codeNormeThermique": null,
+                "etatBien": null,
+                "typeEnergie": null,
+                "codeTypeEnergie": null,
+                "codeDepartement": null,
+                "codeInseeCommune": null,
+                "numeroLot": null,
+                "numeroNomRue": null,
+                "typeUsage": null,
+                "anneeConstruction": null,
+                "periodeConstruction": null,
+                "dateDepotPc": null,
+                "dateDebutConstruction": null,
+                "surfaceBien": null,
+                "bienFinanceLCL": false,
+                "numeroFiscalLocal": null,
+                "eligibleDpe": null,
+                "labelBien": null,
+                "coordonneeCartographiqueX": null,
+                "coordonneeCartographiqueY": null,
+                "prixBien": null,
+                "montantFinanceLCL": 10000.0,
+                "partLCL": null,
+                "dpeActuel": {
+                    "idDpe": null,
+                    "numeroDpe": "2156E1018488U2",
+                    "codeModeleDpeType": null,
+                    "estimationCep": 100.0,
+                    "classeCep": "B",
+                    "estimationGes": 42.0,
+                    "classeGes": "C",
+                    "dateEtablissementDpe": "2017-02-06T00:00:00.000+00:00",
+                    "dateReceptionDpe": null,
+                    "dateFinValiditeDpe": null,
+                    "sirenDiagnostiqueur": null,
+                    "modelDpe": null,
+                    "numeroDpeRemplace": null,
+                    "versionDpe": null,
+                    "methodeDpeApplique": null
+                }
+            },
+            "dpeAvantTravaux": null,
+            "dpeApresTravaux": null,
+            "alignement": null,
+            "eligibilite": null,
+            "garantie": null,
+            "piecesJustificatives": null,
+            "statut": 0
         }
+    ],
+    "alignement": null,
+    "eligibilite": null,
+    "intervenant": {
+        "idIntervenant": null,
+        "idReper": [
+            "1559518855"
+        ]
+    },
+    "indicateurFinancementDedie": "Y",
+    "indicateurNatureDurable": "Z",
+    "typeRisqueClimatiqueAttenue": null,
+    "codeApplicatifOrigine": "01",
+    "indicateurReprise": false,
+    "statut": 0,
+    "agenceCompte": "1234567890000000",
+    "valeurTravaux": null
+}
+par ca 
+{"objetFinancement": [
+        {
+            "idObjetFinancement": "OBAQ0O5CU",
+            "codeObjetFinancement": "02",
+            "quotePartObjet": 30.0
+        },
 
-        if (targetList == null) {
-            field.set(target, new ArrayList<>(sourceList));
-            return;
+    {
+            "idObjetFinancement": "OY8IUIQS8",
+            "codeObjetFinancement": "02",
+            "quotePartObjet": 60.0
         }
-
-        for (Object sourceItem : sourceList) {
-            Object sourceId = getId(sourceItem);
-            Optional<?> targetItemOpt = targetList.stream()
-                    .filter(item -> {
-                        try {
-                            return Objects.equals(getId(item), sourceId);
-                        } catch (Exception e) {
-                            throw new RuntimeException(e);
-                        }
-                    })
-                    .findFirst();
-
-            if (targetItemOpt.isPresent()) {
-                merge(targetItemOpt.get(), sourceItem);
-            } else {
-                targetList.add(sourceItem);
-            }
-        }
-    }
- targetList.add(sourceItem);
-Required type:
-capture of ?
-Provided:
-Object
+]}
