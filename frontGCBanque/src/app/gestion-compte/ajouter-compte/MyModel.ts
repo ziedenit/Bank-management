f2e20c59dc5374ea16f3657fc0c6c16ffd26dd5a
ziedenit <guendil_zied@yahoo.com>
2024-06-10 23:27:44 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,185 +1,111 @@
-<ErrorResponse>
-    <code>TECHNICAL_EXCEPTION_OCCURED </code>
-    <message>
-        <message>Unable to make field private static final long java.util.ArrayList.serialVersionUID accessible: module java.base does not "opens java.util" to unnamed module @43a0cee9</message>
-    </message>
-</ErrorResponse>
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
-Lorsque je patch cet objet
-{
-    "idFinancement": "FFVRILZ5L",
-    "objetFinancement": [
-        {
-            "idObjetFinancement": "OBAQ0O5CU",
-            "codeObjetFinancement": "0203",
-            "quotePartObjet": 100.0,
-            "codeFamilleObjet": null,
-            "gainCEP": 25.0,
-            "dateFinTravaux": null,
-            "bien": {
-                "idBien": null,
-                "codeBatiment": null,
-                "typeBatiment": "maison",
-                "numeroVoie": null,
-                "typeVoie": null,
-                "nomRue": null,
-                "batiment": null,
-                "escalier": null,
-                "etage": null,
-                "porte": null,
-                "codePostal": "91300",
-                "nomCommune": null,
-                "paysBien": null,
-                "adresseComplete": "73 rue gutenberg, 91300 Massy",
-                "codeNormeThermique": null,
-                "etatBien": null,
-                "typeEnergie": null,
-                "codeTypeEnergie": null,
-                "codeDepartement": null,
-                "codeInseeCommune": null,
-                "numeroLot": null,
-                "numeroNomRue": null,
-                "typeUsage": null,
-                "anneeConstruction": null,
-                "periodeConstruction": null,
-                "dateDepotPc": null,
-                "dateDebutConstruction": null,
-                "surfaceBien": null,
-                "bienFinanceLCL": false,
-                "numeroFiscalLocal": null,
-                "eligibleDpe": null,
-                "labelBien": null,
-                "coordonneeCartographiqueX": null,
-                "coordonneeCartographiqueY": null,
-                "prixBien": null,
-                "montantFinanceLCL": 10000.0,
-                "partLCL": null,
-                "dpeActuel": {
-                    "idDpe": null,
-                    "numeroDpe": "2156E1018488U2",
-                    "codeModeleDpeType": null,
-                    "estimationCep": 100.0,
-                    "classeCep": "A",
-                    "estimationGes": 42.0,
-                    "classeGes": "C",
-                    "dateEtablissementDpe": "2017-02-06T00:00:00.000+00:00",
-                    "dateReceptionDpe": null,
-                    "dateFinValiditeDpe": null,
-                    "sirenDiagnostiqueur": null,
-                    "modelDpe": null,
-                    "numeroDpeRemplace": null,
-                    "versionDpe": null,
-                    "methodeDpeApplique": null
+public class FinancementService {
+
+    private FinancementRepository financementRepository;
+
+    public Financement patchFinancementChamps(String idFinancement, Financement financementToUpdate) throws Exception {
+        Financement existingFinancement = financementRepository.findByidFinancement(idFinancement)
+                .orElseThrow(() -> new FinancementNotFoundException(String.format("Le financement à modifier %s est inexistant", idFinancement)));
+
+        merge(existingFinancement, financementToUpdate);
+
+        return financementRepository.save(existingFinancement);
+    }
+
+    private void merge(Object target, Object source) throws Exception {
+        Field[] fields = source.getClass().getDeclaredFields();
+        List<String> bienFields = Arrays.asList(
+                "codeBatiment", "typeBatiment", "numeroVoie", "typeVoie", "nomRue", "batiment", "escalier", "etage", "porte", "codePostal", "nomCommune",
+                "paysBien", "adresseComplete", "codeNormeThermique", "etatBien", "typeEnergie", "codeTypeEnergie", "codeDepartement", "codeInseeCommune",
+                "numeroLot", "numeroNomRue", "typeUsage", "anneeConstruction", "periodeConstruction", "dateDepotPc", "dateDebutConstruction", "surfaceBien",
+                "numeroFiscalLocal", "coordonneeCartographiqueX", "coordonneeCartographiqueY", "prixBien", "montantFinanceLCL", "partLCL"
+        );
+
+        List<String> dpeFields = Arrays.asList(
+                "numeroDpe", "estimationCep", "classeCep", "estimationGes", "classeGes", "dateEtablissementDpe",
+                "dateReceptionDpe", "dateFinValiditeDpe", "sirenDiagnostiqueur"
+        );
+
+        for (Field field : fields) {
+            field.setAccessible(true);
+            try {
+                Object value = field.get(source);
+                if (value != null) {
+                    if (isComplexObject(field.getType())) {
+                        Object targetValue = field.get(target);
+                        if (targetValue == null) {
+                            field.set(target, value);
+                        } else {
+                            merge(targetValue, value);
+                        }
+                    } else if (List.class.isAssignableFrom(field.getType())) {
+                        mergeList(field, target, source);
+                    } else {
+                        field.set(target, value);
+                    }
+                } else {
+                    if (field.getType().equals(Date.class) || bienFields.contains(field.getName()) || dpeFields.contains(field.getName())) {
+                        field.set(target, null);
+                    }
                 }
-            },
-            "dpeAvantTravaux": null,
-            "dpeApresTravaux": null,
-            "alignement": null,
-            "eligibilite": null,
-            "garantie": null,
-            "piecesJustificatives": null,
-            "statut": 0
-        },
-        {
-            "idObjetFinancement": "OY8IUIQS8",
-            "codeObjetFinancement": "0203",
-            "quotePartObjet": 100.0,
-            "codeFamilleObjet": null,
-            "gainCEP": 25.0,
-            "dateFinTravaux": null,
-            "bien": {
-                "idBien": null,
-                "codeBatiment": null,
-                "typeBatiment": "maison",
-                "numeroVoie": null,
-                "typeVoie": null,
-                "nomRue": null,
-                "batiment": null,
-                "escalier": null,
-                "etage": null,
-                "porte": null,
-                "codePostal": "91300",
-                "nomCommune": null,
-                "paysBien": null,
-                "adresseComplete": "73 rue gutenberg, 91300 Massy",
-                "codeNormeThermique": null,
-                "etatBien": null,
-                "typeEnergie": null,
-                "codeTypeEnergie": null,
-                "codeDepartement": null,
-                "codeInseeCommune": null,
-                "numeroLot": null,
-                "numeroNomRue": null,
-                "typeUsage": null,
-                "anneeConstruction": null,
-                "periodeConstruction": null,
-                "dateDepotPc": null,
-                "dateDebutConstruction": null,
-                "surfaceBien": null,
-                "bienFinanceLCL": false,
-                "numeroFiscalLocal": null,
-                "eligibleDpe": null,
-                "labelBien": null,
-                "coordonneeCartographiqueX": null,
-                "coordonneeCartographiqueY": null,
-                "prixBien": null,
-                "montantFinanceLCL": 10000.0,
-                "partLCL": null,
-                "dpeActuel": {
-                    "idDpe": null,
-                    "numeroDpe": "2156E1018488U2",
-                    "codeModeleDpeType": null,
-                    "estimationCep": 100.0,
-                    "classeCep": "B",
-                    "estimationGes": 42.0,
-                    "classeGes": "C",
-                    "dateEtablissementDpe": "2017-02-06T00:00:00.000+00:00",
-                    "dateReceptionDpe": null,
-                    "dateFinValiditeDpe": null,
-                    "sirenDiagnostiqueur": null,
-                    "modelDpe": null,
-                    "numeroDpeRemplace": null,
-                    "versionDpe": null,
-                    "methodeDpeApplique": null
-                }
-            },
-            "dpeAvantTravaux": null,
-            "dpeApresTravaux": null,
-            "alignement": null,
-            "eligibilite": null,
-            "garantie": null,
-            "piecesJustificatives": null,
-            "statut": 0
+            } catch (IllegalAccessException e) {
+                throw new Exception("Failed to merge field: " + e.getMessage());
+            }
         }
-    ],
-    "alignement": null,
-    "eligibilite": null,
-    "intervenant": {
-        "idIntervenant": null,
-        "idReper": [
-            "1559518855"
-        ]
-    },
-    "indicateurFinancementDedie": "Y",
-    "indicateurNatureDurable": "Z",
-    "typeRisqueClimatiqueAttenue": null,
-    "codeApplicatifOrigine": "01",
-    "indicateurReprise": false,
-    "statut": 0,
-    "agenceCompte": "1234567890000000",
-    "valeurTravaux": null
+    }
+
+    private void mergeList(Field field, Object target, Object source) throws Exception {
+        List<?> sourceList = (List<?>) field.get(source);
+        List<?> targetList = (List<?>) field.get(target);
+
+        if (sourceList == null || sourceList.isEmpty()) {
+            return;
+        }
+
+        if (targetList == null) {
+            targetList = new ArrayList<>();
+            field.set(target, targetList);
+        }
+
+        for (Object sourceItem : sourceList) {
+            Object sourceId = getId(sourceItem);
+            Optional<?> targetItemOpt = targetList.stream()
+                    .filter(item -> {
+                        try {
+                            return Objects.equals(getId(item), sourceId);
+                        } catch (Exception e) {
+                            throw new RuntimeException(e);
+                        }
+                    })
+                    .findFirst();
+
+            if (targetItemOpt.isPresent()) {
+                merge(targetItemOpt.get(), sourceItem);
+            } else {
+                ((List<Object>) targetList).add(sourceItem);
+            }
+        }
+    }
+
+    private Object getId(Object object) throws Exception {
+        Field idField = Arrays.stream(object.getClass().getDeclaredFields())
+                .filter(field -> field.getName().startsWith("id"))
+                .findFirst()
+                .orElseThrow(() -> new Exception("No id field found in object of type " + object.getClass().getSimpleName()));
+
+        idField.setAccessible(true);
+        return idField.get(object);
+    }
+
+    private boolean isComplexObject(Class<?> clazz) {
+        return !clazz.isPrimitive() && !String.class.isAssignableFrom(clazz) && !Number.class.isAssignableFrom(clazz)
+                && !Boolean.class.isAssignableFrom(clazz) && !Date.class.isAssignableFrom(clazz)
+                && !clazz.isEnum();
+    }
 }
-par ca 
-{"objetFinancement": [
-        {
-            "idObjetFinancement": "OBAQ0O5CU",
-            "codeObjetFinancement": "02",
-            "quotePartObjet": 30.0
-        },
-
-    {
-            "idObjetFinancement": "OY8IUIQS8",
-            "codeObjetFinancement": "02",
-            "quotePartObjet": 60.0
-        }
-]}
