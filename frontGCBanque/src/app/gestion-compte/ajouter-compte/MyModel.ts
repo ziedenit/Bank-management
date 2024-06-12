db1c2d33363ee3417e9f76602090945a8a82028a
ziedenit <guendil_zied@yahoo.com>
2024-06-12 09:59:38 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,39 +1,58 @@
 public Financement patchFinancementChamps(String idFinancement, Financement financementToUpdate) throws Exception {
-    // Récupérer le financement existant
-    Financement existingFinancement = financementRepository.findById(idFinancement)
-            .orElseThrow(() -> new FinancementNotFoundException(String.format("Le financement à modifier %s est inexistant", idFinancement)));
+        // Récupère le financement existant
+        Financement existingFinancement = financementRepository.findByidFinancement(idFinancement)
+                .orElseThrow(() -> new FinancementNotFoundException(String.format("Le financement à modifier %s est inexistant", idFinancement)));
 
-    // Log l'état initial
-    System.out.println("Financement initial: " + existingFinancement);
-
-    // Mettre à jour les objets de financement
-    if (financementToUpdate.getObjetFinancement() != null) {
-        for (ObjetFinancement updatedObjet : financementToUpdate.getObjetFinancement()) {
-            boolean found = false;
-            for (ObjetFinancement existingObjet : existingFinancement.getObjetFinancement()) {
-                if (existingObjet.getIdObjetFinancement().equals(updatedObjet.getIdObjetFinancement())) {
-                    merge(existingObjet, updatedObjet);
-                    found = true;
-                    break;
+        // Mise à jour les objets de financement
+        if (financementToUpdate.getObjetFinancement() != null) {
+            for (ObjetFinancement updatedObjet : financementToUpdate.getObjetFinancement()) {
+                boolean found = false;
+                for (int i = 0; i < existingFinancement.getObjetFinancement().size(); i++) {
+                    ObjetFinancement existingObjet = existingFinancement.getObjetFinancement().get(i);
+                    if (existingObjet.getIdObjetFinancement().equals(updatedObjet.getIdObjetFinancement())) {
+                        merge(existingObjet, updatedObjet);
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) {
+                    existingFinancement.getObjetFinancement().add(updatedObjet);
                 }
             }
-            if (!found) {
-                existingFinancement.getObjetFinancement().add(updatedObjet);
+        }
+
+        // Mise à jour les autres champs de financement
+        mergeNonListFields(existingFinancement, financementToUpdate);
+
+       // Sauvegarder le financement mis à jour
+       Financement savedFinancement = financementRepository.save(existingFinancement);
+
+        // Supprimer l'ancien financement
+       financementRepository.deleteByIdFinancement(existingFinancement.getIdFinancement());
+
+        return savedFinancement;
+    }
+
+    private void merge(Object target, Object source) throws Exception {
+        Field[] fields = source.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            field.setAccessible(true);
+            Object value = field.get(source);
+            if (value != null) {
+                field.set(target, value);
             }
         }
     }
 
-    // Mettre à jour les autres champs du financement
-    mergeNonListFields(existingFinancement, financementToUpdate);
-
-    // Log l'état final avant la sauvegarde
-    System.out.println("Financement mis à jour: " + existingFinancement);
-
-    // Sauvegarder le financement mis à jour
-    Financement savedFinancement = financementRepository.save(existingFinancement);
-
-    // Log l'état final après la sauvegarde
-    System.out.println("Financement sauvegardé: " + savedFinancement);
-
-    return savedFinancement;
-}
+    private void mergeNonListFields(Object target, Object source) throws Exception {
+        Field[] fields = source.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            if (!field.getName().equals("objetFinancement")) { // Ignore the objetFinancement field
+                field.setAccessible(true);
+                Object value = field.get(source);
+                if (value != null) {
+                    field.set(target, value);
+                }
+            }
+        }
+    }
