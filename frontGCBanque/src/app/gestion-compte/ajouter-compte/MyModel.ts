8fc5abbea32c819def4965a6fb2aa80c16447d81
ziedenit <guendil_zied@yahoo.com>
2024-06-12 20:57:34 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,1 +1,8 @@
-Class 'MongoItemWriter' must either be declared abstract or implement abstract method 'write(Chunk<? extends T>)' in 'ItemWriter'
+Required type:
+ItemWriter
+<? super Rule>
+Provided:
+ItemWriter
+<Chunk<? extends Rule>>
+
+  on step1 
