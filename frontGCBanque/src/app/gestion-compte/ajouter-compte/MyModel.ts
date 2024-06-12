dcf8121f79ccc86cee85ed6af82dbf78278e3a10
ziedenit <guendil_zied@yahoo.com>
2024-06-12 20:52:58 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,33 +1,39 @@
 import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemProcessor;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Iterator;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-public class ExcelItemReader implements ItemReader<Row> {
+public class RuleItemProcessor implements ItemProcessor<Row, Rule> {
 
-    private Iterator<Row> rowIterator;
-
-    public ExcelItemReader(String filePath) throws IOException {
-        FileInputStream fis = new FileInputStream(filePath);
-        Workbook workbook = new XSSFWorkbook(fis);
-        Sheet sheet = workbook.getSheetAt(0);
-        this.rowIterator = sheet.iterator();
-        // Skip header row
-        if (this.rowIterator.hasNext()) {
-            this.rowIterator.next();
-        }
-    }
+    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd/MM/yyyy");
 
     @Override
-    public Row read() {
-        if (this.rowIterator != null && this.rowIterator.hasNext()) {
-            return this.rowIterator.next();
-        } else {
+    public Rule process(Row row) throws ParseException {
+        Rule rule = new Rule();
+
+        rule.setEligibleDPE("Autre bien immobilier".equals(row.getCell(0).getStringCellValue()));
+        rule.setDateDepotPc(parseDate(row.getCell(1).getStringCellValue()));
+        rule.setPresenceDpe("oui".equalsIgnoreCase(row.getCell(2).getStringCellValue()));
+        rule.setPresenceDpeJustificatif("oui".equalsIgnoreCase(row.getCell(3).getStringCellValue()));
+        rule.setEtiquetteDpe(row.getCell(4).getStringCellValue());
+        rule.setValeurCep(row.getCell(5).getNumericCellValue());
+        rule.setNormeThermique(row.getCell(6).getStringCellValue());
+        rule.setPresenceNormeThermiqueJustificatif("oui".equalsIgnoreCase(row.getCell(7).getStringCellValue()));
+        rule.setXtra248(row.getCell(8).getStringCellValue());
+        rule.setXtra249(row.getCell(9).getStringCellValue());
+        rule.setXtra250(row.getCell(10).getStringCellValue());
+        rule.setXtra251(row.getCell(11).getStringCellValue());
+        rule.setXtra275(row.getCell(12).getStringCellValue());
+
+        return rule;
+    }
+
+    private Date parseDate(String dateStr) {
+        try {
+            return DATE_FORMAT.parse(dateStr);
+        } catch (ParseException e) {
             return null;
         }
     }
