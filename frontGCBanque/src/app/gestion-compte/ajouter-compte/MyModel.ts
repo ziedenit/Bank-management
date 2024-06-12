c8dc5053a8535c7f4f945999ccfb9310305ca2c1
ziedenit <guendil_zied@yahoo.com>
2024-06-12 20:55:36 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,15 +1,1 @@
-import org.springframework.batch.item.Chunk;
-import org.springframework.batch.item.ItemWriter;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoTemplate;
-
-public class MongoItemWriter implements ItemWriter<Chunk<? extends Rule>> {
-
-    @Autowired
-    private MongoTemplate mongoTemplate;
-
-    @Override
-    public void write(Chunk<? extends Rule> items) {
-        mongoTemplate.insertAll(items.getItems());
-    }
-}
+Class 'MongoItemWriter' must either be declared abstract or implement abstract method 'write(Chunk<? extends T>)' in 'ItemWriter'
