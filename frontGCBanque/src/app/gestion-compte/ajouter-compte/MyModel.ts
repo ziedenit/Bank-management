182a26cc201e0eaf5cfdbf9251321c45c80185d6
ziedenit <guendil_zied@yahoo.com>
2024-06-12 21:24:07 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,5 +1,30 @@
-Caused by: org.h2.jdbc.JdbcSQLSyntaxErrorException: Table "BATCH_JOB_INSTANCE" not found (this database is empty); SQL statement:
-SELECT JOB_INSTANCE_ID, JOB_NAME
-FROM BATCH_JOB_INSTANCE
-WHERE JOB_NAME = ?
- and JOB_KEY = ? [42104-224]
+package com.cl.msofd.utility;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+import java.util.Date;
+@Getter
+@Setter
+public class Rule implements Serializable {
+    @Id
+    private String id;
+    private boolean eligibleDPE;
+    private Date dateDepotPc;
+    private boolean presenceDpe;
+    private boolean presenceDpeJustificatif;
+    private String etiquetteDpe;
+    private double valeurCep;
+    private String normeThermique;
+    private boolean presenceNormeThermiqueJustificatif;
+    private String Xtra248;
+    private String Xtra249;
+    private String Xtra250;
+    private String Xtra251;
+    private String Xtra275;
+
+
+}
