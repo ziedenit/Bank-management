9a9ffcd6e146d502f0c16d03fe5b0cc7a0513cd6
ziedenit <guendil_zied@yahoo.com>
2024-06-12 20:41:37 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,40 +1,15 @@
-import org.apache.poi.ss.usermodel.Row;
-import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.Chunk;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+public class MongoItemWriter implements ItemWriter<Chunk<? extends Rule>> {
 
-public class RuleItemProcessor implements ItemProcessor<Row, Rule> {
-
-    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd/MM/yyyy");
+    @Autowired
+    private MongoTemplate mongoTemplate;
 
     @Override
-    public Rule process(Row row) throws ParseException {
-        Rule rule = new Rule();
-
-        rule.setEligibleDPE("Autre bien immobilier".equals(row.getCell(0).getStringCellValue()));
-        rule.setDateDepotPc(parseDate(row.getCell(1).getStringCellValue()));
-        rule.setPresenceDpe("oui".equalsIgnoreCase(row.getCell(2).getStringCellValue()));
-        rule.setPresenceDpeJustificatif("oui".equalsIgnoreCase(row.getCell(3).getStringCellValue()));
-        rule.setEtiquetteDpe(row.getCell(4).getStringCellValue());
-        rule.setValeurCep(row.getCell(5).getNumericCellValue());
-        rule.setNormeThermique(row.getCell(6).getStringCellValue());
-        rule.setPresenceNormeThermiqueJustificatif("oui".equalsIgnoreCase(row.getCell(7).getStringCellValue()));
-        rule.setXtra248(row.getCell(8).getStringCellValue());
-        rule.setXtra249(row.getCell(9).getStringCellValue());
-        rule.setXtra250(row.getCell(10).getStringCellValue());
-        rule.setXtra251(row.getCell(11).getStringCellValue());
-        rule.setXtra275(row.getCell(12).getStringCellValue());
-
-        return rule;
-    }
-
-    private Date parseDate(String dateStr) {
-        try {
-            return DATE_FORMAT.parse(dateStr);
-        } catch (ParseException e) {
-            return null;
-        }
+    public void write(Chunk<? extends Rule> items) {
+        mongoTemplate.insertAll(items.getItems());
     }
 }
