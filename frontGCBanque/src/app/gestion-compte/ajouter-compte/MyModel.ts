9da761e803402d6eda092b1d92017b717ae50703
ziedenit <guendil_zied@yahoo.com>
2024-06-12 21:03:43 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,14 +1,24 @@
-@Autowired
+@SpringBootApplication
+public class MsofdApplication extends SpringBootServletInitializer {
+    @Autowired
     private JobLauncher jobLauncher;
+    public static void main(String[] args) {
+        SpringApplication.run(MsofdApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MsofdApplication.class);
+    }
 
     @Autowired
     private Job job;
-
-    public static void main(String[] args) {
-        SpringApplication.run(BatchApplication.class, args);
-    }
 
     @Override
     public void run(String... args) throws Exception {
         jobLauncher.run(job, new JobParametersBuilder().toJobParameters());
     }
+
+}
+c'est la classe boot de mon appli mais j'ai ce probleme 
+Method does not override method from its superclass pour la méthode run
