2be5d0ed4a1a85539e0b52d42efa48738eade6c3
ziedenit <guendil_zied@yahoo.com>
2024-06-12 15:32:04 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,58 +1,25 @@
-public Financement patchFinancementChamps(String idFinancement, Financement financementToUpdate) throws Exception {
-        // Récupère le financement existant
-        Financement existingFinancement = financementRepository.findByidFinancement(idFinancement)
-                .orElseThrow(() -> new FinancementNotFoundException(String.format("Le financement à modifier %s est inexistant", idFinancement)));
 
-        // Mise à jour les objets de financement
-        if (financementToUpdate.getObjetFinancement() != null) {
-            for (ObjetFinancement updatedObjet : financementToUpdate.getObjetFinancement()) {
-                boolean found = false;
-                for (int i = 0; i < existingFinancement.getObjetFinancement().size(); i++) {
-                    ObjetFinancement existingObjet = existingFinancement.getObjetFinancement().get(i);
-                    if (existingObjet.getIdObjetFinancement().equals(updatedObjet.getIdObjetFinancement())) {
-                        merge(existingObjet, updatedObjet);
-                        found = true;
-                        break;
-                    }
-                }
-                if (!found) {
-                    existingFinancement.getObjetFinancement().add(updatedObjet);
-                }
-            }
-        }
+    //Acquisition
+    private String typeObjetFinancement;
+    private String codeBatiment;
 
-        // Mise à jour les autres champs de financement
-        mergeNonListFields(existingFinancement, financementToUpdate);
+    private boolean presenceDateDepotPc;
+    private boolean presenceDateDepotPcJustificatif;
+    private Date dateDepotPc;
 
-       // Sauvegarder le financement mis à jour
-       Financement savedFinancement = financementRepository.save(existingFinancement);
+    private boolean presenceDpe;
+    private boolean presenceDpeJustificatif;
 
-        // Supprimer l'ancien financement
-       financementRepository.deleteByIdFinancement(existingFinancement.getIdFinancement());
+    private boolean presenceNormeThermique;
+    private String normeThermique;
+    private boolean presenceNormeThermiqueJustificatif;
 
-        return savedFinancement;
-    }
+    private Integer anneeConstruction;
+    private String etiquetteDpe;
+    private double valeurCep;
 
-    private void merge(Object target, Object source) throws Exception {
-        Field[] fields = source.getClass().getDeclaredFields();
-        for (Field field : fields) {
-            field.setAccessible(true);
-            Object value = field.get(source);
-            if (value != null) {
-                field.set(target, value);
-            }
-        }
-    }
 
-    private void mergeNonListFields(Object target, Object source) throws Exception {
-        Field[] fields = source.getClass().getDeclaredFields();
-        for (Field field : fields) {
-            if (!field.getName().equals("objetFinancement")) { // Ignore the objetFinancement field
-                field.setAccessible(true);
-                Object value = field.get(source);
-                if (value != null) {
-                    field.set(target, value);
-                }
-            }
-        }
-    }
+    //Rénovation
+    private boolean presenceDpeAvantTravaux;
+    private boolean presenceDpeApresTravaux;
+    private Double gainCep;
