e3593b874566e6af37f56a6909348908eff9e621
ziedenit <guendil_zied@yahoo.com>
2024-06-14 08:14:10 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,81 +1,62 @@
-<div class="container-fluid">
-  <div class="row">
-    <div class="col-12">
-      <div class="ajoutFinancement">
-        <button mat-stroked-button (click)="ajouterObjetFinancement()">
-          <img src="../../../assets/icons/plus.svg" />
-          &nbsp; ajouter un objet de financement
-        </button>
-      </div>
-    </div>
-  </div>
+import { Component, OnInit } from '@angular/core';
+import { FinancementService } from './financement.service';
+import { ObjetFinancement } from './objet-financement.model';
 
-  <div class="breadcrumb-container">
-    <div class="breadcrumb-item" *ngFor="let objet of objetsDeFinancement; let i = index" (click)="selectObjetFinancement(objet)">
-      <div class="breadcrumb-content">
-        <h3 class="d-inline-block font-weight-bold">Objet {{i + 1}}</h3>
-      </div>
-      <div *ngIf="selectedObjetFinancement === objet && !hiddenObjetfinancement" class="details">
-        <div class="row">
-          <!-- Afficher les champs de selectedObjetFinancement -->
-          <div class="col-lg-4">
-            <div class="form-group">
-              <label for="codeObjetFinancement{{i}}">Code Objet Financement</label>
-              <input type="text" class="form-control form-control-sm" [(ngModel)]="selectedObjetFinancement.codeObjetFinancement" id="codeObjetFinancement{{i}}">
-            </div>
-          </div>
-          <div class="col-lg-4">
-            <div class="form-group">
-              <label for="quotePartObjet{{i}}">Quote Part Objet</label>
-              <input type="number" class="form-control form-control-sm" [(ngModel)]="selectedObjetFinancement.quotePartObjet" id="quotePartObjet{{i}}">
-            </div>
-          </div>
-          <!-- Ajoutez les autres champs du modèle ici -->
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-	  //
-	  .ajoutFinancement button {
-  margin-top: 20px;
+@Component({
+  selector: 'app-financement',
+  templateUrl: './financement.component.html',
+  styleUrls: ['./financement.component.css']
+})
+export class FinancementComponent implements OnInit {
+  objetsDeFinancement: ObjetFinancement[] = [];
+  selectedObjetFinancement: ObjetFinancement | null = null;
+  hiddenObjetfinancement = false;
+
+  constructor(private financementService: FinancementService) { }
+
+  ngOnInit(): void {
+    this.loadObjetsFinancement('someId'); // Remplacez 'someId' par l'ID réel
+  }
+
+  loadObjetsFinancement(id: string) {
+    this.financementService.getListObjetFinancementbyId(id).subscribe(
+      (data: ObjetFinancement[]) => {
+        this.objetsDeFinancement = data;
+        if (this.objetsDeFinancement.length > 0) {
+          this.selectedObjetFinancement = this.objetsDeFinancement[0];
+        }
+      },
+      (error) => {
+        console.error('Error loading objets de financement', error);
+      }
+    );
+  }
+
+  selectObjetFinancement(objet: ObjetFinancement) {
+    this.selectedObjetFinancement = objet;
+  }
+
+  toggleObjetFinancement() {
+    this.hiddenObjetfinancement = !this.hiddenObjetfinancement;
+  }
+
+  ajouterObjetFinancement() {
+    const newObjet: ObjetFinancement = {
+      idObjetFinancement: '',
+      codeObjetFinancement: '',
+      quotePartObjet: 0,
+      gainCEP: 0,
+      dateFinTravaux: new Date(),
+      bien: new Bien(),
+      dpeAvantTravaux: new Dpe(),
+      dpeApresTravaux: new Dpe(),
+      alignement: new Alignement(),
+      eligibilite: new Eligibilite(),
+      piecesJustificatives: [],
+      codeFamilleObjet: '',
+      garantie: []
+    };
+    this.objetsDeFinancement.push(newObjet);
+    this.selectObjetFinancement(newObjet);
+  }
 }
-
-.breadcrumb-container {
-  display: flex;
-  flex-wrap: nowrap;
-  overflow-x: auto;
-  padding: 10px 0;
-}
-
-.breadcrumb-item {
-  display: flex;
-  align-items: center;
-  margin-right: 20px;
-  cursor: pointer;
-}
-
-.breadcrumb-item .breadcrumb-content {
-  padding: 10px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  background-color: #f8f9fa;
-  text-align: center;
-}
-
-.breadcrumb-item .details {
-  margin-top: 10px;
-}
-
-.required {
-  color: red;
-}
-
-img {
-  cursor: pointer;
-}
-
-.breadcrumb-item h3 {
-  margin: 0;
-}
-
