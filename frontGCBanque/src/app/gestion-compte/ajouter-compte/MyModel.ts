d14f394fb657b5241b4cc67bfe97bf88063f647c
ziedenit <guendil_zied@yahoo.com>
2024-06-16 23:25:04 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,6 +1,1 @@
-   // Act and Assert
-        mockMvc.perform(get("/listObjetFinancement/" + idFinancement)
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(content().json("[{\"idObjetFinancement\":\"obj1\"},{\"idObjetFinancement\":\"obj2\"}]"));
-    }
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
