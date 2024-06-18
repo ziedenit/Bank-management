53d4df8a5b29e1cfddf16d79a68566403e613dda
ziedenit <guendil_zied@yahoo.com>
2024-06-18 14:08:37 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,40 +1,34 @@
-import org.kie.api.runtime.KieSession;
-import org.kie.api.runtime.KieServices;
+j'ai modifier de la meme facon et j'ai ce fichier drl genere et sur lequel je me base pour lance ce service 
+package com.cl.msofd.engineRules;
+
 import org.kie.api.runtime.KieContainer;
-import org.kie.api.runtime.rule.FactHandle;
+import org.kie.api.runtime.KieSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class DroolsTest {
-    public static void main(String[] args) {
-        try {
-            // Load the KIE services
-            KieServices ks = KieServices.Factory.get();
-            KieContainer kContainer = ks.getKieClasspathContainer();
-            KieSession kSession = kContainer.newKieSession("ksession-rules");
+@Service
+public class DroolsService {
+    @Autowired
+    private KieContainer kieContainer;
 
-            // Initialize the global variable
-            List<AcquisitionResponse> responses = new ArrayList<>();
-            kSession.setGlobal("responses", responses);
 
-            // Insert facts into the session
-            Acquisition acquisition = new Acquisition();
-            // Set properties of acquisition
+    public AcquisitionResponse applyRules(Acquisition acquisition) {
+        List<AcquisitionResponse> responses = new ArrayList<>();
+        KieSession kieSession = kieContainer.newKieSession();
+        kieSession.insert(acquisition);
+        kieSession.setGlobal("responses", responses);
+        kieSession.fireAllRules();
 
-            FactHandle fact1 = kSession.insert(acquisition);
-
-            // Fire all rules
-            kSession.fireAllRules();
-
-            // Retrieve the results
-            for (AcquisitionResponse response : responses) {
-                System.out.println(response);
-            }
-
-            // Dispose the session
-            kSession.dispose();
-        } catch (Throwable t) {
-            t.printStackTrace();
-        }
+        return responses.isEmpty() ? null : responses.get(0);
     }
 }
+mais toujours  un probleme au niveau de cette instruction   kieSession.setGlobal("responses", responses);
+<ErrorResponse>
+    <code>TECHNICAL_EXCEPTION_OCCURED </code>
+    <message>
+        <message>Unexpected global [responses]</message>
+    </message>
+</ErrorResponse>
