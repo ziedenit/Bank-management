83f796147c4e51ffada8d58dc53b5ea5a9ba117d
ziedenit <guendil_zied@yahoo.com>
2024-06-23 14:30:26 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,20 +1,21 @@
 package com.cl.msofd.engineRules;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.ApplicationArguments;
 
-import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.text.ParseException;
 
-@Configuration
-public class AutoGenerateRulesConfig {
+@Component
+public class RulesGeneratorRunner implements ApplicationRunner {
 
     @Autowired
     private ExcelToDroolsService excelToDroolsService;
 
-    @PostConstruct
-    public void generateRulesOnStartup() {
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
         try {
             excelToDroolsService.generateDroolsFile("src/main/resources/rules.drl");
             System.out.println("Rules generated successfully on startup!");
