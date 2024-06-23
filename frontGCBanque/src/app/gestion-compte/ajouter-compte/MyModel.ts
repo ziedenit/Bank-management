e53b1ef354e6594dec1d0768c3ce699edaed1ff6
ziedenit <guendil_zied@yahoo.com>
2024-06-23 14:40:30 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,60 +1,17 @@
 package com.cl.msofd.engineRules;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
-import java.nio.file.*;
-
 @Component
-public class FileWatcherService {
+public class FileWatcherRunner implements CommandLineRunner {
 
     @Autowired
-    private ExcelToDroolsService excelToDroolsService;
+    private FileWatcherService fileWatcherService;
 
-    public void watchFile() throws IOException, InterruptedException {
-        Path path = Paths.get("path/to/your/xls/directory");
-        WatchService watchService = FileSystems.getDefault().newWatchService();
-        path.register(watchService, StandardWatchEventKinds.ENTRY_MODIFY);
-
-        System.out.println("Watching directory: " + path);
-
-        new Thread(() -> {
-            while (true) {
-                WatchKey key;
-                try {
-                    key = watchService.take();
-                } catch (InterruptedException ex) {
-                    return;
-                }
-
-                for (WatchEvent<?> event : key.pollEvents()) {
-                    WatchEvent.Kind<?> kind = event.kind();
-
-                    if (kind == StandardWatchEventKinds.OVERFLOW) {
-                        continue;
-                    }
-
-                    WatchEvent<Path> ev = (WatchEvent<Path>) event;
-                    Path fileName = ev.context();
-
-                    if (fileName.toString().equals("arbreAcquisition.xlsx")) {
-                        System.out.println("File modified: " + fileName);
-                        try {
-                            excelToDroolsService.generateDroolsFile("src/main/resources/rules.drl");
-                            System.out.println("Rules generated successfully after file modification!");
-                        } catch (IOException | ParseException e) {
-                            e.printStackTrace();
-                            System.err.println("Error generating rules after file modification: " + e.getMessage());
-                        }
-                    }
-                }
-
-                boolean valid = key.reset();
-                if (!valid) {
-                    break;
-                }
-            }
-        }).start();
+    @Override
+    public void run(String... args) throws Exception {
+        fileWatcherService.watchFile();
     }
 }
