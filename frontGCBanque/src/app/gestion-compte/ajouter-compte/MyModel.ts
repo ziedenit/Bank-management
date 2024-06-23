e3f6de2947a41032c7c1dbc8d5d9c630e247de97
ziedenit <guendil_zied@yahoo.com>
2024-06-23 21:21:57 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,30 +1,5 @@
-@RestController
-@RequestMapping("/acquisition")
-public class AcquisitionController {
+System.out.println("Generated rules: \n" + rulesContent.toString()); // Log generated rules
 
-    @Autowired
-    private ExcelToDroolsService excelToDroolsService;
-
-    @Autowired
-    private DroolsService droolsService;
-
-    @Autowired
-    private DroolsConfig droolsConfig;
-
-    @GetMapping("/generate-rules")
-    public String generateRules() {
-        try {
-            excelToDroolsService.generateDroolsFile("src/main/resources/rules.drl");
-            droolsConfig.reloadContainer(); // Reload the KieContainer after generating the rules
-            return "Rules generated successfully!";
-        } catch (IOException | ParseException e) {
-            e.printStackTrace();
-            return "Error generating rules: " + e.getMessage();
-        }
+  if (kieBuilder.getResults().hasMessages(org.kie.api.builder.Message.Level.ERROR)) {
+        System.out.println("Error building KieBase: " + kieBuilder.getResults().toString());
     }
-
-    @PostMapping("/apply-rules")
-    public AcquisitionResponse applyRules(@RequestBody Acquisition acquisition) {
-        return droolsService.applyRules(acquisition);
-    }
-}
