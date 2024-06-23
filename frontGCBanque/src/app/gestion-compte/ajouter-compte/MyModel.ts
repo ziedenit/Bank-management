c742913fa33264204d51b8da99df3c633dab88d9
ziedenit <guendil_zied@yahoo.com>
2024-06-23 15:04:26 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,10 +1,7 @@
-    reloadDroolsFile(outputDroolsFile);
-    }
+    private final KieContainer kieContainer;
 
-    private void reloadDroolsFile(String outputDroolsFile) {
-        KieFileSystem kieFileSystem = kieContainer.getKieServices().newKieFileSystem();
-        kieFileSystem.write(ResourceFactory.newFileResource(outputDroolsFile));
-        KieBuilder kieBuilder = kieContainer.getKieServices().newKieBuilder(kieFileSystem);
-        kieBuilder.buildAll();
-        kieContainer.updateToVersion(kieBuilder.getKieModule().getReleaseId());
-    }
+    @Autowired
+    public ExcelToDroolsService(MongoTemplate mongoTemplate, KieContainer kieContainer) {
+        this.mongoTemplate = mongoTemplate;
+        this.kieContainer = kieContainer;
+    
