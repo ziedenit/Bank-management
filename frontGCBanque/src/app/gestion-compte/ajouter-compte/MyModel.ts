1bd80cccf06f2db80dd2c80991097c1e654622f9
ziedenit <guendil_zied@yahoo.com>
2024-07-01 14:02:01 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,6 +1,23 @@
 
-    public List<ObjetFinancement> getListObjectFinancementByIdFinancement(String idFinancement) {
-        return financementRepository.findByidFinancement(idFinancement)
-                .orElseThrow(() -> new ListObjetNotFoundException("Financement not found with id: " + idFinancement))
-                .getObjetFinancement();
+  @Test
+    void testDeleteFinancementByIdFinancement() {
+        doNothing().when(financementRepository).deleteByIdFinancement("FR51TLJ5K");
+
+        financementService.deleteFinancementByIdFinancement("FR51TLJ5K");
+
+        verify(financementRepository, times(1)).deleteByIdFinancement("FR51TLJ5K");
     }
+
+org.mockito.exceptions.base.MockitoException: 
+Only void methods can doNothing()!
+Example of correct use of doNothing():
+    doNothing().
+    doThrow(new RuntimeException())
+    .when(mock).someVoidMethod();
+Above means:
+someVoidMethod() does nothing the 1st time but throws an exception the 2nd time is called
+
+	at com.cl.msofd.service.FinancementServiceTest.testDeleteFinancementByIdFinancement(FinancementServiceTest.java:97)
+	at java.base/java.lang.reflect.Method.invoke(Method.java:568)
+	at java.base/java.util.ArrayList.forEach(ArrayList.java:1511)
+	at java.base/java.util.ArrayList.forEach(ArrayList.java:1511)
