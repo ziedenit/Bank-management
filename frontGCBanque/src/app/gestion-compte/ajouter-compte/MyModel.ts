0c24de5f860a01dac1da48aa526c3307d5bfa9a6
ziedenit <guendil_zied@yahoo.com>
2024-07-04 12:14:31 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,1 +1,14 @@
-Cannot resolve constructor 'Ademe(List<T>)'
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Ademe implements Serializable {
+
+private static final long serialVersionUID = 1L;
+
+    private int total;
+    private String next;
+    private List<DpeAdeme> results;   
+}
+
