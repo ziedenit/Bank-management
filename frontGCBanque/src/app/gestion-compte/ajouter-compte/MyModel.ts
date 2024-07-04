20cde63a15456caa7cbb71bdb5044d3d04032ae0
ziedenit <guendil_zied@yahoo.com>
2024-07-04 23:41:37 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,19 +1,11 @@
-                if (newValue != null) {
-                    field.set(existingFinancement, newValue);
-                }
-This accessibility bypass should be removed.
-Why is this an issue?
-
-2 months ago
-L309
-Code Smell
-
-Major
-
-Open
-
-rebib_s
-30min effort
-
-Comment
-
+private void merge(Object target, Object source) throws IllegalAccessException {
+        Field[] fields = source.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            field.setAccessible(true);
+            Object value = field.get(source);
+            if (value != null) {
+                field.set(target, value);
+            }
+        }
+    }
+refactor this methode ti fix smell code This accessibility update should be removed.  field.set(target, value) et     field.setAccessible(true);
