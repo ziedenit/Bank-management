572ec9fa7a20e27ed3d35131c196d2203a338223
ziedenit <guendil_zied@yahoo.com>
2024-07-04 12:01:18 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,41 +1,22 @@
+org.opentest4j.AssertionFailedError: 
+expected: com.cl.msofd.model.ClientEntreprise@7e2ff255
+ but was: com.cl.msofd.model.ClientEntreprise@ccd3e52
+Expected :com.cl.msofd.model.ClientEntreprise@7e2ff255
+Actual   :com.cl.msofd.model.ClientEntreprise@ccd3e52
+<Click to see difference>
 
-    @Test
-    void should_return_ClientEntreprise_for_LEGAL_PERSON() throws Exception {
-        ClientDetails clientDetails = new ClientDetails();
-        clientDetails.setPersonType("LEGAL_PERSON");
-        clientDetails.setPersonId("12345");
-        clientDetails.setCasaData(new DescriptiveCompany("123456789", "Mock Company"));
-        DescriptiveIndividual representative = new DescriptiveIndividual();
-        representative.setFirstName("John");
-        representative.setUsualLastName("Doe");
-        representative.setCivility(new Civility());
-        clientDetails.setRepresentativesLegals(Collections.singletonList(representative));
 
-        ClientResponse clientResponse = new ClientResponse();
-        clientResponse.setEntrepriseDetails(clientDetails);
+	at java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)
+	at java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:77)
+	at java.base/jdk.internal.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)
+	at java.base/java.lang.reflect.Constructor.newInstanceWithCaller(Constructor.java:499)
+	at com.cl.msofd.service.EntrepriseServiceTest.should_return_ClientEntreprise_for_LEGAL_PERSON(EntrepriseServiceTest.java:88)
+	at java.base/java.lang.reflect.Method.invoke(Method.java:568)
+	at java.base/java.util.ArrayList.forEach(ArrayList.java:1511)
+	at java.base/java.util.ArrayList.forEach(ArrayList.java:1511)
 
-        String responseBody = new ObjectMapper().writeValueAsString(clientResponse);
-        HttpResponse<String> httpResponse = mock(HttpResponse.class);
-        when(httpResponse.statusCode()).thenReturn(200);
-        when(httpResponse.body()).thenReturn(responseBody);
+time=2024-07-04T12:00:33.605+02:00|level=INFO |event_cod=empty|event_typ=TECHNICAL|sec_event_typ=METIER|usr_id=empty|uom_cod=20001|app_id=TestApp|component_id=empty|corr_id=empty|sess_id=empty|src_client_id=empty|layer_id=empty|httpMethod=empty|httpStatus=empty|httpRoute=empty|httpRoutePattern=empty|msg=Closing JPA EntityManagerFactory for persistence unit 'default'
+time=2024-07-04T12:00:33.609+02:00|level=INFO |event_cod=empty|event_typ=TECHNICAL|sec_event_typ=METIER|usr_id=empty|uom_cod=20001|app_id=TestApp|component_id=empty|corr_id=empty|sess_id=empty|src_client_id=empty|layer_id=empty|httpMethod=empty|httpStatus=empty|httpRoute=empty|httpRoutePattern=empty|msg=HikariPool-1 - Shutdown initiated...
+time=2024-07-04T12:00:33.614+02:00|level=INFO |event_cod=empty|event_typ=TECHNICAL|sec_event_typ=METIER|usr_id=empty|uom_cod=20001|app_id=TestApp|component_id=empty|corr_id=empty|sess_id=empty|src_client_id=empty|layer_id=empty|httpMethod=empty|httpStatus=empty|httpRoute=empty|httpRoutePattern=empty|msg=HikariPool-1 - Shutdown completed.
 
-        CompletableFuture<HttpResponse<String>> response = CompletableFuture.completedFuture(httpResponse);
-        when(httpClient.sendAsync(any(HttpRequest.class), ArgumentMatchers.<HttpResponse.BodyHandler<String>>any()))
-                .thenReturn(response);
-
-        when(jsonUtils.covertFromJsonToObject(responseBody, ClientResponse.class)).thenReturn(clientResponse);
-
-        ClientEntreprise expectedClient = ClientEntreprise.builder()
-                .idReper("12345")
-                .typePerson("LEGAL_PERSON")
-                .siren("123456789")
-                .legalName("Mock Company")
-                .firstName("John")
-                .usuaLastName("Doe")
-                .civilite(representative.getCivility().getLabel())
-                .build();
-
-        ClientEntreprise actualClient = entrepriseService.getClient("12345");
-
-        assertThat(actualClient).isEqualTo(expectedClient);
-    }
+Process finished with exit code -1
