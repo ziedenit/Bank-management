26726e28994b41a26b7820717321ddaddace77b1
ziedenit <guendil_zied@yahoo.com>
2024-07-04 13:54:52 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,24 +1,203 @@
-@Test
-void test_alignementAcquisitionAncien() throws ParseException {
-    Date endDate = formatDate.parse("31/12/2012");
-
-    // Test case where DPE is present, construction year is valid and within range
-    String result = alignementService.alignementAcquisitionAncien(true, "A", 100, 150, 200, "RT2012", 2005);
-    assertEquals("01", result);
-
-    // Test case where construction year is valid and within range but no DPE
-    result = alignementService.alignementAcquisitionAncien(false, "A", 100, 150, 200, "RT2012", 2010);
-    assertEquals("01", result);
-
-    // Test case for year 2021
-    result = alignementService.alignementAcquisitionAncien(true, "A", 100, 150, 200, "RT2012", 2021);
-    assertEquals("01", result);
-
-    // Test case for year 2022 or after
-    result = alignementService.alignementAcquisitionAncien(true, "A", 100, 150, 200, "RT2020", 2022);
-    assertEquals("01", result);
-
-    // Test case for construction year less than or equal to 1700
-    result = alignementService.alignementAcquisitionAncien(true, "A", 100, 150, 200, "RT2000", 1699);
-    assertEquals("07", result);
+package com.cl.msofd.service;
+import com.cl.msofd.exception.ClientNotFoundException;
+import com.cl.msofd.clients.ClientResponse;
+import com.cl.msofd.clients.Error;
+import com.cl.msofd.model.ClientParticulier;
+import com.cl.msofd.utility.JSONUtilOFD;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+@Service
+public class ClientService {
+    public static final String AUTHORIZATION = "Authorization";
+    @Autowired
+    private HttpClient httpClient;
+    @Autowired
+    private JSONUtilOFD jsonUtils;
+    @Value("${referentiel.personne.url}")
+    private String referentielPersonne;
+    public ClientParticulier getClient(String idReper) throws IOException, InterruptedException, ExecutionException , ClientNotFoundException {
+        String url = String.format(referentielPersonne, idReper).concat("&person_type=INDIVIDUAL");
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .header(AUTHORIZATION, jsonUtils.basicAuthReferentiel())
+                .build();
+        CompletableFuture<HttpResponse<String>> response = httpClient.sendAsync(request, BodyHandlers.ofString());
+        HttpResponse<String> httpResponse = response.get();
+        String responseBody = httpResponse.body();
+        
+        // Vérifiez si la réponse est un tableau d'erreurs
+        if (httpResponse.statusCode() != 200 || responseBody.startsWith("[")) {
+            // Parsez le tableau d'erreurs
+            ObjectMapper objectMapper = new ObjectMapper();
+            Error[] errors = objectMapper.readValue(responseBody, Error[].class);
+            for (Error error : errors) {
+                if (error.getCode().equals("B801")) {
+                    throw new ClientNotFoundException(error.getMessage());
+                }
+            }
+        }
+        // Si aucune erreur n'est détectée, continuez le traitement normalement
+        ClientResponse personDetails = jsonUtils.covertFromJsonToObject(responseBody, ClientResponse.class);
+        if (personDetails == null) {
+            throw new ClientNotFoundException("Client not found");
+        }
+        // Construction de l'objet ClientParticulier à partir des détails du client
+        return ClientParticulier.builder()
+                .idReper(personDetails.getEntrepriseDetails().getPersonId())
+                .nomUsageClient(personDetails.getEntrepriseDetails().getDescriptiveIndividual().getUsualLastName())
+                .prenomClient(personDetails.getEntrepriseDetails().getDescriptiveIndividual().getFirstName())
+                .dateNaissanceClient(personDetails.getEntrepriseDetails().getDescriptiveIndividual().getBirthDate())
+                .civility(personDetails.getEntrepriseDetails().getDescriptiveIndividual().getCivility().getLabel())
+                .build();
+    }
 }
+//
+
+package com.cl.msofd.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ClientParticulier implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String idReper;
+
+    private String nomUsageClient;
+
+    private String prenomClient;
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private java.sql.Date dateNaissanceClient;
+
+    private String civility;
+}
+
+//
+package com.cl.msofd.clients;
+
+import java.util.List;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+@JsonIgnoreProperties(ignoreUnknown=true)
+public class ClientResponse implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	
+	   @JsonProperty("person_details")
+	    private ClientDetails entrepriseDetails;
+
+	    @JsonProperty("errors")
+	    private List<Error> errors;
+	    	    
+}
+//
+package com.cl.msofd.clients;
+
+import java.io.Serializable;
+import java.util.List;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+@JsonIgnoreProperties(ignoreUnknown=true)
+public class ClientDetails  implements Serializable {
+	private static final long serialVersionUID = 1L;
+	  @JsonProperty("person_type")
+	    private String personType;
+	  
+	  @JsonProperty("person_id")
+	    private String personId;
+	    	    
+	  @JsonProperty("descriptive_individual")
+	    private DescriptiveIndividual descriptiveIndividual;
+     
+      @JsonProperty("casa_data")
+       private DescriptiveCompany casaData;
+      
+      @JsonProperty("representatives_legals")
+      private List <DescriptiveIndividual>  representativesLegals;
+     
+}
+//
+package com.cl.msofd.clients;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+@JsonIgnoreProperties(ignoreUnknown=true)
+public class DescriptiveIndividual implements Serializable {
+	
+	
+    @JsonProperty("usual_last_name")
+    private String usualLastName;
+
+    @JsonProperty("first_name")
+    private String firstName;
+
+    @JsonProperty("civility")
+    private Civility civility;
+    
+    @JsonProperty("birth_date")
+    private java.sql.Date birthDate;
+
+}
+je veux faire une classe de tes pour le service ci haut sachant que je dois mockee le referentiel.personne.url 
+
+
