40e6c0b1a6e27355c291a2ae0c09da12602c8441
ziedenit <guendil_zied@yahoo.com>
2024-07-09 13:24:27 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,38 +1,55 @@
-time=2024-07-09T13:22:16.563+02:00|level=INFO |event_cod=empty|event_typ=TECHNICAL|sec_event_typ=METIER|usr_id=empty|uom_cod=20001|app_id=TestApp|component_id=empty|corr_id=empty|sess_id=empty|src_client_id=empty|layer_id=empty|httpMethod=empty|httpStatus=empty|httpRoute=empty|httpRoutePattern=empty|msg=Initializing Spring TestDispatcherServlet ''
-time=2024-07-09T13:22:16.564+02:00|level=INFO |event_cod=empty|event_typ=TECHNICAL|sec_event_typ=METIER|usr_id=empty|uom_cod=20001|app_id=TestApp|component_id=empty|corr_id=empty|sess_id=empty|src_client_id=empty|layer_id=empty|httpMethod=empty|httpStatus=empty|httpRoute=empty|httpRoutePattern=empty|msg=Initializing Servlet ''
-time=2024-07-09T13:22:16.568+02:00|level=INFO |event_cod=empty|event_typ=TECHNICAL|sec_event_typ=METIER|usr_id=empty|uom_cod=20001|app_id=TestApp|component_id=empty|corr_id=empty|sess_id=empty|src_client_id=empty|layer_id=empty|httpMethod=empty|httpStatus=empty|httpRoute=empty|httpRoutePattern=empty|msg=Completed initialization in 4 ms
-time=2024-07-09T13:22:16.874+02:00|level=INFO |event_cod=empty|event_typ=APPLICATIVE|sec_event_typ=METIER|usr_id=empty|uom_cod=20001|app_id=TestApp|component_id=empty|corr_id=empty|sess_id=empty|src_client_id=empty|layer_id=empty|httpMethod=empty|httpStatus=empty|httpRoute=empty|httpRoutePattern=empty|msg=getClient Entreprise ayant l'id : 123
+@SpringBootTest
+public class EntrepriseControllerTest {
 
-jakarta.servlet.ServletException: Request processing failed: java.lang.RuntimeException: Internal server error
+    @Autowired
+    private WebApplicationContext webApplicationContext;
 
-	at org.springframework.web.servlet.FrameworkServlet.processRequest(FrameworkServlet.java:1022)
-	at org.springframework.web.servlet.FrameworkServlet.doGet(FrameworkServlet.java:903)
-	at jakarta.servlet.http.HttpServlet.service(HttpServlet.java:564)
-	at org.springframework.web.servlet.FrameworkServlet.service(FrameworkServlet.java:885)
-	at org.springframework.test.web.servlet.TestDispatcherServlet.service(TestDispatcherServlet.java:72)
-	at jakarta.servlet.http.HttpServlet.service(HttpServlet.java:658)
-	at org.springframework.mock.web.MockFilterChain$ServletFilterProxy.doFilter(MockFilterChain.java:165)
-	at org.springframework.mock.web.MockFilterChain.doFilter(MockFilterChain.java:132)
-	at org.springframework.test.web.servlet.MockMvc.perform(MockMvc.java:201)
-	at com.cl.msofd.controller.EntrepriseControllerTest.testGetClient_InternalServerError(EntrepriseControllerTest.java:72)
-	at java.base/java.lang.reflect.Method.invoke(Method.java:568)
-	at java.base/java.util.ArrayList.forEach(ArrayList.java:1511)
-	at java.base/java.util.ArrayList.forEach(ArrayList.java:1511)
-Caused by: java.lang.RuntimeException: Internal server error
-	at com.cl.msofd.service.EntrepriseService.getClient(EntrepriseService.java:37)
-	at com.cl.msofd.controller.EntrepriseController.getClient(EntrepriseController.java:47)
-	at java.base/jdk.internal.reflect.NativeMethodAccessorImpl.invoke0(Native Method)
-	at java.base/jdk.internal.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:77)
-	at java.base/jdk.internal.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)
-	at java.base/java.lang.reflect.Method.invoke(Method.java:568)
-	at org.springframework.web.method.support.InvocableHandlerMethod.doInvoke(InvocableHandlerMethod.java:259)
-	at org.springframework.web.method.support.InvocableHandlerMethod.invokeForRequest(InvocableHandlerMethod.java:192)
-	at org.springframework.web.servlet.mvc.method.annotation.ServletInvocableHandlerMethod.invokeAndHandle(ServletInvocableHandlerMethod.java:118)
-	at org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter.invokeHandlerMethod(RequestMappingHandlerAdapter.java:920)
-	at org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter.handleInternal(RequestMappingHandlerAdapter.java:830)
-	at org.springframework.web.servlet.mvc.method.AbstractHandlerMethodAdapter.handle(AbstractHandlerMethodAdapter.java:87)
-	at org.springframework.web.servlet.DispatcherServlet.doDispatch(DispatcherServlet.java:1089)
-	at org.springframework.web.servlet.DispatcherServlet.doService(DispatcherServlet.java:979)
-	at org.springframework.web.servlet.FrameworkServlet.processRequest(FrameworkServlet.java:1014)
-	... 12 more
+    private MockMvc mockMvc;
 
+    @MockBean
+    private EntrepriseService entrepriseService;
+
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void testGetClient_Success() throws Exception {
+        ClientEntreprise clientEntreprise = ClientEntreprise.builder()
+                .idReper("123")
+                .typePerson("LEGAL_PERSON")
+                .siren("123456789")
+                .legalName("Test Legal Name")
+                .firstName("John")
+                .usuaLastName("Doe")
+                .civilite("Mr.")
+                .build();
+
+        when(entrepriseService.getClient(anyString())).thenReturn(clientEntreprise);
+
+        mockMvc.perform(get("/api/v1/clients/entreprise/123")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json("{\"idReper\":\"123\",\"typePerson\":\"LEGAL_PERSON\",\"siren\":\"123456789\",\"legalName\":\"Test Legal Name\",\"usuaLastName\":\"Doe\",\"firstName\":\"John\",\"civilite\":\"Mr.\"}"));
+    }
+
+    @Test
+    public void testGetClient_NotFound() throws Exception {
+        when(entrepriseService.getClient(anyString())).thenThrow(new ClientNotFoundException("Client not found"));
+
+        mockMvc.perform(get("/api/v1/clients/entreprise/123")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void testGetClient_InternalServerError() throws Exception {
+        when(entrepriseService.getClient(anyString())).thenThrow(new RuntimeException("Internal server error"));
+
+        mockMvc.perform(get("/api/v1/clients/entreprise/123")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isInternalServerError());
+    }
+}
