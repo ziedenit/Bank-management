346f5f4e6a92d9aa7512577e3af89c19bc153e5c
ziedenit <guendil_zied@yahoo.com>
2024-07-29 23:22:30 +0200

    Update ajouter-compte.component.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/ajouter-compte.component.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/ajouter-compte.component.ts
@@ -1,26 +1,53 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { ObjetFinancement } from '../model/objetFinancement';
-import { Observable } from 'rxjs';
+import { Alignement } from './alignement';
+import { Eligibilite } from './eligibilite';
+import { Intervenant } from './intervenant';
+import { ObjetFinancement } from './objetFinancement';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class MultiObjetService {
-  private financementURL = `http://localhost:8081/api/v1/financement/listObjetFinancement/`;
-  idFinancement : string []
-    constructor(private http: HttpClient) {}
-    httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type':  'application/json',
-        'Authorization': 'Basic ' + btoa('user1:user1Pass')
-      })
-    };
+
+export class Financement {
+
+idFinancement: string;
+objetFinancement: ObjetFinancement[] ;
+alignement: Alignement;
+eligibilite: Eligibilite;
+
+intervenant: Intervenant;
+indicateurFinancementDedie:string;
+indicateurNatureDurable:string;
+typeRisqueClimatiqueAttenue:string;
+
+codeApplicatifOrigine: string;
+indicateurReprise: boolean;
+statut: number ;
+agenceCompte:string;
+
+
+}
+<div class="container-fluid">
+    <!-- File d'Ariane pour les objets de financement -->
+    <nav aria-label="breadcrumb">
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item" *ngFor="let objet of objetsFinancements; let i = index">
+          <a [routerLink]=""
+          queryParamsHandling="preserve" (click)="onBreadcrumbClick(i,objet)">Objet de financement {{ i + 1 }}</a>
+        </li>
+      </ol>
+    </nav>
+</div>
+  ²
+
+  ngOnInit(): void {
+    this.idFinancement = this.route.snapshot.queryParams["idFinancement"];
+    this.Url_Retour_Base64 = this.route.snapshot.queryParams["urlRetour"];
+ 
+    if (this.Url_Retour_Base64) {this.Url_Retour_Utf8 = atob(this.Url_Retour_Base64);}
+ 
+ 
+    this.multiObjetService.getListeObjetsFinancement(this.idFinancement).subscribe(objets => {
+      this.objetsFinancements = objets;
+      console.log(objets)
+      this.addListOfReceivedObjets(objets
+      )
+    });
    
-   
-    getListeObjetsFinancement(idFinancement: string): Observable<ObjetFinancement[]> {
-      return this.http.get<ObjetFinancement[]>(this.financementURL+idFinancement,this.httpOptions);
-    }
   }
-   
-
