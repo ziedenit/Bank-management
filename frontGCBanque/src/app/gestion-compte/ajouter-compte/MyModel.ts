c8b13dab12eae861f37cb660df7af77ed9a38c7b
ziedenit <guendil_zied@yahoo.com>
2024-08-01 18:15:32 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,16 +1,16 @@
-const nouvelObjet: ObjetFinancement = {
-      idObjetFinancement: "",
-      codeObjetFinancement: "",
-      quotePartObjet: null,
-      gainCEP: null,
-      dateFinTravaux: null,
-      bien: new Bien(),
-      dpeAvantTravaux: new Dpe(),
-      dpeApresTravaux: new Dpe(),
-      alignement: new Alignement(),
-      eligibilite: new Eligibilite(),
-      piecesJustificatives: [],
-      codeFamilleObjet: "",
-      garantie: [],
-      firstDisconnectionOfd: false,
-    };
+<div class="container-fluid">  
+  <!-- File d'Ariane pour les objets de financement -->
+  <nav aria-label="breadcrumb" *ngIf="showFileAriane==true">
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item" *ngFor="let objet of objetsFinancements; let i = index">
+        <a [routerLink]=""
+        queryParamsHandling="preserve" (click)="onBreadcrumbClick(i,objet)">Objet de financement {{ i + 1 }}</a>
+      </li>
+     
+    </ol>
+  </nav> 
+  <div class="col-auto">
+    <button class="btn btn-primary btn-sm " (click)="ajouterObjetFinancement()">Ajouter un nouvel objet de financement  <img src="../../../assets/icons/plus.svg" /></button>
+
+</div>
+        maintenant j'ai ce code html je veux que le button soit a droit au niveau du div container fluid sachant que je peux clicker plusieurs foois pour ajouter un objet dans la file je veux que le container soit responsive et autre chose je veux que chaque elementObjet de financement {{ i + 1 }} de breadcrumb soit moderne sous  forme de design pentagone  
