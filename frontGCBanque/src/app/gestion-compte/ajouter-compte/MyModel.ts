5d0da088696b6cef9328234131c20e04a49ae099
ziedenit <guendil_zied@yahoo.com>
2024-08-02 11:08:44 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,18 +1,1 @@
-<div class="container-fluid">
-  <!-- File d'Ariane pour les objets de financement -->
-  <nav aria-label="breadcrumb" *ngIf="showFileAriane == true" class="breadcrumb-nav">
-    <ol class="breadcrumb-custom">
-      <li class="breadcrumb-item-custom" *ngFor="let objet of objetsFinancements; let i = index">
-        <a [routerLink]=""
-          queryParamsHandling="preserve" (click)="onBreadcrumbClick(i, objet)">
-         <span>Objet de financement</span>  {{ i + 1 }}
-        </a>
-      </li>
-    </ol>
-    <div class="button-container">
-      <button class="btn btn-primary btn-sm" (click)="ajouterObjetFinancement()">Ajouter un nouvel objet de financement
-        <img src="../../../assets/icons/plus.svg" />
-      </button>
-    </div>
-  </nav>
-</div>
+   <button class="close-btn" (click)="removeBreadcrumbItem(i)">x</button>
