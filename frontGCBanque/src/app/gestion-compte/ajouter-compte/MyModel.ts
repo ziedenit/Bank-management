842fa225a4cf35dd6f889952647ab7e28f37e637
ziedenit <guendil_zied@yahoo.com>
2024-08-02 13:15:23 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,1 +1,64 @@
- [(ngModel)]="objetsFinancements[selectedObjetIndex].quotePartObjet"
+     <div class="col-lg-4">
+                <div class="form-group" *ngIf="!hideFieldForm">
+                    <div class="custom-label">
+                        <label for="prixAquisitionBien">Prix du bien <em>(en euro)</em></label>
+                        <span class="required"> *</span>
+                        <input type="text"
+                               [(ngModel)]="objetsFinancements[selectedObjetIndex].quotePartObjet"
+                               (ngModelChange)="onPriceChange($event)"
+                               class="form-control form-control-sm"
+                               id="prixAquisitionBien"
+                               name="description"
+                               [disabled]="isFieldsDisabled" />
+                    </div>
+                </div>
+            </div>
+
+                                j'ai cette méthode a appliquer a chaque fois private checkRequiredFields(responseFinancement: any): void {
+    const bien = responseFinancement.objetFinancement[0]?.bien;
+    const dpeActuel = bien?.dpeActuel;
+    this.checkAndHighlightRequiredField(bien && bien.etatBien == null, "natureBien");
+    this.checkAndHighlightRequiredField(bien && bien.codeBatiment == null, "categorieBatiment");
+    this.checkAndHighlightRequiredField(bien && bien.partLCL == null, "partLcl");
+    this.checkAndHighlightRequiredField(bien && bien.prixBien == null, "prixAquisitionBien");
+    this.checkAndHighlightRequiredField(bien && bien.montantFinanceLCL == null, "montantLclFinance");
+	
+    if (bien?.etatBien == "Ancien" && dpeActuel) {
+        this.checkAndHighlightRequiredField(dpeActuel.numeroDpe == null || dpeActuel.numeroDpe == "", "numeroDpeAdeme");
+        this.checkAndHighlightRequiredField(dpeActuel.sirenDiagnostiqueur == null || (dpeActuel.numeroDpe != null && dpeActuel.sirenDiagnostiqueur == null), "SirenDPE");
+        this.checkAndHighlightRequiredField(dpeActuel.classeCep == null || dpeActuel.classeCep == "", "LettreCEP");
+        this.checkAndHighlightRequiredField(dpeActuel.classeGes == null || dpeActuel.classeGes == "", "LettreGES");
+        this.checkAndHighlightRequiredField(dpeActuel.estimationCep == null, "ValeurCEP");
+        this.checkAndHighlightRequiredField(dpeActuel.estimationGes == null, "ValeurGES");
+        this.checkAndHighlightRequiredField(bien.anneeConstruction == null, "anneeConstruction");
+    }
+    if (bien?.etatBien == "Neuf") {
+        this.checkAndHighlightRequiredField(bien.dateDepotPc == null, "dateDepot");
+		this.checkAndHighlightRequiredField(bien.dpeActuel.numeroDpe != null || bien.dpeActuel.numeroDpe !="" && bien.dpeActuel.sirenDiagnostiqueur == null, "SirenDPE");
+    }
+    this.checkAndHighlightRequiredField(bien && (bien.numeroNomRue == null || bien.numeroNomRue == ""), "numeroNomRue");
+    this.checkAndHighlightRequiredField(bien && (bien.codePostal == null || bien.codePostal == ""), "codePostal");
+    this.checkAndHighlightRequiredField(bien && (bien.nomCommune == null || bien.nomCommune == ""), "ville");
+    this.checkAndHighlightRequiredField(responseFinancement.objetFinancement[0]?.codeObjetFinancement == null, "selectedObjetFinancement");
+	this.checkAndHighlightRequiredField(
+		responseFinancement.objetFinancement[0] != null &&
+		responseFinancement.objetFinancement[0].bien != null &&
+		(responseFinancement.objetFinancement[0].bien.etatBien == "Neuf" ||
+			responseFinancement.objetFinancement[0].bien.etatBien == "Ancien") &&
+		responseFinancement.objetFinancement[0].bien.surfaceBien == null,
+		"SurfaceDuBien"
+	);
+}
+avant moddif j'ai ca  [ngModel]="prixAquisitionBien | number:'1.0-2':'fr'"
+ mais la mothode checkRequiredFields me remonte ca 
+core.js:5980  ERROR TypeError: Cannot read properties of undefined (reading 'quotePartObjet')
+    at FilArianeComponent_div_17_div_5_Template (fil-ariane.component.html:60:32)
+    at executeTemplate (core.js:9323:9)
+    at refreshView (core.js:9192:13)
+    at refreshEmbeddedViews (core.js:10312:17)
+    at refreshView (core.js:9216:9)
+    at refreshEmbeddedViews (core.js:10312:17)
+    at refreshView (core.js:9216:9)
+    at refreshComponent (core.js:10358:13)
+    at refreshChildComponents (core.js:8989:9)
+    at refreshView (core.js:9242:13)
