8c5443b843066327ed011e15d1f9e265078eb914
ziedenit <guendil_zied@yahoo.com>
2024-08-05 09:53:34 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,4 +1,5 @@
-  const currentObjet = this.objetsFinancements[this.selectedObjetIndex];
+private updateCurrentObjetData(): void {
+    const currentObjet = this.objetsFinancements[this.selectedObjetIndex];
 
     if (!currentObjet) {
         console.error("currentObjet est indéfini");
@@ -12,3 +13,24 @@
     if (!currentObjet.bien.dpeActuel) {
         currentObjet.bien.dpeActuel = {};
     }
+
+    // Mettre à jour les propriétés de currentObjet
+    currentObjet.bien.eligibleDpe = this.selectedType;
+    currentObjet.bien.etatBien = this.selectedNatBatiment;
+    currentObjet.bien.codeBatiment = this.codeBatimentSelected;
+    currentObjet.bien.partLCL = this.partLcl;
+    currentObjet.bien.montantFinanceLCL = this.montantLclFinance;
+    currentObjet.bien.prixBien = this.prixAquisitionBien;
+    currentObjet.bien.dateDepotPc = this.dateDepot ? new Date(this.dateDepot) : null;
+    currentObjet.bien.codeNormeThermique = this.normeThermique;
+    currentObjet.bien.dpeActuel.sirenDiagnostiqueur = this.SirenDPE;
+    currentObjet.bien.dpeActuel.numeroDpe = this.numeroDpeAdeme;
+
+    if (!currentObjet.alignement) {
+        currentObjet.alignement = {};
+    }
+
+    currentObjet.alignement.topAlignement = this.DpeResults ? this.alignementResultText : null;
+
+    // Ajoutez plus de vérifications ici si nécessaire pour d'autres propriétés
+}
