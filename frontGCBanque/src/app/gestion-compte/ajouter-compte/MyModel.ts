a8c56a45b8759638d35a3ec5ea27f23085224965
ziedenit <guendil_zied@yahoo.com>
2024-08-05 14:04:16 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,48 +1,24 @@
-  extractedInitialFinancement: Financement;
- objetsFinancements: ObjetFinancement[] = [];
-removeBreadcrumbItem(index: number) {
-			this.objetsFinancements.splice(index, 1);
-			console.log("apres suppression contenue du tablau");
-			console.log(this.objetsFinancements);
-			console.log("le financement au complet avec les objets ajoutés preparation post patch");
-			console.log(this.extractedInitialFinancement);
-		  }
-/////////////////////////
-je veux mettre ajour extractedInitialFinancement avec la nouveau table this.objetsFinancements comment faire quel instruction sachant que le model et le suivant 
-export class Financement {
+package com.cl.msofd.service;
+import org.springframework.stereotype.Service;
 
-idFinancement: string;
-objetFinancement: ObjetFinancement[] ;
-alignement: Alignement;
-eligibilite: Eligibilite;
+import java.security.SecureRandom;
 
-intervenant: Intervenant;
-indicateurFinancementDedie:string;
-indicateurNatureDurable:string;
-typeRisqueClimatiqueAttenue:string;
+@Service
+public class IdGeneratorService {
+    private IdGeneratorService() {
+    }
 
-codeApplicatifOrigine: string;
-indicateurReprise: boolean;
-statut: number ;
-agenceCompte:string;
-
-
+    public String generateId(String objet) {
+    	
+        StringBuilder idfdBuilder = new StringBuilder();
+        String characters = "abcdefghijklmnopqrstuvwxyz0123456789";
+        SecureRandom secureRandom = new SecureRandom();
+        for (int i = 0; i < 8; i++) {
+            int randomIndex = (int) (secureRandom.nextDouble() * characters.length());
+            char randomChar = characters.charAt(randomIndex);
+            idfdBuilder.append(randomChar);
+        }
+        return objet+idfdBuilder.toString().toUpperCase();
+    }
 }
-export class ObjetFinancement {
-	
-    idObjetFinancement:string;
-	codeObjetFinancement:string;// 02=Acquisition  03=Travaux
-	quotePartObjet:number;
-	gainCEP:number;
-	dateFinTravaux: Date;
-	bien:Bien;
-	dpeAvantTravaux: Dpe;
-	dpeApresTravaux: Dpe;
-	alignement: Alignement;
-    eligibilite: Eligibilite;
-	piecesJustificatives : Piece [] ;
-	codeFamilleObjet: string;	
-	garantie: Garantie[]; 
-	firstDisconnectionOfd:boolean;
 
-}
