f0638caaa865f4c5e384f166760ffe785fa00fd4
ziedenit <guendil_zied@yahoo.com>
2024-08-05 11:16:04 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,9 +1,16 @@
 checkAndHighlightRequiredField(condition, elementId) {
-		console.log(this.isfirstDebranchement)
-    if (condition && ! this.isfirstDebranchement) {
-        this.champObligatoire = true;
-        this.donneeObligatoire = 'Donnée obligatoire';
-        document.getElementById(elementId).style.border = "1px solid red";
+    console.log(this.isfirstDebranchement);
+    const element = document.getElementById(elementId);
+    
+    if (element) {  // Vérifiez si l'élément existe
+        if (condition && !this.isfirstDebranchement) {
+            this.champObligatoire = true;
+            this.donneeObligatoire = 'Donnée obligatoire';
+            element.style.border = "1px solid red";
+        } else {
+            element.style.removeProperty('border');
+        }
     } else {
-        document.getElementById(elementId).style.removeProperty('border');
-    }}
+        console.warn(`Element with id ${elementId} not found.`);
+    }
+}
