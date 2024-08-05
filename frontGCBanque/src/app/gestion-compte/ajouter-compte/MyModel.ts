d81f77afebec86cb9d567047ac972be002cf4321
ziedenit <guendil_zied@yahoo.com>
2024-08-05 17:10:40 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,21 +1,36 @@
-// Supposons que vous avez une méthode pour obtenir un nouvel objet
-getNewObjetFinancement() {
-  let nouvelObjet: any = {}; // Remplacez 'any' par le type réel de votre objet
+  ajouterObjetFinancement() {
+	this.showFileAriane=true;
+	const nouvelObjet: ObjetFinancement = {
+		idObjetFinancement: "",
+		codeObjetFinancement: "",
+		quotePartObjet: null,
+		gainCEP: null,
+		dateFinTravaux: null,
+		bien: new Bien(),
+		dpeAvantTravaux: new Dpe(),
+		dpeApresTravaux: new Dpe(),
+		alignement: new Alignement(),
+		eligibilite: new Eligibilite(),
+		piecesJustificatives: [],
+		codeFamilleObjet: "",
+		garantie: [],
+		firstDisconnectionOfd: false,
+	  };
+	 
+	  this.idGeneratorService.generateIdObjetFinancement().subscribe(
+		id => {
+		  nouvelObjet.idObjetFinancement = id;
+		},
+		error => {
+		  console.error('Erreur lors de la génération d\'ID Objet Financement :', error);
+		}
+		this.objetsFinancements.push(nouvelObjet);
+		this.extractedInitialFinancement.objetFinancement.push(nouvelObjet);
 
-  // Appelez la méthode generateIdObjetFinancement et abonnez-vous pour obtenir l'ID
-  this.idGeneratorService.generateIdObjetFinancement().subscribe(
-    id => {
-      nouvelObjet.idObjetFinancement = id; // Assignez l'ID à la propriété
-      console.log('ID Objet Financement généré :', id);
-      
-      // Maintenant, vous pouvez utiliser `nouvelObjet` avec son `idObjetFinancement` assigné
-    },
-    error => {
-      console.error('Erreur lors de la génération d\'ID Objet Financement :', error);
-      // Gérez l'erreur selon vos besoins
-    }
-  );
-
-  // Si vous avez besoin de retourner `nouvelObjet`, vous devrez attendre l'assignation de l'ID.
-  // Une façon de gérer cela pourrait être de retourner un Observable ou de gérer la logique après l'abonnement.
-}
+			
+	}
+No overload matches this call.
+  The last overload gave the following error.
+    Argument of type 'number' is not assignable to parameter of type '() => void'.ts(2769)
+Observable.d.ts(54, 5): The last overload is declared here.
+(property) FilArianeComponent.objetsFinancements: ObjetFinancement[]
