705c6a705f5f23b5abf93ea03156626f861dbb8b
ziedenit <guendil_zied@yahoo.com>
2024-08-06 16:56:43 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,22 +1,24 @@
- onBreadcrumbClick(index: number , currentObjet : any) {
-			this.updateCurrentObjetData();
-			this.selectedObjetIndex = index;
-			this.depExist=true;
-        this.mapFinancementData(this.extractedInitialFinancement);
-        this.setObjetFinancementProperties(currentObjet);
-		this.traiterPiecesJustificatives(this.extractedInitialFinancement);
-        this.setFinancementDataOnScreen(this.extractedInitialFinancement);
-        this.setupFormGroup(this.extractedInitialFinancement.objetFinancement[index].bien);
-		this.checkRequiredFields(this.extractedInitialFinancement);
-			console.log("l'objet selectionnée est ")
-			console.log(this.objetsFinancements[index])
-			console.log("la liste des objets actuel est de taille", this.objetsFinancements.length)
-			console.log(this.objetsFinancements);
-			this.extractedInitialFinancement.objetFinancement=this.objetsFinancements;
-			console.log("financement a patcher avec les valeurs actuels mis a jour", this.extractedInitialFinancement)
-
-		  }
-ajouterObjetFinancement() {
+<div class="container-fluid">
+  <!-- File d'Ariane pour les objets de financement -->
+  <nav aria-label="breadcrumb" class="breadcrumb-nav">
+    <ol class="breadcrumb-custom"   *ngIf="showFileAriane==true">
+      <li class="breadcrumb-item-custom" *ngFor="let objet of objetsFinancements; let i = index">
+        <a [routerLink]=""
+          queryParamsHandling="preserve" (click)="onBreadcrumbClick(i, objet)">
+         <span>Objet de financement</span>  {{ i + 1 }}
+        </a>
+        <button class="close-btn" (click)="removeBreadcrumbItem(i)">x</button>
+      </li>
+    </ol>
+    <div class="button-container" >
+      <button class="btn btn-primary btn-sm" (click)="ajouterObjetFinancement()">Ajouter un nouvel objet de financement
+        <img src="../../../assets/icons/plus.svg" />
+      </button>
+    </div>
+  </nav>
+</div>
+	  ajouterObjetFinancement() {
+	this.showDeleteIcon =true;
 	this.showFileAriane = true;
 	const nouvelObjet: ObjetFinancement = {
 	  idObjetFinancement: "",
@@ -39,27 +41,63 @@ ajouterObjetFinancement() {
 	  id => {
 		nouvelObjet.idObjetFinancement = id;
 		this.objetsFinancements.push(nouvelObjet);
-		this.extractedInitialFinancement.objetFinancement.push(nouvelObjet);
+		this.extractedInitialFinancement.objetFinancement=this.objetsFinancements;
 	  },
 	  error => {
 		console.error('Erreur lors de la génération d\'ID Objet Financement :', error);
 	  }
 	);
+	
   }
-j'ai un probleme sur le button ajouter <div class="button-container" >
-      <button class="btn btn-primary btn-sm" (click)="ajouterObjetFinancement()">Ajouter un nouvel objet de financement
-        <img src="../../../assets/icons/plus.svg" />
-      </button>
-    </div>
-  en cliquant la premiere fois sur le button ca ajouter un seul element de la fil ariane mais en clicnt un fois sur le premier objet et en recliquant ca ajoute deux objets au lieu d'un seul<div class="container-fluid">
-  <!-- File d'Ariane pour les objets de financement -->
-  <nav aria-label="breadcrumb" class="breadcrumb-nav">
-    <ol class="breadcrumb-custom"   *ngIf="showFileAriane==true">
-      <li class="breadcrumb-item-custom" *ngFor="let objet of objetsFinancements; let i = index">
-        <a [routerLink]=""
-          queryParamsHandling="preserve" (click)="onBreadcrumbClick(i, objet)">
-         <span>Objet de financement</span>  {{ i + 1 }}
-        </a>
-        <button class="close-btn" (click)="removeBreadcrumbItem(i)">x</button>
-      </li>
-    </ol>
+	
+	ngOnInit(): void {
+	this.showFileAriane=false;
+    this.id = this.route.snapshot.queryParams["idFinancement"];
+    this.Url_Retour_Base64 = this.route.snapshot.queryParams["urlRetour"];
+  
+    this.financementService.getFinancementbyId(this.id).subscribe(responseFinancement => {
+    this.extractedInitialFinancement=responseFinancement;
+        this.mapFinancementData(responseFinancement);
+		this.mapObjetFinancementData(responseFinancement.objetFinancement[0]);
+        this.setObjetFinancementProperties(responseFinancement.objetFinancement[0]);
+     this.checkRequiredFields(responseFinancement.objetFinancement[0]);
+		this.traiterPiecesJustificatives(responseFinancement);
+        this.setFinancementDataOnScreen(responseFinancement);
+        this.setupFormGroup(responseFinancement.objetFinancement[0].bien);
+		this.formGroup.get('numeroNomRue').valueChanges.subscribe(numeroNomRue=>
+			{	this.searchAddress();});
+		this.formGroup.get('codePostal').valueChanges.subscribe(codePostal=>
+			{	this.searchAddress();});
+		this.formGroup.get('ville').valueChanges.subscribe(ville=>
+			{	this.searchAddress();});
+      
+     
+    });
+
+	
+    if (this.Url_Retour_Base64) {this.Url_Retour_Utf8 = atob(this.Url_Retour_Base64);}
+    this.multiObjetService.getListeObjetsFinancement(this.id).subscribe(objets => {
+      this.objetsFinancements = objets;
+	  this.showDeleteIcon=false;
+	  this.sizeStoredObject=this.objetsFinancements.length;
+	  console.log( "this.sizeStoredObject")
+	  console.log(this.sizeStoredObject)
+	  if(this.objetsFinancements.length>1)
+	  {
+		this.showFileAriane=true;
+	  }
+    });
+}
+j'ai ce bouton  <button class="close-btn" *ngIf="showDeleteIcon==true&& i>sizeStoredObject"(click)="removeBreadcrumbItem(i)">x</button>
+    que je dois l'afficher uniquement si l'objet qui est dans la file est ajouter via le click sur a ajouter c'est a dire non restitué dans cette partie   if (this.Url_Retour_Base64) {this.Url_Retour_Utf8 = atob(this.Url_Retour_Base64);}
+    this.multiObjetService.getListeObjetsFinancement(this.id).subscribe(objets => {
+      this.objetsFinancements = objets;
+	  this.showDeleteIcon=false;
+	  this.sizeStoredObject=this.objetsFinancements.length;
+	  console.log( "this.sizeStoredObject")
+	  console.log(this.sizeStoredObject)
+	  if(this.objetsFinancements.length>1)
+	  {
+		this.showFileAriane=true;
+	  }
+    });
