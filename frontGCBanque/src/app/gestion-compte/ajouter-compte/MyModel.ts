30dc6a0a6c28058efba6e197d936e11e3c0f9e8d
ziedenit <guendil_zied@yahoo.com>
2024-08-07 15:46:22 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,51 +1,58 @@
 ngOnInit(): void {
-	this.showFileAriane=false;
-	this.id = this.route.snapshot.queryParams["idFinancement"];
+    this.showFileAriane = false;
+    this.id = this.route.snapshot.queryParams["idFinancement"];
     this.Url_Retour_Base64 = this.route.snapshot.queryParams["urlRetour"];
 
-    if (this.Url_Retour_Base64) {this.Url_Retour_Utf8 = atob(this.Url_Retour_Base64);}
+    if (this.Url_Retour_Base64) {
+        this.Url_Retour_Utf8 = atob(this.Url_Retour_Base64);
+    }
+
+    // Récupérer le financement par ID
     this.financementService.getFinancementbyId(this.id).subscribe(responseFinancement => {
-		console.log("le financement récupéré de la BDD est:",responseFinancement )
-		this.extractedInitialFinancement=responseFinancement;
+        console.log("le financement récupéré de la BDD est:", responseFinancement);
+        this.extractedInitialFinancement = responseFinancement;
         this.mapFinancementData(responseFinancement);
         this.setObjetFinancementProperties(responseFinancement.objetFinancement[0]);
         this.checkRequiredFields(responseFinancement);
-		this.traiterPiecesJustificatives(responseFinancement);
+        this.traiterPiecesJustificatives(responseFinancement);
         this.setFinancementDataOnScreen(responseFinancement);
         this.setupFormGroup(responseFinancement.objetFinancement[0].bien);
 
-		this.formGroup.get('numeroNomRue').valueChanges.subscribe(numeroNomRue=>
-			{	this.searchAddress();});
-		this.formGroup.get('codePostal').valueChanges.subscribe(codePostal=>
-			{	this.searchAddress();});
-		this.formGroup.get('ville').valueChanges.subscribe(ville=>
-			{	this.searchAddress();});
+        this.formGroup.get('numeroNomRue').valueChanges.subscribe(numeroNomRue => {
+            this.searchAddress();
+        });
+        this.formGroup.get('codePostal').valueChanges.subscribe(codePostal => {
+            this.searchAddress();
+        });
+        this.formGroup.get('ville').valueChanges.subscribe(ville => {
+            this.searchAddress();
+        });
     });
 
-	
-   
+    // Récupérer la liste des objets de financement
     this.multiObjetService.getListeObjetsFinancement(this.id).subscribe(objets => {
-      this.objetsFinancements = objets;
-	  this.showDeleteIcon=false;
-	 // faire appel au méthode des regles meties et appliquer ca sur l'enselmble des objet du financement on passant l'index 
-	 //    this.mapFinancementData(responseFinancement);
-	 //this.setObjetFinancementProperties(responseFinancement.objetFinancement[index]);
-	/*  this.checkRequiredFields(responseFinancement);
-	 this.traiterPiecesJustificatives(responseFinancement);
-	 this.setFinancementDataOnScreen(responseFinancement);
-	 this.setupFormGroup(responseFinancement.objetFinancement[index]].bien); */
-	 // 
-	  if(this.objetsFinancements.length>1)
-	  {
-		this.showFileAriane=true;
-	  }
+        this.objetsFinancements = objets;
+        this.showDeleteIcon = false;
+
+        // Si plusieurs objets, afficher le fil d'Ariane
+        if (this.objetsFinancements.length > 1) {
+            this.showFileAriane = true;
+        }
+
+        // Appliquer les méthodes métier à chaque objet de financement
+        this.objetsFinancements.forEach((objet, index) => {
+            this.applyBusinessRules(objet, index);
+        });
     });
 }
-j'ai ce traitement dans le ngOnit lors du deuxieme subsribe je recupere la listobjetfinancement et je veux pour chaque objet de objet financement faire les appels suivant 
-  //    this.mapFinancementData(responseFinancement);
-	 //this.setObjetFinancementProperties(responseFinancement.objetFinancement[index]);
-	/*  this.checkRequiredFields(responseFinancement);
-	 this.traiterPiecesJustificatives(responseFinancement);
-	 this.setFinancementDataOnScreen(responseFinancement);
-	 this.setupFormGroup(responseFinancement.objetFinancement[index]].bien); */
-mais a la place de responseFinancement c'est le this.extractedInitialFinancement a laquel je dois affecter la liste des objet recuprer     this.objetsFinancements et aussi looper sur la la liste et faire les appel suivant pour chaque objet de la liste example (index)
+
+// Méthode pour appliquer les règles métier à chaque objet de financement
+applyBusinessRules(objet: ObjetFinancement, index: number): void {
+    this.extractedInitialFinancement.objetFinancement[index] = objet;
+    this.mapFinancementData(this.extractedInitialFinancement);
+    this.setObjetFinancementProperties(objet);
+    this.checkRequiredFields(this.extractedInitialFinancement);
+    this.traiterPiecesJustificatives(this.extractedInitialFinancement);
+    this.setFinancementDataOnScreen(this.extractedInitialFinancement);
+    this.setupFormGroup(objet.bien);
+}
