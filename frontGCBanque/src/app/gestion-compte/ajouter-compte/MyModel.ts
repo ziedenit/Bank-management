4e1fbb884d8837e7515ba193988680fad5ef5dca
ziedenit <guendil_zied@yahoo.com>
2024-08-08 11:41:17 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,78 +1,82 @@
-ngOnInit(): void {
-    // ... Autres initialisations
-    this.formGroup.valueChanges.subscribe(changes => {
-        this.saveChangesToObjetFinancement(this.selectedObjetIndex, changes);
-    });
-}
+package com.cl.msofd.controller;
 
-ngOnDestroy(): void {
-    // Avant de détruire le composant, assurez-vous que les changements sont sauvegardés.
-    if (this.selectedObjetIndex !== null) {
-        this.saveChangesToObjetFinancement(this.selectedObjetIndex, this.formGroup.value);
+import com.cl.logs.commun.CommonLogger;
+import com.cl.logs.commun.CommonLoggerFactory;
+import com.cl.logs.types.EventTyp;
+import com.cl.logs.types.SecEventTyp;
+import com.cl.msofd.exception.DpeAdemeNotFoundException;
+import com.cl.msofd.exception.ErrorResponse;
+import com.cl.msofd.model.DpeAdeme;
+import com.cl.msofd.service.DpeAdemeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.annotation.Resource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
+@RestController
+@RequestMapping("/api/v1")
+public class AdemeController {
+
+    @Resource
+    private DpeAdemeService dpeAdemeService;
+
+    private final CommonLogger commonLogger = CommonLoggerFactory.getLogger(AdemeController.class);
+
+    @Operation(summary = "Get ademe dpe", description = "Returns a dpe object retrieved from the ademe API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation"),
+            @ApiResponse(responseCode = "404", description = "Not Found", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content(schema = @Schema())),
+            @ApiResponse(responseCode = "400", description = "Bad request", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+    })
+    @GetMapping("ademe/dpe/{numDpe}")
+    public ResponseEntity<DpeAdeme> getDpe(
+            @Parameter(description = "ademe number = dpe number: unique identifier for each dpe document", required = true)
+            @PathVariable(value = "numDpe") String numDpe) throws IOException, ExecutionException, InterruptedException {
+
+        commonLogger.eventTyp(EventTyp.APPLICATIVE).secEventTyp(SecEventTyp.METIER).logger().info("getDpeAdeme : {}", numDpe);
+
+        try {
+            DpeAdeme dpeAdeme = dpeAdemeService.getDpe(numDpe);
+            return ResponseEntity.ok(dpeAdeme);
+        } catch (InterruptedException ie) {
+            commonLogger.eventTyp(EventTyp.APPLICATIVE).secEventTyp(SecEventTyp.METIER).logger().error("InterruptedException: ", ie);
+            Thread.currentThread().interrupt();  // Réinterrompre le thread
+            return ResponseEntity.status(500).body(null);
+        } catch (ExecutionException ee) {
+            commonLogger.eventTyp(EventTyp.APPLICATIVE).secEventTyp(SecEventTyp.METIER).logger().error("ExecutionException: ", ee);
+            return ResponseEntity.status(500).body(null);
+        } catch (DpeAdemeNotFoundException ademeE) {
+            try {
+                DpeAdeme dpeAdemeNeuf = dpeAdemeService.getDpeNeuf(numDpe);
+                return ResponseEntity.ok(dpeAdemeNeuf);
+            } catch (Exception e) {
+                 if (e instanceof InterruptedException) {
+                    Thread.currentThread().interrupt();
+                }
+                commonLogger.eventTyp(EventTyp.APPLICATIVE).secEventTyp(SecEventTyp.METIER).logger().error("Exception: ", e);
+                return ResponseEntity.status(500).body(null);
+            }
+        }
     }
 }
-
-private saveChangesToObjetFinancement(index: number, changes: any): void {
-    const objetFinancement = this.extractedInitialFinancement.objetFinancement[index];
-    if (!objetFinancement) return;
-
-    // Mettre à jour les propriétés de l'objet de financement avec les changements du formulaire
-    Object.assign(objetFinancement.bien, changes);
-}
-
-//////
-onBreadcrumbClick(index: number): void {
-    // Sauvegarde les modifications de l'objet en cours
-    this.saveChangesToObjetFinancement(this.selectedObjetIndex, this.formGroup.value);
+je veux modifier ce controlleur pour faire appel a ce deux aussi dans ce controlleur apres l'appel   DpeAdeme dpeAdemeNeuf = dpeAdemeService.getDpeNeuf(numDpe);
+    public DpeAdeme getDpeNeufTertiaire(String numDpe) throws ExecutionException, InterruptedException, IOException {
+        return getDpeByUrl(Constants.ADEME_URL_Tertiaire_Neuf, numDpe);
+    }
     
-    // Met à jour l'index sélectionné
-    this.selectedObjetIndex = index;
-
-    // Applique les règles métier à l'objet sélectionné
-    this.mapFinancementData(this.extractedInitialFinancement, index);
-    this.setObjetFinancementProperties(this.extractedInitialFinancement.objetFinancement[index]);
-    this.checkRequiredFields(this.extractedInitialFinancement.objetFinancement[index]);
-    this.traiterPiecesJustificatives(this.extractedInitialFinancement, index);
-    this.setFinancementDataOnScreen(this.extractedInitialFinancement, index);
-    this.setupFormGroup(this.extractedInitialFinancement.objetFinancement[index].bien);
-}
-//
-ajouterObjetFinancement(): void {
-    this.showDeleteIcon = true;
-    this.showFileAriane = true;
-
-    const nouvelObjet: ObjetFinancement = {
-        idObjetFinancement: "",
-        codeObjetFinancement: "",
-        quotePartObjet: null,
-        gainCEP: null,
-        dateFinTravaux: null,
-        bien: new Bien(),
-        dpeAvantTravaux: new Dpe(),
-        dpeApresTravaux: new Dpe(),
-        alignement: new Alignement(),
-        eligibilite: new Eligibilite(),
-        piecesJustificatives: [],
-        codeFamilleObjet: "",
-        garantie: [],
-        firstDisconnectionOfd: false,
-    };
-
-    this.idGeneratorService.generateIdObjetFinancement().subscribe(
-        id => {
-            nouvelObjet.idObjetFinancement = id;
-            this.objetsFinancements.push(nouvelObjet);
-            this.extractedInitialFinancement.objetFinancement = this.objetsFinancements;
-            this.manuallyAddedIndices.push(this.objetsFinancements.length - 1);
-
-            // Met à jour l'index sélectionné
-            this.selectedObjetIndex = this.objetsFinancements.length - 1;
-            
-            // Passe à l'édition du nouvel objet
-            this.setupFormGroup(nouvelObjet.bien);
-        },
-        error => {
-            console.error('Erreur lors de la génération d\'ID Objet Financement :', error);
-        }
-    );
-}
+    public DpeAdeme getDpeAncienTertiaire(String numDpe) throws ExecutionException, InterruptedException, IOException {
+        return getDpeByUrl(Constants.ADEME_URL_Tertiaire_Ancien, numDpe);
+    }
