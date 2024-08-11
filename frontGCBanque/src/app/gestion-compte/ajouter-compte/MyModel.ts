8a99a51ca476c26470d08f8519e9c722fa2fa13f
ziedenit <guendil_zied@yahoo.com>
2024-08-11 20:53:26 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,3 +1,17 @@
-this.formGroup.get('anneeConstruction').valueChanges.subscribe(value => {
-      console.log('Value changed:', value);
-    });
+dateInferieureAujourdhuiValidator(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const value = control.value?.toString().trim();
+    if (!value) {
+      return null; // Skip validation for empty values
+    }
+
+    const currentYear = new Date().getFullYear();
+    const inputYear = parseInt(value, 10);
+
+    if (isNaN(inputYear) || inputYear > currentYear) {
+      return { invalidDate: true };
+    }
+
+    return null;
+  };
+}
