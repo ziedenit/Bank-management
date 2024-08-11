702a88911d4577e2da464ac3b438c015d8711c57
ziedenit <guendil_zied@yahoo.com>
2024-08-11 20:05:35 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -6,25 +6,15 @@ import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 })
 export class ValidationService {
 
-  constructor() { }
-
-  dateInferieureAujourdhuiValidator(): ValidatorFn {
+  anneeConstructionValidator(): ValidatorFn {
     return (control: AbstractControl): ValidationErrors | null => {
       const currentYear = new Date().getFullYear();
-      const inputYear = parseInt(control.value, 10);
+      const anneeConstruction = parseInt(control.value, 10);
 
-      // Check if the value is a valid number and if it is greater than the current year
-      if (inputYear > currentYear) {
-        control.setValue(''); // Clear the field if the year is in the future
-        return { invalidDate: true };
+      if (isNaN(anneeConstruction) || anneeConstruction > currentYear) {
+        return { anneeConstructionInvalide: true };
       }
-
       return null;
     };
   }
-}
-if (inputYear > currentYear) {
-    console.log('Année invalide, champ réinitialisé');
-    control.setValue(''); // Clear the field if the year is in the future
-    return { invalidDate: true };
 }
