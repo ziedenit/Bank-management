07a659fc8e0daa1efd34afe67885361f0d39feb0
ziedenit <guendil_zied@yahoo.com>
2024-08-11 19:29:27 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,1 +1,50 @@
-tomcat-embed-core-10.1.25.jar dans quel version de spring pour corriger la vulnerabilté 
+<div class="col-lg-3 col-md-6">
+                        <label for="anneeConstruction" class="d-block dpeLabel">Année de construction  <span class="required" *ngIf="selectedNatBatiment=='option1'">*</span></label>
+                       
+                        <input type="text" id="anneeConstruction"  formControlName="anneeConstruction" name="anneeConstruction" class="form-control">
+                      </div>
+
+
+                          j'ai ce champs dans un formulaire anneeConstruction je veux ajouter un controle pour que ce champs ne soit pas superieure a la date du jour date courant 
+                          ce champs fait partie de ce model ( c'est u_n champs string )
+import { Dpe } from './dpe';
+export class Bien {
+    idBien:string;
+    codeBatiment:string;
+    codeNormeThermique:string;
+    typeBatiment:string;
+    codePostal:string;
+    nomCommune:string;
+    adresseComplete:string;
+    anneeConstruction:string;
+    dateDepotPc:string;
+    surfaceBien:number;
+    bienFinanceLCL:boolean;
+    dpeActuel:Dpe;
+    etatBien:string;
+    numeroVoie:string;
+    nomRue:string;
+    prixBien: number;
+    montantFinanceLCL:number;
+    partLCL:number;
+    typeUsage:string;
+    numeroNomRue:string;
+
+    typeEnergie:string;
+    batiment:string;
+    escalier:string;
+    etage: string;
+    porte:string;
+
+    typeVoie:string;
+    codeDepartement:string;
+    codeInseeCommune:string;
+    numeroLot:string;
+    periodeConstruction:string;
+    coordonneeCartographiqueX:number;
+    coordonneeCartographiqueY:number;
+    dateDebutConstruction:Date;
+
+    eligibleDpe:string;
+
+}
