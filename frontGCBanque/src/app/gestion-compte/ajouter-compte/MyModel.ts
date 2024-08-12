8c2702cf5589a8167aa72519731cbf166b9cc4c2
ziedenit <guendil_zied@yahoo.com>
2024-08-12 12:11:12 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,3 +1,4 @@
- static createDefault(): Alignement {
-        return new Alignement('', '', '', '', null, null);
-    }
+Expected 6 arguments, but got 0.ts(2554)
+alignement.ts(16, 8): An argument for 'topAlignement' was not provided.
+(alias) new Alignement(topAlignement: string, xtra275TopAlignement: string, topAlignementXtra: string, xtra275TopAlignementXtra: string, topXtra274: string, topXtra276: string): Alignement
+import Alignement
