3266368d8361a39a888bbbf84a954a32a510c02d
ziedenit <guendil_zied@yahoo.com>
2024-08-12 17:55:56 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,48 +1,97 @@
-public Financement patchFinancementChamps(String idFinancement, Financement financementToUpdate) throws InvocationTargetException, IllegalAccessException {
-    Financement existingFinancement = getFinancementByIdFinancement(idFinancement);
+ @Test
+    void should_patch_existing_financement_par_champs() throws Exception {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
 
-    // Update simple fields of Financement
-    merge(existingFinancement, financementToUpdate);
+        Dpe dpeActuel = Dpe.builder()
+                .classeCep("A")
+                .classeGes("C")
+                .estimationCep(60.0)
+                .estimationGes(80.0)
+                .numeroDpe("DDD7E1287825F")
+                .build();
 
-    // Update ObjetFinancement list
-    if (financementToUpdate.getObjetFinancement() != null) {
-        for (ObjetFinancement updatedObjet : financementToUpdate.getObjetFinancement()) {
-            boolean found = false;
-            for (ObjetFinancement existingObjet : existingFinancement.getObjetFinancement()) {
-                if (existingObjet.getIdObjetFinancement().equals(updatedObjet.getIdObjetFinancement())) {
-                    merge(existingObjet, updatedObjet);
-                    found = true;
-                    break;
-                }
-            }
-            if (!found) {
-                existingFinancement.getObjetFinancement().add(updatedObjet);
-            }
-        }
-    }
+        Bien bien = Bien.builder()
+                .bienFinanceLCL(true)
+                .codeBatiment("00001")
+                .typeBatiment("Appartement")
+                .codePostal("75016")
+                .surfaceBien(79.0)
+                .anneeConstruction(2000)
+                .dpeActuel(dpeActuel)
+                .montantFinanceLCL(900000.00)
+                .build();
 
-    // Save and delete the existing Financement
-    Financement savedFinancement = financementRepository.save(existingFinancement);
-    financementRepository.deleteByIdFinancement(existingFinancement.getIdFinancement());
+        List<Garantie> listGarantie = new ArrayList<>();
+        Garantie garantie = Garantie.builder()
+                .idGarantie("G12345678")
+                .bien(bien)
+                .build();
+        listGarantie.add(garantie);
 
-    return savedFinancement;
-}
 
-private void merge(Object target, Object source) throws InvocationTargetException, IllegalAccessException {
-    Map<String, Method> targetSetters = new HashMap<>();
-    for (Method method : target.getClass().getMethods()) {
-        if (method.getName().startsWith("set") && method.getParameterCount() == 1) {
-            targetSetters.put(method.getName().substring(3).toLowerCase(), method);
-        }
-    }
-    for (Method method : source.getClass().getMethods()) {
-        if (method.getName().startsWith("get") && method.getParameterCount() == 0) {
-            String fieldName = method.getName().substring(3).toLowerCase();
-            Object value = method.invoke(source);
-            if (value != null && targetSetters.containsKey(fieldName)) {
-                Method setter = targetSetters.get(fieldName);
-                setter.invoke(target, value);
-            }
-        }
-    }
-}
+        ObjetFinancement object = ObjetFinancement.builder()
+        		.idObjetFinancement("O12345678")
+                .codeObjetFinancement("02")
+                .quotePartObjet(100.0)
+                .bien(bien)
+                .garantie(listGarantie)
+                .build();
+
+        List<String> idRepers = new ArrayList<>();
+        idRepers.add("8415595180");
+        Intervenant intervenant = Intervenant.builder()
+                .idReper(idRepers)
+                .build();
+        ArrayList<ObjetFinancement> listObjets = new ArrayList();
+        listObjets.add(object);
+        
+        
+        Financement financementExistant = Financement.builder()
+                .idFinancement("F12345670")                
+                .objetFinancement(listObjets)
+                .intervenant(intervenant)
+                .build();
+
+        Financement createdFiancement = financementService.createFinancement(financementExistant);
+
+
+        Dpe dpeAct = Dpe.builder()
+                .classeCep("A")
+                .estimationCep(60.0)
+                .build();
+
+        Bien bienObjet = Bien.builder()
+                .codePostal("75018")
+                .surfaceBien(120.0)
+                .dpeActuel(dpeAct)
+                .build();
+
+        ObjetFinancement objectFinancement = ObjetFinancement.builder()
+        		.idObjetFinancement("O12345678")
+                .codeObjetFinancement("02")
+                .quotePartObjet(100.0)
+                .bien(bienObjet)
+                .build();
+        ArrayList<ObjetFinancement> listObjetsTest = new ArrayList();
+        listObjetsTest.add(objectFinancement);
+        
+        Financement patchedFinancement = Financement.builder()
+        		.indicateurFinancementDedie("Y")
+                .alignement(Alignement.builder()
+                		.topAlignement("01")
+                		.topAlignementXtra("06")
+                		.build())
+                .objetFinancement(listObjetsTest)
+                .build();
+
+
+        MvcResult result = mockMvc.perform(patch("/api/v1/financement/champs/" + createdFiancement.getIdFinancement()).content(asJsonString(patchedFinancement))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        JSONObject jsonObject = new JSONObject(result.getResponse().getContentAsString());
+        assertNotNull(jsonObject.getString("objetFinancement"));
+        assertNotNull(jsonObject.getString("alignement")); 
+        assertNotNull(jsonObject.getString("indicateurFinancementDedie"));
