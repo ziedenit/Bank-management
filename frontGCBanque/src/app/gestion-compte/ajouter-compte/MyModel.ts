cafc7fd0948146696aa80d7965fc6e2c54a4168f
ziedenit <guendil_zied@yahoo.com>
2024-08-12 11:26:51 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,5 +1,37 @@
-import { Injectable } from '@angular/core'; 
+class Alignement {
+    constructor(
+        public topAlignement: string = '',
+        public xtra275TopAlignement: string = '',
+        public topAlignementXtra: string = '',
+        public xtra275TopAlignementXtra: string = '',
+        public topXtra274: string | null = null,
+        public topXtra276: string | null = null
+    ) {}
 
+    // Méthode statique pour faciliter la création d'instances
+    static create(init?: Partial<Alignement>): Alignement {
+        return new Alignement(
+            init?.topAlignement ?? '',
+            init?.xtra275TopAlignement ?? '',
+            init?.topAlignementXtra ?? '',
+            init?.xtra275TopAlignementXtra ?? '',
+            init?.topXtra274 ?? null,
+            init?.topXtra276 ?? null
+        );
+    }
+}
+//
+// Utilisation directe du constructeur
+const alignement1 = new Alignement('topAlign1', 'Y', 'topAlignXtra1', 'N', null, null);
+
+// Utilisation de la méthode create
+const alignement2 = Alignement.create({
+    topAlignement: 'topAlign2',
+    xtra275TopAlignement: 'N',
+    topAlignementXtra: 'topAlignXtra2',
+    xtra275TopAlignementXtra: 'Y'
+});
+//
 @Injectable({
     providedIn: 'root'
 })
@@ -20,6 +52,7 @@ export class AlignmentService {
         let newTopAlignementXtra = topAlignementXtra;
 
         try {
+            // Traitement de topAlignement
             switch (topAlignement) {
                 case this.ALIGNE_01:
                 case this.ALIGNE_02:
@@ -40,6 +73,7 @@ export class AlignmentService {
                     break;
             }
 
+            // Traitement de topAlignementXtra
             switch (topAlignementXtra) {
                 case this.ALIGNE_01:
                 case this.ALIGNE_02:
@@ -62,81 +96,15 @@ export class AlignmentService {
         } catch (e) {
             xtra275TopAlignement = 'N';
             xtra275TopAlignementXtra = 'N';
-                   console.error('Une erreur est survenue:', e);
         }
 
-        return Alignement.builder()
-            .topAlignement(newTopAlignement)
-            .xtra275TopAlignement(xtra275TopAlignement)
-            .topAlignementXtra(newTopAlignementXtra)
-            .xtra275TopAlignementXtra(xtra275TopAlignementXtra)
-            .topXtra274(null)
-            .topXtra276(null)
-            .build();
-    }
-}
-//
-class Alignement {
-    constructor(
-        public topAlignement: string,
-        public xtra275TopAlignement: string,
-        public topAlignementXtra: string,
-        public xtra275TopAlignementXtra: string,
-        public topXtra274: string | null = null,
-        public topXtra276: string | null = null
-    ) {}
-
-    static builder() {
-        return new AlignementBuilder();
-    }
-}
-
-class AlignementBuilder {
-    private topAlignement: string = '';
-    private xtra275TopAlignement: string = '';
-    private topAlignementXtra: string = '';
-    private xtra275TopAlignementXtra: string = '';
-    private topXtra274: string | null = null;
-    private topXtra276: string | null = null;
-
-    topAlignement(value: string): AlignementBuilder {
-        this.topAlignement = value;
-        return this;
-    }
-
-    xtra275TopAlignement(value: string): AlignementBuilder {
-        this.xtra275TopAlignement = value;
-        return this;
-    }
-
-    topAlignementXtra(value: string): AlignementBuilder {
-        this.topAlignementXtra = value;
-        return this;
-    }
-
-    xtra275TopAlignementXtra(value: string): AlignementBuilder {
-        this.xtra275TopAlignementXtra = value;
-        return this;
-    }
-
-    topXtra274(value: string | null): AlignementBuilder {
-        this.topXtra274 = value;
-        return this;
-    }
-
-    topXtra276(value: string | null): AlignementBuilder {
-        this.topXtra276 = value;
-        return this;
-    }
-
-    build(): Alignement {
-        return new Alignement(
-            this.topAlignement,
-            this.xtra275TopAlignement,
-            this.topAlignementXtra,
-            this.xtra275TopAlignementXtra,
-            this.topXtra274,
-            this.topXtra276
-        );
+        return Alignement.create({
+            topAlignement: newTopAlignement,
+            xtra275TopAlignement: xtra275TopAlignement,
+            topAlignementXtra: newTopAlignementXtra,
+            xtra275TopAlignementXtra: xtra275TopAlignementXtra,
+            topXtra274: null,
+            topXtra276: null
+        });
     }
 }
