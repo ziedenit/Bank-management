579b8a4e96e647bfd82fbcca15a81323e5d70280
ziedenit <guendil_zied@yahoo.com>
2024-08-13 18:34:27 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,18 +1,3 @@
- this.saveCurrentObjectValues();
- this.saveCurrentObjectValues();
-private saveCurrentObjectValues(): void {
-  // Sauvegarde des valeurs dans l'objet de financement actuel
-  if (this.extractedInitialFinancement && this.extractedInitialFinancement.objetFinancement) {
-    const currentObj = this.extractedInitialFinancement.objetFinancement[this.selectedObjetIndex];
-    if (currentObj && currentObj.bien) {
-      const bien = currentObj.bien;
-      bien.partLCL = this.partLcl;
-      bien.montantFinanceLCL = this.montantLclFinance;
-      bien.prixBien = this.prixAquisitionBien;
-      bien.dateDepotPc = this.dateDepot ? new Date(this.dateDepot).toISOString() : null;
-      bien.codeNormeThermique = this.getCodeNormeThermique(this.normeThermique);
-      bien.sirenDiagnostiqueur = this.SirenDPE;
-      bien.numeroDpe = this.numeroDpeAdeme;
-    }
-  }
-}
+Property 'getCodeNormeThermique' does not exist on type 'MultiOfdComponent'. Did you mean 'normeThermique'?ts(2551)
+multi-ofd.component.ts(119, 2): 'normeThermique' is declared here.
+any
