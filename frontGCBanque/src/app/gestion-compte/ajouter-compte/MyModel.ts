b64beeaa6a3d03b8fe284efa896053bdc302aa4a
ziedenit <guendil_zied@yahoo.com>
2024-08-13 14:16:02 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,58 +1,57 @@
-// Écoute des changements pour chaque champ du formulaire
-    this.formGroup.get('numeroNomRue').valueChanges.subscribe(value => {
-        this.extractedInitialFinancement.objetFinancement[index].bien.numeroNomRue = value;
+// Fonction appelée lors du changement d'objet dans le fil d'Ariane
+onBreadcrumbClick(index: number): void {
+    // Sauvegarder les valeurs actuelles du formulaire avant de passer à un autre objet
+    const bienActuel = this.extractedInitialFinancement.objetFinancement[this.selectedObjetIndex]?.bien;
+    if (bienActuel) {
+        this.updateFinancementObject(bienActuel, this.formGroup.value);
+    }
+
+    this.selectedObjetIndex = index;
+    
+    // Recharger les nouvelles valeurs dans le formulaire
+    this.setObjetFinancementProperties(this.extractedInitialFinancement.objetFinancement[index]);
+    this.mapFinancementDataMultiOfd(this.extractedInitialFinancement, index);
+    this.setupFormGroup(this.extractedInitialFinancement.objetFinancement[index].bien, index);
+    this.setFinancementDataOnScreenMultiOfd(this.extractedInitialFinancement, index);
+    this.depExist = true;
+}
+//
+private setupFormGroup(bien: any, index: number): void {
+    if (!bien) return;
+    this.formGroup = this.fb.group({
+        numeroNomRue: [bien.numeroNomRue || null],
+        codePostal: [bien.codePostal || null],
+        ville: [bien.nomCommune || null],
+        dateDiangnostique: [bien.dpeActuel?.dateEtablissementDpe?.toString().substring(0, 10) || null],
+        anneeConstruction: [bien.anneeConstruction || null],
+        typeBatiment: [bien.typeBatiment || null],
+        lettreCEP: [bien.dpeActuel?.classeCep || null],
+        lettreGES: [bien.dpeActuel?.classeGes || null],
+        surfaceBien: [bien.surfaceBien || null],
+        valeurCep: [bien.dpeActuel?.estimationCep || null],
+        valeurGes: [bien.dpeActuel?.estimationGes || null],
+        energieType: [bien.typeEnergie || null]
     });
 
-    this.formGroup.get('codePostal').valueChanges.subscribe(value => {
-        this.extractedInitialFinancement.objetFinancement[index].bien.codePostal = value;
+    // Sauvegarde les valeurs actuelles du formulaire dans l'objet correspondant dans extractedInitialFinancement
+    this.formGroup.valueChanges.subscribe(value => {
+        this.updateFinancementObject(bien, value);
     });
+}
 
-    this.formGroup.get('ville').valueChanges.subscribe(value => {
-        this.extractedInitialFinancement.objetFinancement[index].bien.nomCommune = value;
-    });
-
-    this.formGroup.get('dateDiangnostique').valueChanges.subscribe(value => {
-        if (this.extractedInitialFinancement.objetFinancement[index].bien.dpeActuel) {
-            this.extractedInitialFinancement.objetFinancement[index].bien.dpeActuel.dateEtablissementDpe = value;
-        }
-    });
-
-    this.formGroup.get('anneeConstruction').valueChanges.subscribe(value => {
-        this.extractedInitialFinancement.objetFinancement[index].bien.anneeConstruction = value;
-    });
-
-    this.formGroup.get('typeBatiment').valueChanges.subscribe(value => {
-        this.extractedInitialFinancement.objetFinancement[index].bien.typeBatiment = value;
-    });
-
-    this.formGroup.get('lettreCEP').valueChanges.subscribe(value => {
-        if (this.extractedInitialFinancement.objetFinancement[index].bien.dpeActuel) {
-            this.extractedInitialFinancement.objetFinancement[index].bien.dpeActuel.classeCep = value;
-        }
-    });
-
-    this.formGroup.get('lettreGES').valueChanges.subscribe(value => {
-        if (this.extractedInitialFinancement.objetFinancement[index].bien.dpeActuel) {
-            this.extractedInitialFinancement.objetFinancement[index].bien.dpeActuel.classeGes = value;
-        }
-    });
-
-    this.formGroup.get('surfaceBien').valueChanges.subscribe(value => {
-        this.extractedInitialFinancement.objetFinancement[index].bien.surfaceBien = value;
-    });
-
-    this.formGroup.get('valeurCep').valueChanges.subscribe(value => {
-        if (this.extractedInitialFinancement.objetFinancement[index].bien.dpeActuel) {
-            this.extractedInitialFinancement.objetFinancement[index].bien.dpeActuel.estimationCep = value;
-        }
-    });
-
-    this.formGroup.get('valeurGes').valueChanges.subscribe(value => {
-        if (this.extractedInitialFinancement.objetFinancement[index].bien.dpeActuel) {
-            this.extractedInitialFinancement.objetFinancement[index].bien.dpeActuel.estimationGes = value;
-        }
-    });
-
-    this.formGroup.get('energieType').valueChanges.subscribe(value => {
-        this.extractedInitialFinancement.objetFinancement[index].bien.typeEnergie = value;
-    });
+// Fonction qui met à jour les propriétés de l'objet en fonction des valeurs du formulaire
+private updateFinancementObject(bien: any, value: any): void {
+    bien.numeroNomRue = value.numeroNomRue;
+    bien.codePostal = value.codePostal;
+    bien.nomCommune = value.ville;
+    bien.dpeActuel = bien.dpeActuel || {};
+    bien.dpeActuel.dateEtablissementDpe = value.dateDiangnostique;
+    bien.anneeConstruction = value.anneeConstruction;
+    bien.typeBatiment = value.typeBatiment;
+    bien.dpeActuel.classeCep = value.lettreCEP;
+    bien.dpeActuel.classeGes = value.lettreGES;
+    bien.surfaceBien = value.surfaceBien;
+    bien.dpeActuel.estimationCep = value.valeurCep;
+    bien.dpeActuel.estimationGes = value.valeurGes;
+    bien.typeEnergie = value.energieType;
+}
