af0b246ee8f2a8ac92f89fc71a4860ae9c5de972
ziedenit <guendil_zied@yahoo.com>
2024-08-13 18:21:40 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -206,3 +206,60 @@
 
 </div> 
 </div>
+ci haus le code du formulaire que je gere en ngmodel les diffirent champs et ci bas le code a completer par des validateur sur les champs a chaque fois ou un champs change de valeur en changent d'element de fil ( en appelant la méthode onBreadcrumbClick)
+private setObjetFinancementProperties(objetFinancement: any): void {
+	switch (objetFinancement.codeObjetFinancement) {
+  
+		case "02":
+			this.objetFinance = "Acquisition de bâtiment";
+			break;
+		case "03":
+			this.objetFinance = "Rénovation de bâtiment";
+			this.depExist = true;
+			break;
+		default:
+			break;
+	}
+    if (!objetFinancement || !objetFinancement.bien) return;
+    const bien = objetFinancement.bien;
+
+	if (bien.eligibleDpe) {
+        this.selectedType = this.eligibleDpeMapping[bien.eligibleDpe].type;
+        this.hideFieldForm = this.eligibleDpeMapping[bien.eligibleDpe].hideFieldForm;
+      
+      
+      }
+
+	if (bien.etatBien) {this.selectedNatBatiment = this.etatBienMapping[bien.etatBien];}
+    if (bien.codeBatiment) {this.codeBatimentSelected = this.codeBatimentMapping[bien.codeBatiment];}
+    
+    this.partLcl = bien?.partLCL;
+    this.montantLclFinance = bien?.montantFinanceLCL;
+	this.prixAquisitionBien = bien?.prixBien;
+
+	this.dateDepot = bien?.dateDepotPc ? formatDate(bien.dateDepotPc, 'yyyy-MM-dd', "en-US") : undefined;
+    if (bien.codeNormeThermique) {this.normeThermique = this.codeNormeThermiqueMapping[bien.codeNormeThermique];}
+    if (bien.dpeActuel && bien.dpeActuel.sirenDiagnostiqueur) { this.SirenDPE = bien.dpeActuel.sirenDiagnostiqueur;}
+    this.numeroDpeAdeme = bien.dpeActuel?.numeroDpe;
+
+	if (objetFinancement.alignement && objetFinancement.alignement.topAlignement) {
+        this.DpeResults = true;
+        this.alignementResultText = this.alignementMapping[objetFinancement.alignement.topAlignement];}
+
+// ecouteur sur les champs pour detecter et sauvgarder les changement sur l'objet
+
+      }
+//
+ onBreadcrumbClick(index: number ) {
+			this.selectedObjetIndex=index;
+			 // Appliquer les regles metier sur l'element selectionné du fil Ariane a revoir l'adaptation?
+       this.setObjetFinancementProperties(this.extractedInitialFinancement.objetFinancement[index]);
+       this.mapFinancementDataMultiOfd(this.extractedInitialFinancement,index)
+			this.setupFormGroup(this.extractedInitialFinancement.objetFinancement[index].bien,index); 
+      this.setFinancementDataOnScreenMultiOfd(this.extractedInitialFinancement,index)
+			this.depExist=true; 
+		  }
+		  removeBreadcrumbItem(index: number) {
+			this.objetsFinancements.splice(index, 1);
+			this.extractedInitialFinancement.objetFinancement=this.objetsFinancements;
+		  }
