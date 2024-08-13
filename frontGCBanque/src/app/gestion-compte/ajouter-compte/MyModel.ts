38b14848eba0b05a891170bfa71fd8826cd72e44
ziedenit <guendil_zied@yahoo.com>
2024-08-13 14:34:07 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,8 +1,8 @@
 export class VotreComposant implements OnInit {
-    selectedObjetIndex: number;
+    selectedObjetIndex: number = 0;
     formGroup: FormGroup;
     extractedInitialFinancement: any;
-    
+
     ngOnInit(): void {
         this.id = this.route.snapshot.queryParams["idFinancement"];
         this.Url_Retour_Base64 = this.route.snapshot.queryParams["urlRetour"];
@@ -15,7 +15,7 @@ export class VotreComposant implements OnInit {
             this.extractedInitialFinancement = responseFinancement;
             console.log("Le financement récupéré de la BDD est:", responseFinancement);
 
-            this.selectedObjetIndex = 0; // Initialisation sur le premier objet
+            // Initialisation sur le premier objet
             this.setObjetFinancementProperties(responseFinancement.objetFinancement[0]);
             this.setupFormGroup(responseFinancement.objetFinancement[0].bien);
         });
@@ -48,43 +48,33 @@ export class VotreComposant implements OnInit {
             valeurGes: [bien.dpeActuel?.estimationGes || null],
             energieType: [bien.typeEnergie || null]
         });
-
-        // Sauvegarder les modifications du formulaire dans l'objet bien correspondant
-        this.formGroup.valueChanges.subscribe(value => {
-            this.updateFinancementObject(this.extractedInitialFinancement.objetFinancement[this.selectedObjetIndex].bien, value);
-        });
     }
 
-    private updateFinancementObject(bien: any, value: any): void {
-        bien.numeroNomRue = value.numeroNomRue;
-        bien.codePostal = value.codePostal;
-        bien.nomCommune = value.ville;
+    private saveFormData(): void {
+        const bien = this.extractedInitialFinancement.objetFinancement[this.selectedObjetIndex].bien;
+        if (!bien) return;
+
+        // Mettre à jour l'objet bien avec les données du formulaire
+        bien.numeroNomRue = this.formGroup.get('numeroNomRue').value;
+        bien.codePostal = this.formGroup.get('codePostal').value;
+        bien.nomCommune = this.formGroup.get('ville').value;
         bien.dpeActuel = bien.dpeActuel || {};
-        bien.dpeActuel.dateEtablissementDpe = value.dateDiangnostique;
-        bien.anneeConstruction = value.anneeConstruction;
-        bien.typeBatiment = value.typeBatiment;
-        bien.dpeActuel.classeCep = value.lettreCEP;
-        bien.dpeActuel.classeGes = value.lettreGES;
-        bien.surfaceBien = value.surfaceBien;
-        bien.dpeActuel.estimationCep = value.valeurCep;
-        bien.dpeActuel.estimationGes = value.valeurGes;
-        bien.typeEnergie = value.energieType;
+        bien.dpeActuel.dateEtablissementDpe = this.formGroup.get('dateDiangnostique').value;
+        bien.anneeConstruction = this.formGroup.get('anneeConstruction').value;
+        bien.typeBatiment = this.formGroup.get('typeBatiment').value;
+        bien.dpeActuel.classeCep = this.formGroup.get('lettreCEP').value;
+        bien.dpeActuel.classeGes = this.formGroup.get('lettreGES').value;
+        bien.surfaceBien = this.formGroup.get('surfaceBien').value;
+        bien.dpeActuel.estimationCep = this.formGroup.get('valeurCep').value;
+        bien.dpeActuel.estimationGes = this.formGroup.get('valeurGes').value;
+        bien.typeEnergie = this.formGroup.get('energieType').value;
     }
 
     onBreadcrumbClick(index: number): void {
-        if (this.selectedObjetIndex !== undefined) {
-            // Sauvegarder les valeurs actuelles avant de passer à un autre objet
-            const bienActuel = this.extractedInitialFinancement.objetFinancement[this.selectedObjetIndex]?.bien;
-            if (bienActuel) {
-                this.updateFinancementObject(bienActuel, this.formGroup.value);
-            }
-        }
+        // Sauvegarder les valeurs actuelles avant de passer à un autre objet
+        this.saveFormData();
 
-        // Si l'utilisateur clique sur l'objet déjà sélectionné, on ne recharge pas le formulaire
-        if (this.selectedObjetIndex === index) {
-            return;
-        }
-
+        // Passer au nouvel objet
         this.selectedObjetIndex = index;
 
         // Recharger les nouvelles valeurs dans le formulaire à partir de extractedInitialFinancement
