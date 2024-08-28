9a39ea501ffc8ea1f80f60e72fb197199f2c5435
ziedenit <guendil_zied@yahoo.com>
2024-08-28 10:12:00 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,24 +1,9 @@
-const allObjects = this.objetsFinancements.length;
-const missingIndexes = [];
-
-for (let i = 0; i < allObjects; i++) {
-    if (!this.calculedAlignementIndexObjects.includes(i)) {
-        missingIndexes.push(i + 1);
-    }
-}
-
-let message = '';
-if (missingIndexes.length > 0) {
-    message = `Les indices non inclus dans calculedAlignementIndexObjects sont : `;
-    missingIndexes.forEach((index, i) => {
-        message += `indice (${index})`;
-        if (i < missingIndexes.length - 1) {
-            message += ' , '; // Add comma and space for separation
-        }
-    });
-} else {
-    message = "Tous les indices sont inclus dans calculedAlignementIndexObjects.";
-}
-
-// Now you can bind this `message` variable to your HTML
-console.log(message);
+<div class="container-fluid" >
+        <div class=footer>
+        <div class="ButtonsFooter" style="display: flex; justify-content: flex-end">
+        <button (click)="showAlignement()"   mat-raised-button color="primary" [disabled]="selectedType!='option1'">Calculer</button>
+         &nbsp; 
+       <button  mat-raised-button color="primary"  (click)="postContinuer()" >Continuer</button>
+      </div>
+      </div>           
+</div>
