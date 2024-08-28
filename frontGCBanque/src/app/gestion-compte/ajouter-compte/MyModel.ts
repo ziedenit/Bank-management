8574cef57736718b83f695372652d6314beb6e68
ziedenit <guendil_zied@yahoo.com>
2024-08-28 10:13:50 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,9 +1,19 @@
-<div class="container-fluid" >
-        <div class=footer>
-        <div class="ButtonsFooter" style="display: flex; justify-content: flex-end">
-        <button (click)="showAlignement()"   mat-raised-button color="primary" [disabled]="selectedType!='option1'">Calculer</button>
-         &nbsp; 
-       <button  mat-raised-button color="primary"  (click)="postContinuer()" >Continuer</button>
-      </div>
-      </div>           
+<div class="container-fluid">
+    <div class="footer">
+        <div class="ButtonsFooter" style="display: flex; justify-content: space-between; align-items: center;">
+            <!-- Ajoutez cet élément pour afficher le message avec les classes conditionnelles -->
+            <span 
+                *ngIf="message" 
+                [ngClass]="{'message-red': message.includes('Les indices non inclus'), 'message-green': !message.includes('Les indices non inclus')}" 
+                style="margin-right: auto;">
+                {{ message }}
+            </span>
+            
+            <div style="display: flex;">
+                <button (click)="showAlignement()" mat-raised-button color="primary" [disabled]="selectedType!='option1'">Calculer</button>
+                &nbsp;
+                <button mat-raised-button color="primary" (click)="postContinuer()">Continuer</button>
+            </div>
+        </div>
+    </div>           
 </div>
