98640c6d1479bd75009f3202d76af2647e7f9c88
ziedenit <guendil_zied@yahoo.com>
2024-08-28 10:14:58 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,19 +1,7 @@
-<div class="container-fluid">
-    <div class="footer">
-        <div class="ButtonsFooter" style="display: flex; justify-content: space-between; align-items: center;">
-            <!-- Ajoutez cet élément pour afficher le message avec les classes conditionnelles -->
-            <span 
-                *ngIf="message" 
-                [ngClass]="{'message-red': message.includes('Les indices non inclus'), 'message-green': !message.includes('Les indices non inclus')}" 
-                style="margin-right: auto;">
-                {{ message }}
-            </span>
-            
-            <div style="display: flex;">
-                <button (click)="showAlignement()" mat-raised-button color="primary" [disabled]="selectedType!='option1'">Calculer</button>
-                &nbsp;
-                <button mat-raised-button color="primary" (click)="postContinuer()">Continuer</button>
-            </div>
-        </div>
-    </div>           
-</div>
+.message-red {
+    color: red;
+}
+
+.message-green {
+    color: green;
+}
