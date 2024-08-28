c5d34edef49e7b891ea9420a2a4e5df2dc36d1bc
ziedenit <guendil_zied@yahoo.com>
2024-08-28 09:46:26 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,6 +1,5 @@
-const bien = currentObject.bien;
-
-    // Ensure dpeActuel exists, initialize if necessary
-    if (!bien.dpeActuel) {
-        bien.dpeActuel = new Dpe();
-    }
+const allObjects=this.objetsFinancements.length;
+		if(this.calculedAlignementIndexObjects.length<allObjects)
+            console.log("les indexs non inclus dans allObjects  ")
+si allObjects objet egal a 4 par example et this.calculedAlignementIndexObjects = [0,1,2] il faut que j'afficher l'indice 3 qui n'est pas inclus dans
+ calculedAlignementIndexObjects        
