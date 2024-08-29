46863cb6de0d672b20198122be5ba9710768b599
ziedenit <guendil_zied@yahoo.com>
2024-08-29 17:50:10 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,1 +1,1 @@
-[disabled]="isButtonDisabled"
+je veux maintenant  ajouter un  bulle d'info en rapprochant le curseur sur le button deja grisé qui permets d'afficher le message suivant Merci de calculer l'alignement pour chaque objet ajouté a  la liste 
