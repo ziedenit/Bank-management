05690736d8484793108044f60ea12ff52e0cf592
ziedenit <guendil_zied@yahoo.com>
2024-09-03 16:02:22 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,7 +1,11 @@
 this.evaluatedIndex.push(index);
-// Vérifie si tous les éléments de evaluatedIndex sont présents dans manuallyAddedIndices
-const allEvaluatedAreAdded = this.evaluatedIndex.every(evalIndex => this.manuallyAddedIndices.includes(evalIndex));
 
-if ((index == this.newIndex || index == 0) && allEvaluatedAreAdded) {
+// Vérifie si tous les éléments de manuallyAddedIndices sont présents dans evaluatedIndex
+const allManuallyAddedAreEvaluated = this.manuallyAddedIndices.every(manualIndex => this.evaluatedIndex.includes(manualIndex));
+
+// Vérifie si manuallyAddedIndices est vide
+const isManuallyAddedEmpty = this.manuallyAddedIndices.length === 0;
+
+if ((index == this.newIndex) || (index == 0 && (allManuallyAddedAreEvaluated || isManuallyAddedEmpty))) {
     this.ajoutFinancementDisabled = false;
 }
