2bd5c291a7f8a2a281fcda1d3a4cdc9ac55c32a8
ziedenit <guendil_zied@yahoo.com>
2024-09-06 12:19:13 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,11 +1,21 @@
-this.evaluatedIndex.push(index);
+<ErrorResponse>
+    <code>TECHNICAL_EXCEPTION_OCCURED </code>
+    <message>
+        <message>JSON parse error: Cannot deserialize value of type `java.util.Date` from String "30/09/2012": not a valid representation (error: Failed to parse Date value '30/09/2012': Cannot parse date "30/09/2012": not compatible with any of standard forms ("yyyy-MM-dd'T'HH:mm:ss.SSSX", "yyyy-MM-dd'T'HH:mm:ss.SSS", "EEE, dd MMM yyyy HH:mm:ss zzz", "yyyy-MM-dd"))</message>
+    </message>
+</ErrorResponse>
 
-// Vérifie si tous les éléments de manuallyAddedIndices sont présents dans evaluatedIndex
-const allManuallyAddedAreEvaluated = this.manuallyAddedIndices.every(manualIndex => this.evaluatedIndex.includes(manualIndex));
-
-// Vérifie si manuallyAddedIndices est vide
-const isManuallyAddedEmpty = this.manuallyAddedIndices.length === 0;
-
-if ((index == this.newIndex) || (index == 0 && (allManuallyAddedAreEvaluated || isManuallyAddedEmpty))) {
-    this.ajoutFinancementDisabled = false;
+        je suis en train de tester un service et mon json et le suivant dans postman
+{
+        "eligibileDPE" : true,
+        "dateDepotPc" : "30/09/2012",
+        "dpePresent" :true,
+        "presenceDpeJustificatif" : true,
+        "dateConstructionDpe":null,
+        "etiquetteDpe" : "A",
+        "valeurCep": 0.0,
+        "normeThermique" : "",
+        "presenceNormeThermiqueJustificatif" : null
 }
+
+je veux que le champs date depot pc soit accepté 
