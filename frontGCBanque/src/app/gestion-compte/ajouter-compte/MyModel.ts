0f2f676db5dbd7b9899c25d533875b610ffa240a
ziedenit <guendil_zied@yahoo.com>
2024-09-09 14:24:41 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,17 +1,21 @@
- private saveCurrentObjectValues(currentObject: ObjetFinancement)
-.......
-if (this.isDpeChecked) {
-			currentObject.piecesJustificatives.push({
-            typePiece: this.selectedOptionJustif === "DPE" ? "DPE" : "Compromis de vente",
-            dpeActuel: this.selectedOptionJustif === "DPE",
-            numeroDpe: this.numeroDpeAdeme
-          });
-        }    
-        if (this.isNormeThermiqueChecked) {
-			currentObject.piecesJustificatives.push({ typePiece: "Norme thermique" });
-        }
-        if (this.isDateDepotChecked) {
-			currentObject.piecesJustificatives.push({ typePiece: "Permis de construire" });
-        }
-Argument of type '{ typePiece: string; dpeActuel: boolean; numeroDpe: string; }' is not assignable to parameter of type 'Piece'.
-  Type '{ typePiece: string; dpeActuel: boolean; numeroDpe: string; }' is missing the following properties from type 'Piece': id, origineCreation, dateCreation, origineModification, and 4 more.
+private saveCurrentObjectValues(currentObject: ObjetFinancement) {
+    if (this.isDpeChecked) {
+        const newPiece = new Piece();
+        newPiece.typePiece = this.selectedOptionJustif === "DPE" ? "DPE" : "Compromis de vente";
+        newPiece.dpeActuel = this.selectedOptionJustif === "DPE";
+        newPiece.numeroDpe = this.numeroDpeAdeme;
+        currentObject.piecesJustificatives.push(newPiece);
+    }
+
+    if (this.isNormeThermiqueChecked) {
+        const newPiece = new Piece();
+        newPiece.typePiece = "Norme thermique";
+        currentObject.piecesJustificatives.push(newPiece);
+    }
+
+    if (this.isDateDepotChecked) {
+        const newPiece = new Piece();
+        newPiece.typePiece = "Permis de construire";
+        currentObject.piecesJustificatives.push(newPiece);
+    }
+}
