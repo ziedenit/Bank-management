6edc3eebcc3facf3a09a39c029f711c8fb9d669c
ziedenit <guendil_zied@yahoo.com>
2024-09-09 22:39:08 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,68 +1,41 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
-
-@Component({
-  selector: 'app-justificatifs',
-  templateUrl: './justificatifs.component.html',
-  styleUrls: ['./justificatifs.component.scss']
-})
-export class JustificatifsComponent {
-
-  @Input() hideFieldForm: boolean;
-  @Input() dateDepot: boolean;
-  @Input() numeroDpeAdeme: boolean;
-  @Input() normeThermique: string;
-  @Input() selectedOptionJustif: string;
-
-  @Input() isDpeChecked:boolean
-  @Input() isDateDepotChecked: boolean 
-  @Input() isNormeThermiqueChecked: boolean
-
-  @Output() isDateDepotCheckedChange = new EventEmitter<boolean>();
-  @Output() isDpeCheckedChange = new EventEmitter<boolean>();
-  @Output() isNormeThermiqueCheckedChange = new EventEmitter<boolean>();
-  @Output() hiddenJustificatifChange = new EventEmitter<boolean>();
-  @Output() selectedOptionJustifChange = new EventEmitter<string>(); 
-
-  hiddenJustificatif=false;
-  elementJustificatif=true;
-  
-
-  onSelectedOptionJustifChange(event: any) 
-  {
-    console.log("coucouuuu justif",this.selectedOptionJustif)
-    this.selectedOptionJustif = event.target.value;
-    this.selectedOptionJustifChange.emit(this.selectedOptionJustif); 
-    
-    }
-
-  onDateDepotCheckedChange(event: any) {
-    this.isDateDepotChecked = event.target.checked;
-    this.isDateDepotCheckedChange.emit(this.isDateDepotChecked);
-   
+<app-justificatifs
+  [hideFieldForm]="hideFieldForm"
+  [dateDepot]="dateDepot"
+  [numeroDpeAdeme]="numeroDpeAdeme"
+  [normeThermique]="normeThermique"
+  [isDpeChecked]="isDpeChecked"
+  [isDateDepotChecked]="isDateDepotChecked"
+  [isNormeThermiqueChecked]="isNormeThermiqueChecked"
+  [selectedOptionJustif]="selectedOptionJustif"
+  (isDpeCheckedChange)="handleDpeCheckedChange($event)"
+  (isDateDepotCheckedChange)="handleDateDepotCheckedChange($event)"
+  (isNormeThermiqueCheckedChange)="handleNormeThermiqueCheckedChange($event)"
+  (selectedOptionJustifChange)="handleSelectedOptionJustifChange($event)">
+</app-justificatifs>
+   // Méthode pour gérer le changement de DPE
+  handleDpeCheckedChange(isChecked: boolean): void {
+    this.isDpeChecked = isChecked;
+    this.saveCurrentObjectValues(this.extractedInitialFinancement.objetFinancement[this.selectedObjetIndex]);
+    this.checkPiecesJustificatives(this.extractedInitialFinancement, this.selectedObjetIndex);
   }
 
-  onDpeCheckedChange(event: any) {
-    this.isDpeChecked = event.target.checked;
-    this.isDpeCheckedChange.emit(this.isDpeChecked);
-    
+  // Méthode pour gérer le changement de la date de dépôt
+  handleDateDepotCheckedChange(isChecked: boolean): void {
+    this.isDateDepotChecked = isChecked;
+    this.saveCurrentObjectValues(this.extractedInitialFinancement.objetFinancement[this.selectedObjetIndex]);
+    this.checkPiecesJustificatives(this.extractedInitialFinancement, this.selectedObjetIndex);
   }
 
-  onNormeThermiqueCheckedChange(event: any) {
-    this.isNormeThermiqueChecked = event.target.checked;
-    this.isNormeThermiqueCheckedChange.emit(this.isNormeThermiqueChecked);
-   
+  // Méthode pour gérer le changement de la norme thermique
+  handleNormeThermiqueCheckedChange(isChecked: boolean): void {
+    this.isNormeThermiqueChecked = isChecked;
+    this.saveCurrentObjectValues(this.extractedInitialFinancement.objetFinancement[this.selectedObjetIndex]);
+    this.checkPiecesJustificatives(this.extractedInitialFinancement, this.selectedObjetIndex);
   }
 
-  hideDataJustificatif() {
-    this.hiddenJustificatif=true;
-    return (this.elementJustificatif = false);
-   }
-  showDataJustificatif() {
-    this.hiddenJustificatif=false;
-    return (this.elementJustificatif = true);
-    }
-  
-
-}
-
-
+  // Méthode pour gérer le changement de l'option justificative
+  handleSelectedOptionJustifChange(selectedOption: string): void {
+    this.selectedOptionJustif = selectedOption;
+    this.saveCurrentObjectValues(this.extractedInitialFinancement.objetFinancement[this.selectedObjetIndex]);
+    this.checkPiecesJustificatives(this.extractedInitialFinancement, this.selectedObjetIndex);
+  }
