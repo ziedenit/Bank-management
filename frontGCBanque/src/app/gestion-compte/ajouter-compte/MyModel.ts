d92bf79b1baa6e6ea9579692106ffe2e47380b4c
ziedenit <guendil_zied@yahoo.com>
2024-09-10 15:15:43 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,3 +1,46 @@
-if (!currentObject.piecesJustificatives) {
-    currentObject.piecesJustificatives = [];
-}
+ if (this.isDpeChecked) {
+			currentObject.piecesJustificatives.push({
+				typePiece: this.selectedOptionJustif === "DPE" ? "DPE" : "Compromis de vente",
+				dpeActuel: this.selectedOptionJustif === "DPE",
+				numeroDpe: this.numeroDpeAdeme,
+				id: undefined,
+				origineCreation: '',
+				dateCreation: undefined,
+				origineModification: '',
+				dateModification: undefined,
+				idPiece: '',
+				referenceGed: '',
+				sousTypePiece: ''
+			});
+        }    
+        if (this.isNormeThermiqueChecked&&currentObject.piecesJustificatives // contient deja une piece de type Norme thermique") {
+			currentObject.piecesJustificatives.push({
+				typePiece: "Norme thermique",
+				id: undefined,
+				origineCreation: '',
+				dateCreation: undefined,
+				origineModification: '',
+				dateModification: undefined,
+				idPiece: '',
+				referenceGed: '',
+				sousTypePiece: '',
+				numeroDpe: '',
+				dpeActuel: false
+			});
+		  console.log("isNormeThermiqueChecked")
+        }
+        if (this.isDateDepotChecked // contient deja une piece de type Permis de construire") {
+			currentObject.piecesJustificatives.push({
+				typePiece: "Permis de construire",
+				id: undefined,
+				origineCreation: '',
+				dateCreation: undefined,
+				origineModification: '',
+				dateModification: undefined,
+				idPiece: '',
+				referenceGed: '',
+				sousTypePiece: '',
+				numeroDpe: '',
+				dpeActuel: false
+			});
+        }
