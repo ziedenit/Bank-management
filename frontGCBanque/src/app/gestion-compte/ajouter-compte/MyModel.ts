02d669ccb5bdd9a31ac305fe3c5205f6601eb024
ziedenit <guendil_zied@yahoo.com>
2024-09-11 21:04:13 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,14 +1,20 @@
-<!--bloc Résultats-->
-<app-resultats
-  [selectedType]="selectedType"
-  [DpeResults]="DpeResults"
-  [hideFieldForm]="hideFieldForm"
-  [alignementResultText]="alignementResultText"
-  [eligibiliteDpeMessage]="eligibiliteDpeMessage"
-  [champObligatoire]="champObligatoire"
-  [donneeObligatoire]="donneeObligatoire"
-  [errorDpeMessage]="errorDpeMessage"
-  [errorNormeThermiqueMessage]="errorNormeThermiqueMessage"
-  [errorDateDepotMessage]="errorDateDepotMessage"
-  [showBlocResult]="showBlocResult">
-</app-resultats>
+onBreadcrumbClick(index: number): void {
+    // Sauvegarder les données de l'objet courant avant de changer
+    this.saveCurrentObjectValues(this.extractedInitialFinancement.objetFinancement[this.selectedObjetIndex]);
+
+    // Mettre à jour l'index de l'objet sélectionné
+    this.selectedObjetIndex = index;
+
+    // Appliquer les règles métiers sur l'objet sélectionné
+    this.setObjetFinancementData(this.extractedInitialFinancement.objetFinancement[index]);
+
+    // Réinitialiser les champs du formulaire pour l'objet sélectionné
+    this.resetFormWithCurrentObject(this.extractedInitialFinancement.objetFinancement[index]);
+
+    // Restaurer les résultats d'alignement et pièces justificatives pour l'objet
+    this.restoreAlignementResultFromObject(index);
+    this.checkRequiredFields(this.extractedInitialFinancement, index);
+    this.checkPiecesJustificatives(this.extractedInitialFinancement, index);
+
+    this.depExist = true; 
+}
