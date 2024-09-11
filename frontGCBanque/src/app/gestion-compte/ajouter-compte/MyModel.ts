ddf4fdb22fac76135d7cb9b03da134d2769cf4c6
ziedenit <guendil_zied@yahoo.com>
2024-09-11 21:10:05 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,18 +1,35 @@
-onBreadcrumbClick(index: number): void {
-    // Sauvegarder les données de l'objet courant avant de passer à l'objet sélectionné
-    this.saveCurrentObjectValues(this.extractedInitialFinancement.objetFinancement[this.selectedObjetIndex]);
+showAlignement(index: number): void {
+    // Sauvegarder les modifications avant de calculer l'alignement
+    this.prepareLigneContext();
+    this.saveCurrentObjectValues(this.extractedInitialFinancement.objetFinancement[index]);
 
-    // Mettre à jour l'index de l'objet sélectionné
-    this.selectedObjetIndex = index;
+    // Calculer l'alignement
+    forkJoin([
+        this.engineService.alignement(this.ligneContext),
+        this.engineService.alignementXtra(this.ligneContextXtra)
+    ]).subscribe(([aligne, aligneXtra]) => {
+        this.alignementResultText = this.alignementMapping[aligne];
+        this.alignementResult = aligne;
+        this.alignementXtraResult = aligneXtra;
+        this.alignementContext = this.xtraRepriseService.calculXtra(aligne, aligneXtra);
 
-    // Appliquer les règles métiers sur l'élément sélectionné du fil d'Ariane
-    this.setObjetFinancementData(this.extractedInitialFinancement.objetFinancement[index]);
-    this.checkRequiredFields(this.extractedInitialFinancement, index);
-    this.checkPiecesJustificatives(this.extractedInitialFinancement, index);
-    this.setupFormGroup(this.extractedInitialFinancement.objetFinancement[index].bien);
+        // Sauvegarder l'alignement dans l'objet courant
+        this.extractedInitialFinancement.objetFinancement[index].alignement = this.alignementContext;
 
-    // Restaurer les résultats d'alignement pour l'objet sélectionné
-    this.restoreAlignementResultFromObject(index);
+        // Sauvegarder l'état du bloc de résultats pour cet objet
+        this.saveAlignementResultInObject(index);
+    });
 
-    this.depExist = true;
+    this.errorDpeMessage = this.checkFileDpeInserted();
+    this.errorNormeThermiqueMessage = this.checkNormeThermique();
+    this.errorDateDepotMessage = this.checkFileDateDepotInserted();
+
+    this.evaluatedIndex.push(index);
+
+    const allManuallyAddedAreEvaluated = this.manuallyAddedIndices.every(manualIndex => this.evaluatedIndex.includes(manualIndex));
+    const isManuallyAddedEmpty = this.manuallyAddedIndices.length === 0;
+
+    if ((index == this.newIndex) || (index == 0 && (allManuallyAddedAreEvaluated || isManuallyAddedEmpty))) {
+        this.ajoutFinancementDisabled = false;
+    }
 }
