449b2ec6aaddbb2f775a93767d5250f401d698f8
ziedenit <guendil_zied@yahoo.com>
2024-09-11 21:11:13 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,35 +1,11 @@
-showAlignement(index: number): void {
-    // Sauvegarder les modifications avant de calculer l'alignement
-    this.prepareLigneContext();
-    this.saveCurrentObjectValues(this.extractedInitialFinancement.objetFinancement[index]);
+private resetAlignementResults(): void {
+    // Réinitialiser tous les résultats d'alignement à un état vide
+    this.alignementContext = null;
+    this.alignementResultText = '';
+    this.eligibiliteDpeMessage = '';
 
-    // Calculer l'alignement
-    forkJoin([
-        this.engineService.alignement(this.ligneContext),
-        this.engineService.alignementXtra(this.ligneContextXtra)
-    ]).subscribe(([aligne, aligneXtra]) => {
-        this.alignementResultText = this.alignementMapping[aligne];
-        this.alignementResult = aligne;
-        this.alignementXtraResult = aligneXtra;
-        this.alignementContext = this.xtraRepriseService.calculXtra(aligne, aligneXtra);
-
-        // Sauvegarder l'alignement dans l'objet courant
-        this.extractedInitialFinancement.objetFinancement[index].alignement = this.alignementContext;
-
-        // Sauvegarder l'état du bloc de résultats pour cet objet
-        this.saveAlignementResultInObject(index);
-    });
-
-    this.errorDpeMessage = this.checkFileDpeInserted();
-    this.errorNormeThermiqueMessage = this.checkNormeThermique();
-    this.errorDateDepotMessage = this.checkFileDateDepotInserted();
-
-    this.evaluatedIndex.push(index);
-
-    const allManuallyAddedAreEvaluated = this.manuallyAddedIndices.every(manualIndex => this.evaluatedIndex.includes(manualIndex));
-    const isManuallyAddedEmpty = this.manuallyAddedIndices.length === 0;
-
-    if ((index == this.newIndex) || (index == 0 && (allManuallyAddedAreEvaluated || isManuallyAddedEmpty))) {
-        this.ajoutFinancementDisabled = false;
-    }
+    // Réinitialiser l'état UI
+    this.DpeResults = false;
+    this.elementResults = false;
+    this.showBlocResult = false;
 }
