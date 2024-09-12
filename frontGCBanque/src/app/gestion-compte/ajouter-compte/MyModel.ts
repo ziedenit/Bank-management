3c1d1a2605dc83fec55ef8fb4179439ad6b64275
ziedenit <guendil_zied@yahoo.com>
2024-09-12 15:35:46 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -5,7 +5,7 @@ function onBreadcrumbClick(index: number) {
     // Vérifie si l'index existe déjà dans la map
     if (clickCounts.has(index)) {
         // Si oui, incrémente le nombre de clics pour cet index
-        const currentCount = clickCounts.get(index) ?? 0;
+        const currentCount = clickCounts.get(index)!; // On sait que la valeur existe, on peut utiliser "!" pour éviter undefined
         clickCounts.set(index, currentCount + 1);
     } else {
         // Si non, initialise le compteur de cet index à 1
