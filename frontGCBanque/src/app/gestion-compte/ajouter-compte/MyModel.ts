feb13c2366a2f3a5c97857c9fcf1c2e784ae4da9
ziedenit <guendil_zied@yahoo.com>
2024-09-15 20:47:46 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,12 +1,9 @@
-if (this.isfirstDebranchement) {
-        // Si c'est le premier débranchement, ne pas appliquer la validation (pas de bordure rouge)
-        return;
-    }
-private setObjetFinancementData(objetFinancement: ObjetFinancement): void {
-    this.idObjetFinancement = objetFinancement.idObjetFinancement;
-    this.familleObjet = objetFinancement.codeFamilleObjet;
-    this.typeObjetFinancement = objetFinancement.codeObjetFinancement;
-    
-    // Assurez-vous que `isfirstDebranchement` est initialisé correctement
-    this.isfirstDebranchement = objetFinancement.firstDisconnectionOfd ?? true; // Par défaut à true si non défini
-}
+ if (element) {
+            if (condition) {
+                this.champObligatoire = true;
+                this.donneeObligatoire = 'Donnée obligatoire';
+                element.classList.add('field-error');
+            } else {
+                element.classList.remove('field-error');
+            }
+        }
