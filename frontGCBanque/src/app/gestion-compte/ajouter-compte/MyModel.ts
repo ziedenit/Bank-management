eae35707c46db05d6526abbf85f07d8e3f6b4094
ziedenit <guendil_zied@yahoo.com>
2024-09-15 19:13:55 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,76 +1,41 @@
- onBreadcrumbClick(index: number): void {
-    this.selectedObjetIndex = index;
+<div class="container-fluid">
+  <!-- File d'Ariane pour les objets de financement -->
+  <nav aria-label="breadcrumb" class="breadcrumb-nav">
+    <ol class="breadcrumb-custom">
+      <li class="breadcrumb-item-custom" *ngFor="let objet of objetsFinancements; let i = index">
+        <a [routerLink]=""
+          queryParamsHandling="preserve"
+          (click)="onBreadcrumbClick(i)">
+          <span>Objet de financement</span> {{ i + 1 }}
+        </a>
+        <button class="close-btn" *ngIf="manuallyAddedIndices.includes(i)" (click)="removeBreadcrumbItem(i)">x</button>
+      </li>
+    </ol>
+    <div class="button-container">
+      <button class="btn btn-primary btn-sm" (click)="ajouterObjetFinancement()" [disabled]="ajoutFinancementDisabled">
+        Ajouter un nouvel objet de financement
+        <img src="../../../assets/icons/plus.svg" />
+      </button>
+    </div>
+  </nav>
+</div>
 
-    // Appliquez les règles métier à l'objet sélectionné
-    this.applyBusinessRules(this.extractedInitialFinancement, index);
-  }
-
-  // Méthode pour appliquer les règles métier à l'objet sélectionné
-  applyBusinessRules(financement: any, index: number): void {
-    // Appliquez vos règles spécifiques ici
-    this.setObjetFinancementData(financement.objetFinancement[index]);
-    this.checkRequiredFields(financement, index);
-    this.checkPiecesJustificatives(financement, index);
-    this.setupFormGroup(financement.objetFinancement[index].bien);
-  }
-
-removeBreadcrumbItem(index: number): void {
-    this.objetsFinancements.splice(index, 1);
-    const manuallyAddedIndex = this.manuallyAddedIndices.indexOf(index);
-    if (manuallyAddedIndex > -1) {
-      this.manuallyAddedIndices.splice(manuallyAddedIndex, 1);
-    }
-    if (this.selectedObjetIndex >= index) {
-      this.selectedObjetIndex = Math.max(this.selectedObjetIndex - 1, 0);
-    }
-    this.applyBusinessRules(this.extractedInitialFinancement, this.selectedObjetIndex); // Recalculer les règles métier après suppression
-  }
-
-  ajouterObjetFinancement(): void {
-    this.isDateDepotChecked = false;
-    this.isNormeThermiqueChecked = false;
-    this.isDpeChecked = false;
-    this.showBlocResult = false;
-    this.showDeleteIcon = true;
-    this.showFileAriane = true;
-
-    const nouvelObjet: ObjetFinancement = {
-      idObjetFinancement: null,
-      codeObjetFinancement: '02',
-      quotePartObjet: null,
-      gainCEP: null,
-      dateFinTravaux: null,
-      bien: this.createNewBien(),
-      dpeAvantTravaux: this.createNewDpe(),
-      dpeApresTravaux: this.createNewDpe(),
-      alignement: Alignement.createDefault(),
-      eligibilite: new Eligibilite(),
-      codeFamilleObjet: '01',
-      garantie: [],
-      firstDisconnectionOfd: true,
-      piecesJustificatives: []
-    };
-
-    this.idGeneratorService.generateIdObjetFinancement().subscribe(
-      idFinancement => {
-        nouvelObjet.idObjetFinancement = idFinancement;
-        this.idGeneratorService.generateIdBien().subscribe(
-          idBien => {
-            nouvelObjet.bien.idBien = idBien;
-            this.objetsFinancements.push(nouvelObjet);
-            this.extractedInitialFinancement.objetFinancement = [...this.objetsFinancements];
-            this.manuallyAddedIndices.push(this.objetsFinancements.length - 1);
-            this.newIndex = this.objetsFinancements.length - 1;
-            this.ajoutFinancementDisabled = true;
-            this.applyBusinessRules(this.extractedInitialFinancement, this.newIndex); // Appliquez les règles métier au nouvel objet ajouté
-          },
-          error => {
-            console.error('Erreur lors de la génération de l\'ID du bien : ', error);
-          }
-        );
-      },
-      error => {
-        console.error('Erreur lors de la génération de l\'ID de l\'objet de financement : ', error);
-      }
-    );
-  }
+<!-- Contenu pour chaque objet de financement -->
+<div class="container-fluid" *ngFor="let objet of objetsFinancements; let i = index">
+  <div class="row">
+    <div class="col-12">
+      <div class="card border-0">
+        <div class="card-body" style="border: 1px solid #b1bfeb; box-shadow: 0 2px 10px  #b1bfeb;">
+          <div class="blockSize">
+            <h3 class="d-inline-block font-weight-bold" style="font-size: 17px;">Objet de financement {{ i + 1 }}</h3>
+            <span class="float-right">
+              <img *ngIf="hiddenObjetfinancement==false" src="../../../assets/icons/arrow-up.svg" alt="Fleche haut" (click)="hideDataObjetFinancement()">
+              <img *ngIf="hiddenObjetfinancement==true" src="../../../assets/icons/arrow-down.svg" alt="Fleche bas" (click)="showDataObjetFinancement()">
+            </span>
+            <!-- Le formulaire de données pour chaque objet de financement -->
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
