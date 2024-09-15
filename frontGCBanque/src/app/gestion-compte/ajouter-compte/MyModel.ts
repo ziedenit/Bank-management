06047bd4de8dab352a30514a1f82d58f2343fc09
ziedenit <guendil_zied@yahoo.com>
2024-09-15 20:42:31 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,108 +1,82 @@
-.field-error {
-  border: 1px solid red !important;
-}
-checkFormFieldsFormGroup() {
-    const formData = this.formGroup.value;
-    console.log("La valeur de formGroup est la suivante:", this.formGroup.value);
-    this.champObligatoire = false;
-    this.donneeObligatoire = '';
+  <div *ngIf="messageErreurPartlcl" class="errorpart" > 
+                      {{messageErreurPartlcl}}
+                    </div>
+ngOnInit(): void {
 
-    const validateField = (fieldName: string, condition: boolean) => {
-        const element = document.getElementById(fieldName);
-        if (element) {
-            if (condition) {
-                this.champObligatoire = true;
-                this.donneeObligatoire = 'Donnée obligatoire';
-                element.classList.add('field-error');
-            } else {
-                element.classList.remove('field-error');
-            }
-        }
-    };
-
-    // Ajoutez toutes vos validations ici
-    validateField('prixAquisitionBien', this.prixAquisitionBien == null || this.prixAquisitionBien == 0);
-    validateField('montantLclFinance', this.montantLclFinance == null || this.montantLclFinance == 0);
-    validateField('partLcl', this.partLcl == null || this.partLcl == 0);
-    validateField('numeroDpeAdeme', this.selectedNatBatiment == 'option1' && (!this.numeroDpeAdeme));
-    validateField('SirenDPE', (this.selectedNatBatiment == 'option1' && (!this.SirenDPE)) || (this.numeroDpeAdeme && (!this.SirenDPE)));
-    // Et ainsi de suite pour les autres champs...
-}
-//
-ajouterObjetFinancement() {
-    this.formGroup.reset(); // Réinitialiser le formulaire
-
-    // Réinitialisation des variables de champ pour le nouvel objet
-    this.prixAquisitionBien = null;
-    this.montantLclFinance = null;
-    this.partLcl = null;
-    this.SirenDPE = null;
-    this.numeroDpeAdeme = null;
-    this.selectedNatBatiment = null;
-    this.codeBatimentSelected = null;
-
-    // Réinitialisation des bordures d'erreur (en cas de persistance après reset)
-    const fieldsToClear = ['prixAquisitionBien', 'montantLclFinance', 'partLcl', 'numeroDpeAdeme', 'SirenDPE'];
-    fieldsToClear.forEach(field => {
-        const element = document.getElementById(field);
-        if (element) {
-            element.classList.remove('field-error');
-        }
+    this.id = this.route.snapshot.queryParams["idFinancement"];
+    this.Url_Retour_Base64 = this.route.snapshot.queryParams["urlRetour"];
+    if (this.Url_Retour_Base64) {
+		this.Url_Retour_Utf8 = atob(this.Url_Retour_Base64);
+	}
+	else {
+		console.warn("Url_Retour_Base64 est falsy. Impossible de le convertir en Utf8.");
+	}
+    this.financementService.getFinancementbyId(this.id).subscribe(responseFinancement => {
+		this.extractedInitialFinancement = responseFinancement;
+		console.log("le financement récupéré de la BDD est le suivant:",responseFinancement )	
+        this.setFinancementData(responseFinancement);
+        this.setObjetFinancementData(responseFinancement.objetFinancement[0]);
+        this.checkRequiredFields(responseFinancement,0);
+		this.checkPiecesJustificatives(responseFinancement,0);
+        this.setupFormGroup(responseFinancement.objetFinancement[0].bien);
+		  
     });
-
-    // Création du nouvel objet de financement vierge
-    const nouvelObjet: ObjetFinancement = {
-        idObjetFinancement: null,
-        codeObjetFinancement: "02",
-        quotePartObjet: null,
-        gainCEP: null,
-        dateFinTravaux: null,
-        bien: this.createNewBien(),  // Initialise l'objet bien avec tous les champs
-        dpeAvantTravaux: this.createNewDpe(), // Initialise DPE avant travaux
-        dpeApresTravaux: this.createNewDpe(), // Initialise DPE après travaux
-        alignement: Alignement.createDefault(), // Alignement par défaut
-        eligibilite: new Eligibilite(), // Eligibilité par défaut
-        codeFamilleObjet: "01", // Famille par défaut
-        garantie: [],
-        firstDisconnectionOfd: true,
-        piecesJustificatives: []
-    };
-
-    // Générer un nouvel ID pour l'objet de financement
-    this.idGeneratorService.generateIdObjetFinancement().subscribe(
-        idFinancement => {
-            nouvelObjet.idObjetFinancement = idFinancement;
-
-            // Générer l'ID pour le bien de l'objet avant de l'ajouter
-            this.idGeneratorService.generateIdBien().subscribe(
-                idBien => {
-                    nouvelObjet.bien.idBien = idBien;
-
-                    // Ajouter le nouvel objet de financement à la liste
-                    this.objetsFinancements.push(nouvelObjet);
-                    this.extractedInitialFinancement.objetFinancement = [...this.objetsFinancements];
-
-                    // Marquer l'objet comme ajouté manuellement
-                    this.manuallyAddedIndices.push(this.objetsFinancements.length - 1);
-                    this.newIndex = this.objetsFinancements.length - 1;
-                    this.ajoutFinancementDisabled = true;
-
-                    console.log("Nouvel objet ajouté avec ID bien et financement générés", nouvelObjet);
-                },
-                error => {
-                    console.error("Erreur lors de la génération de l'ID du bien : ", error);
-                }
-            );
-        },
-        error => {
-            console.error("Erreur lors de la génération de l'ID de l'objet de financement : ", error);
-        }
-    );
+    private checkRequiredFields(responseFinancement: Financement, index:number): void {
+    const bien = responseFinancement.objetFinancement[index]?.bien;
+    const dpeActuel = bien?.dpeActuel;
+    this.checkAndHighlightRequiredField(bien && bien.etatBien == null, "natureBien");
+    this.checkAndHighlightRequiredField(bien && bien.codeBatiment == null, "categorieBatiment");
+    this.checkAndHighlightRequiredField(bien && bien.partLCL == null, "partLcl");
+    this.checkAndHighlightRequiredField(bien && bien.prixBien == null, "prixAquisitionBien");
+    this.checkAndHighlightRequiredField(bien && bien.montantFinanceLCL == null, "montantLclFinance");	
+    if (bien?.etatBien == "Ancien" && dpeActuel) {
+        this.checkAndHighlightRequiredField(dpeActuel.numeroDpe == null || dpeActuel.numeroDpe == "", "numeroDpeAdeme");
+        this.checkAndHighlightRequiredField(dpeActuel.sirenDiagnostiqueur == null || (dpeActuel.numeroDpe != null && dpeActuel.sirenDiagnostiqueur == null), "SirenDPE");
+        this.checkAndHighlightRequiredField(dpeActuel.classeCep == null || dpeActuel.classeCep == "", "LettreCEP");
+        this.checkAndHighlightRequiredField(dpeActuel.classeGes == null || dpeActuel.classeGes == "", "LettreGES");
+		this.checkAndHighlightRequiredField((dpeActuel.classeCep == null || dpeActuel.classeCep == "" ) && (this.hideFieldCEP), "LettreCEPlist");
+        this.checkAndHighlightRequiredField((dpeActuel.classeGes == null || dpeActuel.classeGes == "") && this.hideFieldGES, "lettreGeslist");
+        this.checkAndHighlightRequiredField(dpeActuel.estimationCep == null, "ValeurCEP");
+        this.checkAndHighlightRequiredField(dpeActuel.estimationGes == null, "ValeurGES");
+        this.checkAndHighlightRequiredField(bien.anneeConstruction == null, "anneeConstruction");
+    }
+    if (bien?.etatBien == "Neuf") {
+        this.checkAndHighlightRequiredField(bien.dateDepotPc == null, "dateDepot");
+				this.checkAndHighlightRequiredField(bien.dpeActuel.numeroDpe != null&& bien.dpeActuel.sirenDiagnostiqueur == null, "SirenDPE");
+    }
+    this.checkAndHighlightRequiredField(bien && (bien.numeroNomRue == null || bien.numeroNomRue == ""), "numeroNomRue");
+    this.checkAndHighlightRequiredField(bien && (bien.codePostal == null || bien.codePostal == ""), "codePostal");
+    this.checkAndHighlightRequiredField(bien && (bien.nomCommune == null || bien.nomCommune == ""), "ville");
+	this.checkAndHighlightRequiredField(
+		responseFinancement.objetFinancement[index] != null &&responseFinancement.objetFinancement[index].bien != null &&
+		(responseFinancement.objetFinancement[index].bien.etatBien == "Neuf" ||responseFinancement.objetFinancement[index].bien.etatBien == "Ancien") &&
+		responseFinancement.objetFinancement[index].bien.surfaceBien == null,
+		"SurfaceDuBien"
+	);
 }
-//
+    checkAndHighlightRequiredField(condition, elementId) {
+    console.log("condition");
+    console.log(condition);
+    console.log("this.isfirstDebranchement");
+    console.log(this.isfirstDebranchement);
+    console.log("elementId");
+    console.log(elementId);
 
-<input type="text"
-       id="prixAquisitionBien"
-       [(ngModel)]="prixAquisitionBien"
-       class="form-control form-control-sm"
-       [ngClass]="{'field-error': prixAquisitionBien == null || prixAquisitionBien == 0}" />
+    const element = document.getElementById(elementId);
+    if (element) {
+        if (condition && !this.isfirstDebranchement) {
+            this.champObligatoire = true;
+            this.donneeObligatoire = 'Donnée obligatoire';
+            element.style.border = "1px solid red";
+        } else {
+            element.style.removeProperty('border');
+        }
+    } else {
+        console.error(`Element not found: ${elementId}`);
+    }
+}
+private setObjetFinancementData(objetFinancement: ObjetFinancement): void {
+	this.idObjetFinancement = objetFinancement.idObjetFinancement;
+	this.familleObjet=objetFinancement.codeFamilleObjet;
+	this.typeObjetFinancement=objetFinancement.codeObjetFinancement;
+	this.isfirstDebranchement = objetFinancement.firstDisconnectionOfd;
