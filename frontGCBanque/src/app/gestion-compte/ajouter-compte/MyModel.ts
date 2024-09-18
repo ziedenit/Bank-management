978cbbf48483e5d5eb876d482ec4397605f2055f
ziedenit <guendil_zied@yahoo.com>
2024-09-18 18:37:55 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,8 +1,102 @@
- // Forcer la mise à jour des valeurs du formulaire et des champs ngModel
-  this.formGroup.markAllAsTouched();
-  this.formGroup.updateValueAndValidity();
+private saveCurrentObjectValues(currentObject: ObjetFinancement): void {
+  // Vérifier si les pièces justificatives existent déjà
+  const existingDpePiece = currentObject.piecesJustificatives.some(piece => piece.typePiece === 'DPE');
+  const existingCompromisPiece = currentObject.piecesJustificatives.some(piece => piece.typePiece === 'Compromis de vente');
+  const existingNormeThermiquePiece = currentObject.piecesJustificatives.some(piece => piece.typePiece === 'Norme thermique');
+  const existingPermisPiece = currentObject.piecesJustificatives.some(piece => piece.typePiece === 'Permis de construire');
 
-  // Sauvegarder explicitement les valeurs du premier objet de financement avant l'ajout du deuxième
-  this.saveCurrentObjectValues(this.extractedInitialFinancement.objetFinancement[this.selectedObjetIndex]);
+  // --------- Gestion de la pièce DPE ---------
+  if (this.isDpeChecked) {
+    // Si coché et n'existe pas, ajouter la pièce DPE
+    if (!existingDpePiece) {
+      currentObject.piecesJustificatives.push({
+        typePiece: "DPE",
+        numeroDpe: this.numeroDpeAdeme,
+        id: null,  // ou valeur réelle si existante
+        origineCreation: '',
+        dateCreation: new Date(),
+        origineModification: '',
+        dateModification: new Date(),
+        idPiece: '',
+        referenceGed: '',
+        sousTypePiece: ''
+      });
+    }
+  } else {
+    // Si décoché et existe, supprimer la pièce DPE
+    if (existingDpePiece) {
+      currentObject.piecesJustificatives = currentObject.piecesJustificatives.filter(piece => piece.typePiece !== 'DPE');
+    }
+  }
 
-  console.log('First object saved with values:', this.extractedInitialFinancement.objetFinancement[this.selectedObjetIndex]);
+  // --------- Gestion de la pièce Compromis de vente ---------
+  if (this.isDpeChecked && this.selectedOptionJustif === "Compromis de vente") {
+    // Si coché et n'existe pas, ajouter la pièce Compromis de vente
+    if (!existingCompromisPiece) {
+      currentObject.piecesJustificatives.push({
+        typePiece: "Compromis de vente",
+        id: null,  // ou valeur réelle si existante
+        origineCreation: '',
+        dateCreation: new Date(),
+        origineModification: '',
+        dateModification: new Date(),
+        idPiece: '',
+        referenceGed: '',
+        sousTypePiece: ''
+      });
+    }
+  } else {
+    // Si décoché et existe, supprimer la pièce Compromis de vente
+    if (existingCompromisPiece) {
+      currentObject.piecesJustificatives = currentObject.piecesJustificatives.filter(piece => piece.typePiece !== 'Compromis de vente');
+    }
+  }
+
+  // --------- Gestion de la pièce Norme thermique ---------
+  if (this.isNormeThermiqueChecked) {
+    if (!existingNormeThermiquePiece) {
+      currentObject.piecesJustificatives.push({
+        typePiece: "Norme thermique",
+        id: null,
+        origineCreation: '',
+        dateCreation: new Date(),
+        origineModification: '',
+        dateModification: new Date(),
+        idPiece: '',
+        referenceGed: '',
+        sousTypePiece: '',
+        numeroDpe: '',
+        dpeActuel: false
+      });
+    }
+  } else {
+    if (existingNormeThermiquePiece) {
+      currentObject.piecesJustificatives = currentObject.piecesJustificatives.filter(piece => piece.typePiece !== 'Norme thermique');
+    }
+  }
+
+  // --------- Gestion de la pièce Permis de construire ---------
+  if (this.isDateDepotChecked) {
+    if (!existingPermisPiece) {
+      currentObject.piecesJustificatives.push({
+        typePiece: "Permis de construire",
+        id: null,
+        origineCreation: '',
+        dateCreation: new Date(),
+        origineModification: '',
+        dateModification: new Date(),
+        idPiece: '',
+        referenceGed: '',
+        sousTypePiece: '',
+        numeroDpe: '',  // ou valeur réelle si nécessaire
+        dpeActuel: false
+      });
+    }
+  } else {
+    if (existingPermisPiece) {
+      currentObject.piecesJustificatives = currentObject.piecesJustificatives.filter(piece => piece.typePiece !== 'Permis de construire');
+    }
+  }
+
+  console.log('Updated pieces justificatives:', currentObject.piecesJustificatives);
+}
