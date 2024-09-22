39f1e706ad51a922001508193a0c981069ce1a84
ziedenit <guendil_zied@yahoo.com>
2024-09-22 17:39:30 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,21 +1,3 @@
- console.log("après changement, alignementResultText pour l'objet ", index, this.extractedInitialFinancement.objetFinancement[index].alignementResultText);
-//
-showAlignement(index: number): void {
-    this.checkFormFieldsFormGroup();
-    this.showBlocResult = true;
-    this.extractedInitialFinancement.objetFinancement[index].firstDisconnectionOfd = false;
-    this.saveCurrentObjectValues(this.extractedInitialFinancement.objetFinancement[index]);
-
-    // Sauvegarde de l'alignement après le calcul
-    forkJoin([
-        this.engineService.alignement(this.ligneContext),
-        this.engineService.alignementXtra(this.ligneContextXtra)            
-    ]).subscribe(([aligne, aligneXtra]) => {    
-        this.alignementResultText = this.alignementMapping[aligne];
-        console.log("alignementResultText calculé pour l'objet ", index, this.alignementResultText);
-        
-        // Sauvegarder l'alignementResultText dans l'objet correspondant
-        this.extractedInitialFinancement.objetFinancement[index].alignementResultText = this.alignementResultText;
-        console.log("alignementResultText sauvegardé pour l'objet ", index, this.extractedInitialFinancement.objetFinancement[index].alignementResultText);
-    });
-}
+alignementResultText calculé pour l'objet  0 L'actif financé a un impact positif sur l'un des 6 objectifs environnementaux de l'UE
+multi-ofd.component.ts:756 
+ alignementResultText sauvegardé pour l'objet  0 undefined
