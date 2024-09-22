e1bdc8ac976a203a45697096184e367addb549e9
ziedenit <guendil_zied@yahoo.com>
2024-09-22 17:42:06 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,3 +1,26 @@
-alignementResultText calculé pour l'objet  0 L'actif financé a un impact positif sur l'un des 6 objectifs environnementaux de l'UE
-multi-ofd.component.ts:756 
- alignementResultText sauvegardé pour l'objet  0 undefined
+import { Alignement } from './alignement';
+import { Bien } from './bien';
+import { Dpe } from './dpe';
+import { Eligibilite } from './eligibilite';
+import {  Piece } from './piece';
+import {  Garantie } from './garantie';
+
+export class ObjetFinancement {
+	[x: string]: any;
+
+    idObjetFinancement:string;
+	codeObjetFinancement:string;// 02=Acquisition  03=Travaux
+	quotePartObjet:number;
+	gainCEP:number;
+	dateFinTravaux: Date;
+	bien:Bien;
+	dpeAvantTravaux: Dpe;
+	dpeApresTravaux: Dpe;
+	alignement: Alignement;
+    eligibilite: Eligibilite;
+	piecesJustificatives : Piece [] ;
+	codeFamilleObjet: string;	
+	garantie: Garantie[]; 
+	firstDisconnectionOfd:boolean;
+
+}
