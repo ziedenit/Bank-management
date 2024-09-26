aa18056e3ee44d0c1fe7e55f2a65d711ebe2397a
ziedenit <guendil_zied@yahoo.com>
2024-09-26 16:13:30 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,5 +1,12 @@
-Charge et configure le conteneur Drools : Elle lit un fichier de règles défini par la propriété spring.drools.rules-file (par exemple, un fichier .drl ou une table de décision) situé dans le classpath de l'application, et utilise KieServices pour créer un KieContainer, qui contient toutes les règles prêtes à être exécutées.
+git push
+fatal: The upstream branch of your current branch does not match
+the name of your current branch.  To push to the upstream branch
+on the remote, use
 
-Expose le KieContainer comme un bean Spring : Elle expose le KieContainer en tant que bean Spring grâce à l'annotation @Bean, permettant à d'autres parties de l'application d'y accéder pour exécuter les règles.
+    git push origin HEAD:develop
 
-Cela permet d'intégrer le moteur de règles Drools dans une application Spring de manière automatique et fluide.
+To push to the branch of the same name on the remote, use
+
+    git push origin HEAD
+
+To choose either option permanently, see push.default in 'git help config'.
