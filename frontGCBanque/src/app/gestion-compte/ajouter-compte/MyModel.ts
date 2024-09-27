f9c9691ffa93a7f5aaea9ae714be94466c4dbd4b
ziedenit <guendil_zied@yahoo.com>
2024-09-27 15:59:01 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,2 +1,9 @@
-Regles de gestion : ajout de pop up + griser le button continuer ( quand il ya une modification )
-Regles de gestion : limiter le numbre d'objet a ajouter 
+Objet : Définition des règles de gestion pour la multi-acquisition (fil Ariane)
+
+Bonjour,
+
+Dans le cadre du développement du fil Ariane pour la multi-acquisition, MOA et MOE ont défini ensemble les deux règles de gestion suivantes :
+
+Ajout d'une pop-up et désactivation (grisement) du bouton "Continuer" en cas de modification des données.
+Limitation du nombre d'objets pouvant être ajoutés.
+Ces règles seront prises en compte dans les prochaines étapes du développement.
