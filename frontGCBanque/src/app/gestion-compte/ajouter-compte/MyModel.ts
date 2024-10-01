95ace2effc4e6971d94e0d22fd2fe3a841c4832d
ziedenit <guendil_zied@yahoo.com>
2024-10-01 12:07:22 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,24 +1,8 @@
-import { Component } from '@angular/core';
+ postContinuer(): void {
 
-@Component({
-  selector: 'app-your-component',
-  templateUrl: './your-component.component.html',
-  styleUrls: ['./your-component.component.css'],
-})
-export class YourComponent {
-  dateDepot: string;
-  hideFieldForm = false;
-  isFieldsDisabled = false;
-  alertDisplayed: boolean = false; // Drapeau pour suivre si l'alerte a été affichée
-
-  // Méthode pour afficher l'alerte ou le pop-up
-  recalculerAlignment(): void {
-    if (!this.alertDisplayed) { // Vérifier si l'alerte n'a pas encore été affichée
-      alert("Merci de recalculer l'alignement suite à ce changement.");
-      // Ou utilisez un pop-up personnalisé ici si vous le souhaitez
-      this.alertDisplayed = true; // Mettre à jour le drapeau pour éviter les futurs affichages
-    }
-  }
-}
-//
+			this.callPatchFinancement(this.id, this.extractedInitialFinancement);
+			console.log("le financement à patcher est le suivant:",this.extractedInitialFinancement) 
+			//window.location.href = this.Url_Retour_Utf8; *	
+		
+	}
 
