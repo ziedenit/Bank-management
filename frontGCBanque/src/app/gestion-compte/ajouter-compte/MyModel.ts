1060c40b182859f1552c6f84561516185df437e9
ziedenit <guendil_zied@yahoo.com>
2024-10-01 11:45:42 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,32 +1,14 @@
-export class VotreComponent {
-  isPopUpShown: boolean = false; // Indicateur pour savoir si le pop-up a déjà été affiché
+disableContinuerButton()
+		{
 
-  // Autres propriétés ici...
+		}
 
-  // Méthode pour afficher le pop-up
-  showPopUp() {
-    if (!this.isPopUpShown) {
-      // Afficher le pop-up ici, par exemple avec un service de modal ou une simple alerte
-      alert('Vous avez modifié une valeur.'); // Exemple d'alerte simple
 
-      // Marquer que le pop-up a été affiché
-      this.isPopUpShown = true;
-    }
-  }
-}
-
-monitorFieldChange(getFieldValue: () => any) {
-  const initialValue = getFieldValue();
-
-  return {
-    unsubscribe: () => {}, // Méthode vide si vous n'utilisez pas d'abonnement réel
-    // Lorsqu'il y a un changement, on affiche le pop-up une seule fois
-    detectChange: () => {
-      if (initialValue !== getFieldValue()) {
-        // Appeler la méthode pour afficher le pop-up une seule fois
-        this.showPopUp();
-      }
-    }
-  };
-}
-
+   <div class="col-lg-4">
+              <div class="form-group" *ngIf="hideFieldForm==false">
+                  <div class="custom-label">
+                      <label for="dateDepot">Date de dépôt de permis de construire <span class="required" *ngIf="selectedNatBatiment=='option2'"> *</span></label>
+                      <input type="date" class="form-control form-control-sm" id="dateDepot" [(ngModel)]="dateDepot" placeholder="yyyy/MM/dd" [disabled]="isFieldsDisabled" (change)="checkDate()" (ngModelChange)="disableContinuerButton()" />
+                  </div>
+              </div>
+          </div>
