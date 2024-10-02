ab383fe81820a5734177930b4b3456a7f31f8a87
ziedenit <guendil_zied@yahoo.com>
2024-10-02 10:54:30 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,10 +1,48 @@
-recalculerAlignment() {
-			this.postDisabled=true;
-			if (!this.alertDisplayed) { 
-			  alert("Merci de calculer l'alignement suite à ce changement.");
-			  this.alertDisplayed = true; 
-			}
-		  }
+import { Component, EventEmitter, Output } from '@angular/core';
 
-  @Output() postDisabled : boolean;
-  @Output() alertDisplayed : boolean; 
+@Component({
+  selector: 'app-votre-composant',
+  templateUrl: './votre-composant.component.html',
+  styleUrls: ['./votre-composant.component.css']
+})
+export class VotreComposant {
+  
+  @Output() postDisabledChange: EventEmitter<boolean> = new EventEmitter<boolean>();
+  @Output() alertDisplayedChange: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+  postDisabled: boolean = false;
+  alertDisplayed: boolean = false;
+
+  recalculerAlignment() {
+    this.postDisabled = true;
+    this.postDisabledChange.emit(this.postDisabled); // Émettre la nouvelle valeur de postDisabled
+
+    if (!this.alertDisplayed) {
+      alert("Merci de calculer l'alignement suite à ce changement.");
+      this.alertDisplayed = true;
+      this.alertDisplayedChange.emit(this.alertDisplayed); // Émettre la nouvelle valeur de alertDisplayed
+    }
+  }
+}
+//
+<!-- Exemple dans le template du composant parent -->
+<app-votre-composant (postDisabledChange)="handlePostDisabledChange($event)" 
+                     (alertDisplayedChange)="handleAlertDisplayedChange($event)">
+</app-votre-composant>
+//
+export class ParentComposant {
+
+  postDisabled: boolean;
+  alertDisplayed: boolean;
+
+  handlePostDisabledChange(postDisabled: boolean) {
+    this.postDisabled = postDisabled;
+    console.log('postDisabled a changé:', postDisabled);
+  }
+
+  handleAlertDisplayedChange(alertDisplayed: boolean) {
+    this.alertDisplayed = alertDisplayed;
+    console.log('alertDisplayed a changé:', alertDisplayed);
+  }
+}
+	
