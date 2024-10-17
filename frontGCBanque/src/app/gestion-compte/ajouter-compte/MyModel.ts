19d760934e82a841ff5ec6b85d83513211e12b7e
ziedenit <guendil_zied@yahoo.com>
2024-10-17 17:47:16 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,46 +1,10 @@
-.container-starter {
-  margin-left: 50px;
-  margin-top: 50px;
-}
-
-h2, h3 {
-  text-align: left;
-}
-
-h1 {
-  display: inline-block;
-  font-size: 3rem; /* Taille de base */
-  max-width: 100%; /* Évite que le titre dépasse du conteneur */
-  word-wrap: break-word; /* Permet le retour à la ligne si nécessaire */
-}
-
-.header-title {
-  display: flex;
-  justify-content: center; /* Centre le titre horizontalement */
-  align-items: center; /* Centre le titre verticalement */
-  text-align: center;
-  margin: 0 auto;
-}
-
-.check {
-  height: 30px;
-}
-
-/* Media queries pour différentes tailles d'écrans */
-@media (max-width: 1200px) {
-  h1 {
-    font-size: 2.5rem; /* Pour les écrans larges */
-  }
-}
-
-@media (max-width: 768px) {
-  h1 {
-    font-size: 2rem; /* Pour les tablettes */
-  }
-}
-
-@media (max-width: 480px) {
-  h1 {
-    font-size: 1.5rem; /* Pour les petits écrans */
-  }
-}
+argocd.env: found 1 matching artifact files and directories 
+WARNING: Uploading artifacts as "dotenv" to coordinator... POST https://scm.saas.cagip.group.gca/api/v4/jobs/59397476/artifacts: 400 Bad Request (Invalid Format)  id=59397476 responseStatus=400 Bad Request status=400 token=64_np6bW
+WARNING: Retrying...                                context=artifacts-uploader error=invalid argument
+WARNING: Uploading artifacts as "dotenv" to coordinator... POST https://scm.saas.cagip.group.gca/api/v4/jobs/59397476/artifacts: 400 Bad Request (Invalid Format)  id=59397476 responseStatus=400 Bad Request status=400 token=64_np6bW
+WARNING: Retrying...                                context=artifacts-uploader error=invalid argument
+WARNING: Uploading artifacts as "dotenv" to coordinator... POST https://scm.saas.cagip.group.gca/api/v4/jobs/59397476/artifacts: 400 Bad Request (Invalid Format)  id=59397476 responseStatus=400 Bad Request status=400 token=64_np6bW
+FATAL: invalid argument                            
+Cleaning up project directory and file based variables
+00:01
+ERROR: Job failed: exit code 1
