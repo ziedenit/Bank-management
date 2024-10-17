2b4153c4e71f316b064ca03b618f19978ae6df23
ziedenit <guendil_zied@yahoo.com>
2024-10-17 14:07:51 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,10 +1,27 @@
- // Abonnement aux changements de valeur des champs
-    this.formGroup.get('lettreCEP').valueChanges.subscribe((value) => {
-      console.log('lettreCEP changed:', value); // Ajoutez un log pour déboguer
-      this.hideFieldCEP = !value || value === 'option0';
-    });
 
-    this.formGroup.get('lettreGES').valueChanges.subscribe((value) => {
-      console.log('lettreGES changed:', value); // Ajoutez un log pour déboguer
-      this.hideFieldGES = !value || value === 'option0';
-    });
+<div class="header">
+	<div class="row main">
+		<div class="col-2">
+			<div class="header-logo">
+				<img src="assets/images/logo-lcl.png" alt="Logo LCL">
+			</div>
+		</div>
+		<div class="col-8">
+			<div class="header-title">
+				<h1>{{title}}</h1>
+			</div>
+		</div>
+	
+	</div>
+</div>
+
+<app-oidc (userInfoEmitter)="connectedHandler($event)"></app-oidc>
+
+<div class="container-starter" *ngIf="connected && !errorOIDC">
+
+	</div>
+
+<div *ngIf="connected && !errorOIDC">
+	<router-outlet></router-outlet>
+</div>
+
