2ae91b6cde63dceb6d2eb199a46e599abf59f445
ziedenit <guendil_zied@yahoo.com>
2024-10-17 15:07:57 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,49 +1,46 @@
-<div class="header">
-	<div class="row main">
-		<div class="col-2">
-			<div class="header-logo">
-				<img src="assets/images/logo-lcl.png" alt="Logo LCL">
-			</div>
-		</div>
-		
-		<div class="checkbox-group">
-		<div class="row">
-			<div class="header-title">
-				<h1>{{title}}</h1>
-			</div>
-		</div>
-	</div>
-	
-	</div>
-</div>
-
-<app-oidc (userInfoEmitter)="connectedHandler($event)"></app-oidc>
-
-<div class="container-starter" *ngIf="connected && !errorOIDC">
-
-	</div>
-
-<div *ngIf="connected && !errorOIDC">
-	<router-outlet></router-outlet>
-</div>
-//
-	.container-starter {
+.container-starter {
   margin-left: 50px;
   margin-top: 50px;
 }
 
-
-h1{
-  display: inline-block;  
+h2, h3 {
+  text-align: left;
 }
 
-.check{
- height: 30px;
+h1 {
+  display: inline-block;
+  font-size: 3rem; /* Taille de base */
+  max-width: 100%; /* Évite que le titre dépasse du conteneur */
+  word-wrap: break-word; /* Permet le retour à la ligne si nécessaire */
 }
 
-.checkbox-group {
+.header-title {
   display: flex;
-  flex-direction: column;
-  gap: 10px;
+  justify-content: center; /* Centre le titre horizontalement */
+  align-items: center; /* Centre le titre verticalement */
+  text-align: center;
+  margin: 0 auto;
 }
-	
+
+.check {
+  height: 30px;
+}
+
+/* Media queries pour différentes tailles d'écrans */
+@media (max-width: 1200px) {
+  h1 {
+    font-size: 2.5rem; /* Pour les écrans larges */
+  }
+}
+
+@media (max-width: 768px) {
+  h1 {
+    font-size: 2rem; /* Pour les tablettes */
+  }
+}
+
+@media (max-width: 480px) {
+  h1 {
+    font-size: 1.5rem; /* Pour les petits écrans */
+  }
+}
