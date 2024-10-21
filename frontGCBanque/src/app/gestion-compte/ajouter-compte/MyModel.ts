0aa16ad2038bdcac3f666999311d46ff3b6b73d7
ziedenit <guendil_zied@yahoo.com>
2024-10-21 15:55:19 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,21 +1,14 @@
 apiVersion: networking.k8s.io/v1
 kind: NetworkPolicy
 metadata:
-  name: allow-namespace-y-to-access-api
-  namespace: namespace-x  # Namespace où se trouve l'API (namespace cible)
+  name: allow-mesperone-ingress
+  namespace: namespace-x  # Remplace par ton namespace cible
 spec:
-  podSelector:
-    matchLabels:
-      app: my-api  # Les pods de l'API dans namespace-x
+  podSelector: {}  # Applique la règle à tous les pods dans namespace-x
   policyTypes:
   - Ingress
   ingress:
   - from:
     - namespaceSelector:
         matchLabels:
-          name: namespace-y  # Namespace source (namespace Y)
-    ports:
-    - protocol: TCP
-      port: 80  # Port où l'API écoute (ex. HTTP)
-    - protocol: TCP
-      port: 443  # Port pour HTTPS, si nécessaire
+          name: mesperone  # Autoriser le trafic uniquement depuis mesperone
