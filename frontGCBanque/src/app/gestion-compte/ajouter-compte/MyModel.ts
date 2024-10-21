04c264e15636250569964b0c03158a817a2ba5d4
ziedenit <guendil_zied@yahoo.com>
2024-10-21 15:49:58 +0200

    Update MyModel.ts

--- frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
+++ frontGCBanque/src/app/gestion-compte/ajouter-compte/MyModel.ts
@@ -1,10 +1,21 @@
-apiVersion: rbac.authorization.k8s.io/v1
-kind: Role
+apiVersion: networking.k8s.io/v1
+kind: NetworkPolicy
 metadata:
-  namespace: nom-du-namespace
-  name: pod-reader
-rules:
-- apiGroups: [""]
-  resources: ["pods"]
-  verbs: ["get", "list", "watch"]
-	  
+  name: allow-namespace-y-to-access-api
+  namespace: namespace-x  # Namespace où se trouve l'API (namespace cible)
+spec:
+  podSelector:
+    matchLabels:
+      app: my-api  # Les pods de l'API dans namespace-x
+  policyTypes:
+  - Ingress
+  ingress:
+  - from:
+    - namespaceSelector:
+        matchLabels:
+          name: namespace-y  # Namespace source (namespace Y)
+    ports:
+    - protocol: TCP
+      port: 80  # Port où l'API écoute (ex. HTTP)
+    - protocol: TCP
+      port: 443  # Port pour HTTPS, si nécessaire
